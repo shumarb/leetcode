9f503f374627586897c2199cf8c564c9d32ba7cc
Umar <shumarb@outlook.com>
2025-01-04 11:28:21 +0800

    Updated code to match that of submission

--- leetcode/easy/java/PalindromeNumber.java
+++ leetcode/easy/java/PalindromeNumber.java
@@ -1,13 +1,13 @@
 class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        boolean isTest = false;
-        String strX = Integer.toString(x);
-        int j = strX.length() - 1;
-        for (int i = 0; i < strX.length() - 1; i++) {
-            if (isTest) {
-                System.out.println("compare: " + strX.charAt(i) + " vs " + strX.charAt(j));
-            }
-            if (strX.charAt(i) != strX.charAt(j--)) {
+        if (x < 0) {
+            return false;
+        }
+        String xStr = String.valueOf(x);
+        int l = 0;
+        int r = xStr.length() - 1;
+        while (l < r) {
+            if (xStr.charAt(l++) != xStr.charAt(r--)) {
                 return false;
             }
         }
