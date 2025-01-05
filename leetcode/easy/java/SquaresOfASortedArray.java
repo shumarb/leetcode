31cee61f8547333b7f16bfe1e5ea0f9a2589140d
Umar <shumarb@outlook.com>
2025-01-05 11:16:49 +0800

    Improved performance (using 2 pointers in opposite direction)

--- leetcode/easy/java/SquaresOfASortedArray.java
+++ leetcode/easy/java/SquaresOfASortedArray.java
@@ -1,9 +1,19 @@
 class SquaresOfASortedArray {
-    public int[] sortedSquares(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A[i] * A[i];
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
+        while (i <= j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                result[k--] = nums[i] * nums[i];
+                i++;
+            } else {
+                result[k--] = nums[j] * nums[j];
+                j--;
+            }
         }
-        Arrays.sort(A);
-        return A;
+        return result;
     }
+
 }
