42098d17d60fe603cc49a0cbf079fbb5d0d15bba
Umar <shumarb@outlook.com>
2025-01-08 12:42:02 +0800

    Added comments to to reinforce understanding of heap logic, improved performance after resubmission of same code

--- medium/java/TopKFrequentWords.java
+++ medium/java/TopKFrequentWords.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/top-k-frequent-words/description/
 
-class TopKFrequentWords {
+class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         boolean t = false;
         List<String> ans = new ArrayList<> ();
@@ -13,25 +13,49 @@ class TopKFrequentWords {
             System.out.println("map: " + map);
         }
 
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, (a, b) -> {
-            if (b.getValue() != a.getValue()) {
-                return b.getValue() - a.getValue();
+        /**
+         Given ordering: Decreasing value, Increasing key
+         Arranging of elements:
+         - Max heap: Prioritizes larger values first, and for equal values, prioritize smaller keys (lexicographically).
+         - Min heap: Prioritizes smaller values first, and for equal values, prioritize larger keys (lexicographically).
+         */
+        PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<>(
+                (a, b) -> a.getValue() == b.getValue()
+                        ? b.getKey().compareTo(a.getKey())
+                        : a.getValue() - b.getValue()
+        );
+        for (Map.Entry<String, Integer> e: map.entrySet()) {
+            minHeap.offer(e);
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
-            return a.getKey().compareTo(b.getKey());
-        });
-        if (t) {
-            System.out.println("list: " + list);
-        }
-
-        for (int i = 0; i < k; i++) {
-            Map.Entry<String, Integer> entry = list.get(i);
-            ans.add(entry.getKey());
         }
         if (t) {
-            System.out.println(ans);
+            System.out.println("minHeap: " + minHeap);
         }
 
+        /**
+         MinHeap ordering from root to leaves in increasing order of values:
+         kth, (k-1)th, (k-2)th,...2nd, 1st
+         so ans is ordered as kth, (k-1)th, (k-2)th,...2nd, 1st
+         hence reverse ans via collections.
+
+         eg: [9 2 4 5 7], k = 3
+         minHeap of size 3: [5, 7, 9], root is kth smallest
+         add root of minHeap until empty --> ans [5 7 9]
+         then reverse the order to get descending order --> [9 7 5].
+         */
+        int i = k - 1;
+        while (!minHeap.isEmpty()) {
+            ans.add(minHeap.poll().getKey());
+        }
+        if (t) {
+            System.out.println("before reverse: " + ans);
+        }
+        Collections.reverse(ans);
+        if (t) {
+            System.out.println("after reverse: " + ans);
+        }
         return ans;
     }
 }
