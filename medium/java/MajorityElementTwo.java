f6ee11b0c71121e1dea2b2d6e9a748044eaaeeb8
Umar <shumarb@outlook.com>
2025-01-11 11:35:28 +0800

    New submission - Majority Element II

---
+++ medium/java/MajorityElementTwo.java
@@ -0,0 +1,30 @@
+// Question: https://leetcode.com/problems/majority-element-ii/description/
+
+class MajorityElement2 {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> list = new ArrayList<> ();
+        Map<Integer, Integer> map = new HashMap<> ();
+        boolean t = false;
+
+        for (int x: nums) {
+            map.put(x, 1 + map.getOrDefault(x, 0));
+        }
+        if (t) {
+            System.out.println("nums: " + Arrays.toString(nums));
+            System.out.println("map: " + map);
+            System.out.println("list: " + list);
+        }
+        for (Map.Entry<Integer, Integer> e: map.entrySet()) {
+            if (t) {
+                System.out.println("e: " + e);
+            }
+            if (e.getValue() > (nums.length / 3)) {
+                list.add(e.getKey());
+            }
+        }
+        if (t) {
+            System.out.println("after | list: " + list);
+        }
+        return list;
+    }
+}
