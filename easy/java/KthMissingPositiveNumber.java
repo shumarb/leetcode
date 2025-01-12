c6dc3cb04d29b5a004699b634d5fb6fde1ef99bf
Umar <shumarb@outlook.com>
2025-01-12 13:18:52 +0800

    Resubmission with less code, same performance data: Kth Missing Positive Number

--- easy/java/KthMissingPositiveNumber.java
+++ easy/java/KthMissingPositiveNumber.java
@@ -1,56 +1,39 @@
 class KthMissingPositiveNumber {
     public int findKthPositive(int[] arr, int k) {
-        int[] nums = new int[arr[arr.length - 1] + 1];
-        List<Integer> missing = new ArrayList<> ();
+        int last = arr[arr.length - 1];
         boolean t = false;
-        for (int x: arr) {
-            nums[x] = 1;
+        boolean[] missingInArr = new boolean[last + 1];
+        Arrays.fill(missingInArr, true);
+        for (int i = 0; i < arr.length; i++) {
+            missingInArr[arr[i]] = false;
         }
         if (t) {
             System.out.println("arr: " + Arrays.toString(arr));
-            System.out.println("nums: " + Arrays.toString(nums));
+            System.out.println("missingInArr: " + Arrays.toString(missingInArr));
         }
 
-        // 1. Check if kth missing is within arr)
-        for (int i = 1; i <= nums.length - 1; i++) {
-            // 1.1. Missing number found, add it to list of missing numbers
-            if (nums[i] == 0) {
-                if (t) {
-                    System.out.println("adding " + i);
-                }
-                missing.add(i);
-                // 1.2. If k missing numbers achieved, current number is the kth number
-                if (missing.size() == k) {
-                    return i;
-                }
+        int count = 0;
+        int kthMissing = 0;
+        for (int i = 1; i <= last; i++) {
+            if (missingInArr[i]) {
+                count++;
             }
-        }
-        if (t) {
-            System.out.println("missing: " + missing);
-        }
-
-        /**
-         2.  At this point, number of missing numbers accumulated < k
-         and all missing numbers checked in range [1, last element of array],
-         so starting from (1 + last element of array),
-         keep on adding until k numbers of missing obtained
-         */
-        int num = arr[arr.length - 1] + 1;
-        if (t) {
-            System.out.println("start from " + num);
-        }
-
-        while (missing.size() != k) {
-            missing.add(num);
-            if (missing.size() == k) {
+            if (count == k) {
+                kthMissing = i;
                 break;
             }
-            num += 1;
-        }
-        if (t) {
-            System.out.println("missing: " + missing);
         }
 
-        return num;
+        int start = last + 1;
+        while (count != k) {
+            kthMissing = start;
+            count += 1;
+            if (count == k) {
+                break;
+            }
+            start += 1;
+        }
+
+        return kthMissing;
     }
 }
