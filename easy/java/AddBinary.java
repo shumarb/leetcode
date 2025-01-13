8c09c54f129e83663fe6f0bb3982b1701e771e9d
Umar <shumarb@outlook.com>
2025-01-13 14:40:17 +0800

    New submission: Add Binary

---
+++ easy/java/AddBinary.java
@@ -0,0 +1,16 @@
+// Question: https://leetcode.com/problems/add-binary/description/
+
+import java.math.BigInteger;
+
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger b1 = new BigInteger(a, 2);
+        BigInteger b2 = new BigInteger(b, 2);
+        BigInteger sum = b1.add(b2);
+        boolean t = false;
+        if (t) {
+            System.out.println("b1: " + b1 + "\nb2: " + b2 + "\nsum: " + sum + "\nsum in base 2: " + sum.toString(2));
+        }
+        return sum.toString(2);
+    }
+}
