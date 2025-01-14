c6b5643cb44586671965f749372feb722eaffb36
Umar <shumarb@outlook.com>
2025-01-14 15:52:52 +0800

    Reorganised comments

--- medium/java/FindMinimumInRotatedSortedArray.java
+++ medium/java/FindMinimumInRotatedSortedArray.java
@@ -9,20 +9,20 @@ class FindMinimumInRotatedSortedArray {
             int mid = low + (high - low) / 2;
 
             /**
-             1.  All elements from indices [mid, high] are sorted in ascended order,
-             but element at index mid could be the minimum,
-             so discard all element from indices [mid + 1, high]
-             and check all element from indices [low, mid]
+                 1.  All elements from indices [mid, high] are sorted in ascended order,
+                 but element at index mid could be the minimum,
+                 so discard all element from indices [mid + 1, high]
+                 and check all element from indices [low, mid]
              */
             if (nums[mid] <= nums[high]) {
                 high = mid;
 
-                /**
+            /**
                  2.  All numbers from indices [low, mid] sorted in ascending order,
                  so the minimum is not in indices [low, mid],
                  so discard all elements from [low, mid]
                  and check all element from indices [mid + 1, high]
-                 */
+             */
             } else {
                 low = mid + 1;
             }
