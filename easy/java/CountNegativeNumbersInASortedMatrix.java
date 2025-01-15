af0ff5afa7010d883d9768e74d5a10696cf7655f
Umar <shumarb@outlook.com>
2025-01-15 10:26:24 +0800

    Added submission with improved rank using binary search

--- easy/java/CountNegativeNumbersInASortedMatrix.java
+++ easy/java/CountNegativeNumbersInASortedMatrix.java
@@ -1,107 +1,42 @@
 // Question: https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
 
 class CountNegativeNumbersInASortedMatrix {
+    private boolean t = false;
+
     public int countNegatives(int[][] grid) {
-        int countNegative = 0;
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] < 0) {
-                    countNegative++;
-                }
+            int[] arr = grid[i];
+            if (t) {
+                System.out.println("arr: " + Arrays.toString(arr));
+            }
+            count += countNegative(arr);
+        }
+
+        return count;
+    }
+
+    private int countNegative(int[] arr) {
+        int count = 0;
+        int low = 0;
+        int high = arr.length - 1;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (t) {
+                System.out.println("index    | " + low + ", " + mid + ", " + high);
+                System.out.println("elements | " + arr[low] + ", " + arr[mid] + ", " + arr[high]);
+            }
+            if (arr[mid] >= 0) {
+                low = mid + 1;
+            } else {
+                index = mid;
+                high = mid - 1;
             }
         }
-        return countNegative;
+        if (t) {
+            System.out.println("first negative index: " + index);
+        }
+        return (index == -1) ? 0 : arr.length - index;
     }
 }
-
-/**
- * Solution done using binary search of last negative index and first positive index
- * with runtime of O(log N) time,
- * but performance after submission shows solution runs in 1ms, beating 20.51%
- *
- *
- * class Solution {
- *     private boolean t = false;
- *
- *     public int maximumCount(int[] nums) {
- *         int firstP = getFirstP(nums);
- *         int lastN = getLastN(nums);
- *         int countP = countPositive(nums, firstP);
- *         int countN = countNegative(nums, lastN);
- *         return Math.max(countP, countN);
- *     }
- *
- *     private int countNegative(int[] nums, int n) {
- *         if (n == -1) {
- *             return 0;
- *         }
- *         int count = 0;
- *         for (int i = 0; i < n; i++) {
- *             count++;
- *         }
- *
- *         count += 1; // increase by 1 due to 0-based counting if counting from start of array
- *         if (t) {
- *             System.out.println("count negative: " + count);
- *         }
- *         return count;
- *     }
- *
- *     private int countPositive(int[] nums, int p) {
- *         if (p == -1) {
- *             return 0;
- *         }
- *         int count = 0;
- *         for (int i = p; i < nums.length; i++) {
- *             count++;
- *         }
- *
- *         if (t) {
- *             System.out.println("count positive: " + count);
- *         }
- *         return count;
- *     }
- *
- *     private int getLastN(int[] nums) {
- *         int low = 0;
- *         int high = nums.length - 1;
- *         int index = -1;
- *
- *         while (low <= high) {
- *             int mid = low + (high - low) / 2;
- *             if (nums[mid] >= 0) {
- *                 high = mid - 1;
- *             } else {
- *                 index = mid;
- *                 low = mid + 1;
- *             }
- *         }
- *
- *         if (t) {
- *             System.out.println("negative | index: " + index);
- *         }
- *         return index;
- *     }
- *
- *     private int getFirstP(int[] nums) {
- *         int index = -1;
- *         int low = 0;
- *         int high = nums.length - 1;
- *
- *         while (low <= high) {
- *             int mid = low + (high - low) / 2;
- *             if (nums[mid] <= 0) {
- *                 low = mid + 1;
- *             } else {
- *                 index = mid;
- *                 high = mid - 1;
- *             }
- *         }
- *
- *         if (t) {
- *             System.out.println("positive | index: " + index);
- *         }
- *         return index;
- *     }
- * }
- */
