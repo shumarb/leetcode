4a1a7d6f4ed430abfe12ccc4b1f1e7b757529585
Umar <shumarb@outlook.com>
2025-01-16 11:47:20 +0800

    Question: Intersection of Two Arrays | Submission of new solution with same runtime and improved memory complexity

--- easy/java/IntersectionOfTwoArrays.java
+++ easy/java/IntersectionOfTwoArrays.java
@@ -2,37 +2,66 @@
 
 class IntersectionOfTwoArrays {
     public int[] intersection(int[] nums1, int[] nums2) {
-        boolean[] a1 = new boolean[1001];
-        boolean[] a2 = new boolean[1001];
+        int[] small;
+        int[] big;
         boolean t = false;
 
-        populate(nums1, a1);
-        populate(nums2, a2);
-
-        int c = 0;
-        for (int i = 0; i <= 1000; i++) {
-            if (a1[i] && a2[i]) {
-                c++;
-            }
+        if (nums1.length > nums2.length) {
+            big = nums1;
+            small = nums2;
+        } else {
+            small = nums1;
+            big = nums2;
+        }
+        if (t) {
+            System.out.println("nums1: " + Arrays.toString(nums1) + ", nums2: " + Arrays.toString(nums2));
+            System.out.println("big: " + Arrays.toString(big) + ", small: " + Arrays.toString(small));
         }
 
-        int[] ans = new int[c];
+        int max1 = getMax(nums1);
+        int max2 = getMax(nums2);
+        if (t) {
+            System.out.println("max1: " + max1 + ", max2: " + max2);
+        }
+
+        boolean[] bigFreq = new boolean[1 + Math.max(max1, max2)];
+        for (int x: big) {
+            bigFreq[x] = true;
+        }
+        if (t) {
+            System.out.println("bigFreq: " + Arrays.toString(bigFreq));
+        }
+
+        List<Integer> list = new ArrayList<> ();
+        for (int x: small) {
+            if (bigFreq[x]) {
+                if (t) {
+                    System.out.println("intersect: " + x);
+                }
+                list.add(x);
+                bigFreq[x] = false;
+            }
+        }
+        if (t) {
+            System.out.println("list: " + list);
+        }
+
+        int[] ans = new int[list.size()];
         int j = 0;
-        for (int i = 0; i <= 1000; i++) {
-            if (a1[i] && a2[i]) {
-                ans[j++] = i;
-            }
+        for (int x: list) {
+            ans[j++] = x;
         }
-
         if (t) {
             System.out.println("ans: " + Arrays.toString(ans));
         }
         return ans;
     }
 
-    private void populate(int[] n, boolean[] a) {
-        for (int x: n) {
-            a[x] = true;
+    private int getMax(int[] a) {
+        int max = a[0];
+        for (int x: a) {
+            max = Math.max(max, x);
         }
+        return max;
     }
 }
