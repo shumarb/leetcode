2a258f7cc8cd641528e4eff29af938bb3d3d0d40
Umar <shumarb@outlook.com>
2025-01-16 19:18:04 +0800

    New submission: Maximum Depth of Binary Tree

---
+++ easy/java/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+// Question: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
