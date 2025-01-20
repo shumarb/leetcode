9c6a8518094ba702724b6417443abffe995e1e2e
Umar <shumarb@outlook.com>
2025-01-21 00:18:55 +0800

    New submission: Longest Common Prefix

---
+++ easy/java/LongestCommonPrefix.java
@@ -0,0 +1,45 @@
+// Question:
+
+class LongestCommonPrefix {
+    private boolean t = false;
+
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        String shortest = findShortest(strs);
+        if (t) {
+            System.out.println("strs: " + Arrays.toString(strs));
+            System.out.println("shortest: " + shortest);
+        }
+        for (int i = 0; i < shortest.length(); i++) {
+            boolean result = update(shortest, i, strs);
+            if (!result) {
+                break;
+            } else {
+                sb.append(shortest.charAt(i));
+            }
+        }
+        if (t) {
+            System.out.println("final result: " + sb.toString());
+        }
+        return sb.toString();
+    }
+
+    private boolean update(String shortest, int i, String[] strs) {
+        for (String word: strs) {
+            if (word.charAt(i) != shortest.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String findShortest(String[] strs) {
+        String shortest = strs[0];
+        for (String x: strs) {
+            if (x.length() < shortest.length()) {
+                shortest = x;
+            }
+        }
+        return shortest;
+    }
+}
