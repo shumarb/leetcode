2940c499e6030f82b294460e071a8f61aed67bce
Umar <shumarb@outlook.com>
2025-01-21 20:00:36 +0800

    New submission: Find the Peaks

---
+++ easy/java/FindThePeaks.java
@@ -0,0 +1,13 @@
+// Question: https://leetcode.com/problems/find-the-peaks/description/
+
+class FindThePeaks {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> peaks = new ArrayList<> ();
+        for (int i = 1; i < mountain.length - 1; i++) {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+                peaks.add(i);
+            }
+        }
+        return peaks;
+    }
+}
