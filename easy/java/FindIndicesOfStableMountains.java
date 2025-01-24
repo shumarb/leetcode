f5395e48e5d87ceb7b373711fc32850b771b36fe
Umar <shumarb@outlook.com>
2025-01-25 03:14:02 +0800

    New submission: Find Indices Of Stable Mountains

---
+++ easy/java/FindIndicesOfStableMountains.java
@@ -0,0 +1,13 @@
+// Question: https://leetcode.com/problems/find-indices-of-stable-mountains/description/
+
+class FindIndicesOfStableMountains {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> ans = new ArrayList<> ();
+        for (int i = 1; i < height.length; i++) {
+            if (height[i - 1] > threshold) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
