e268fa40034c51e30e079700bbe750ea0eef0753
Umar <shumarb@outlook.com>
2025-01-25 14:53:18 +0800

    Updated submission with same runtime and rank but is concise

--- easy/java/ContainsDuplicate.java
+++ easy/java/ContainsDuplicate.java
@@ -3,15 +3,12 @@
 class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<> ();
-        for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            if (set.contains(n)) {
+        for (int element: nums) {
+            if (set.contains(element)) {
                 return true;
-            } else {
-                set.add(n);
             }
+            set.add(element);
         }
         return false;
     }
-
 }
