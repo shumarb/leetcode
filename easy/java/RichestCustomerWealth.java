d64e85d36d1c0a10cb7f131621cf0859df6bf617
Umar <shumarb@outlook.com>
2025-01-26 12:46:03 +0800

    Updated submission with reduced code and improved runtime and rank

--- easy/java/RichestCustomerWealth.java
+++ easy/java/RichestCustomerWealth.java
@@ -1,18 +1,15 @@
+// Question: https://leetcode.com/problems/richest-customer-wealth/description/
+
 class RichestCustomerWealth {
     public int maximumWealth(int[][] accounts) {
-        int wealth = 0;
-        for (int j = 0; j < accounts[0].length; j++) {
-            wealth += accounts[0][j];
-        }
-
-        for (int i = 1; i < accounts.length; i++) {
-            int newWealth = 0;
-            for (int j = 0; j < accounts[i].length; j++) {
-                newWealth += accounts[i][j];
+        int maximumWealth = 0;
+        for (int[] customer: accounts) {
+            int currentWealth = 0;
+            for (int money: customer) {
+                currentWealth += money;
             }
-            wealth = Math.max(wealth, newWealth);
+            maximumWealth = Math.max(currentWealth, maximumWealth);
         }
-        return wealth;
+        return maximumWealth;
     }
-
 }
