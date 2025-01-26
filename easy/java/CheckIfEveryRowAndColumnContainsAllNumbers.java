e41264d81cc34b7033767a28b1d16dddb5ddf032
Umar <shumarb@outlook.com>
2025-01-26 16:27:48 +0800

    New submission: Check if Every Row and Column Contains All Numbers

---
+++ easy/java/CheckIfEveryRowAndColumnContainsAllNumbers.java
@@ -0,0 +1,52 @@
+// Question: https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/description/
+
+class CheckIfEveryRowAndColumnContainAllNumbers {
+    public boolean checkValid(int[][] matrix) {
+        int n = matrix.length;
+
+        if (n == 1) {
+            return matrix[0][0] >= 1 && matrix[0][0] <= n;
+        }
+
+        boolean isTest = false;
+        for (int[] rowArr: matrix) {
+            if (isTest) {
+                System.out.println("rowArr: " + Arrays.toString(rowArr));
+            }
+            if (!isValid(rowArr, n)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int k = 0;
+            int[] colArr = new int[n];
+            for (int j = 0; j < n; j++) {
+                colArr[k++] = matrix[j][i];
+            }
+            if (isTest) {
+                System.out.println("colArr: " + Arrays.toString(colArr));
+            }
+            if (!isValid(colArr, n)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(int[] arr, int n) {
+        int[] freqArr = new int[n + 1];
+        for (int element: arr) {
+            if (element >= 1 && element <= n) {
+                freqArr[element]++;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (freqArr[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
