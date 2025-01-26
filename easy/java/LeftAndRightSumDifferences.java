0e79d485a85867b0fcc6f5019e8d9603ee8d63a6
Umar <shumarb@outlook.com>
2025-01-26 12:30:52 +0800

    Updated solution with same runtime, tiny drop in rank, and significantly reduced code and improved space complexity

--- easy/java/LeftAndRightSumDifferences.java
+++ easy/java/LeftAndRightSumDifferences.java
@@ -2,32 +2,19 @@
 
 class LeftAndRightSumDifferences {
     public int[] leftRightDifference(int[] nums) {
-        int n = nums.length;
-        int[] diff = new int[n];
-        int[] pSum = new int[n];
-        int[] sSum = new int[n];
-        boolean t = false;
-
-        pSum[0] = sSum[n - 1] = 0;
-        for (int i = 1; i < n; i++) {
-            pSum[i] = nums[i - 1] + pSum[i - 1];
-        }
-        for (int i = n - 2; i >= 0; i--){
-            sSum[i] = nums[i + 1] + sSum[i + 1];
-        }
-        if (t) {
-            System.out.println("pSum:   " + Arrays.toString(pSum));
-            System.out.println("nums:   " + Arrays.toString(nums));
-            System.out.println("sSum:   " + Arrays.toString(sSum));
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int element: nums) {
+            rightSum += element;
         }
 
-        for (int i = 0; i < n; i++) {
-            diff[i] = Math.abs(pSum[i] - sSum[i]);
-        }
-        if (t) {
-            System.out.println("diff:   " + Arrays.toString(diff));
+        for (int i = 0; i < nums.length; i++) {
+            int value = nums[i];
+            rightSum -= value;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum += value;
         }
 
-        return diff;
+        return nums;
     }
 }
