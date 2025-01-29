af63f52f8a76eccedc922ada212a6b2078eba623
Umar <shumarb@outlook.com>
2025-01-29 11:59:02 +0800

    New submission: Three Divisors

---
+++ easy/java/ThreeDivisors.java
@@ -0,0 +1,24 @@
+// Question:
+
+class ThreeDivisors {
+    public boolean isThree(int n) {
+        if (n <= 3) {
+            return false;
+        }
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isDivisible(n, i)) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+
+    private boolean isDivisible(int n, int d) {
+        boolean isTest = false;
+        if (isTest) {
+            System.out.println("check | n: " + n + ", d: " + d);
+        }
+        return n % d == 0;
+    }
+}
