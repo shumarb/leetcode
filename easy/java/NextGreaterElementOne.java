20cf9d40b16151424eec9dc458faeedf63164a15
Umar <shumarb@outlook.com>
2025-01-30 11:37:29 +0800

    Updated solution using stack to consolidate understanding, decrease in runtime and rank

--- easy/java/NextGreaterElementOne.java
+++ easy/java/NextGreaterElementOne.java
@@ -1,48 +1,37 @@
-// Question:
+// Question: https://leetcode.com/problems/next-greater-element-i/description/
 
 class NextGreaterElementOne {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        boolean t = false;
-        Map<Integer, Integer> indexMap = new HashMap<>();
-        int max = getMax(nums2);
-        for (int i = 0; i < nums2.length; i++) {
-            indexMap.put(nums2[i], i);
-        }
-        if (t) {
-            System.out.println("nums1: " + Arrays.toString(nums1));
-            System.out.println("nums2: " + Arrays.toString(nums2));
-            System.out.println("max in nums2: " + max);
-            System.out.println("indexMap of nums2: " + indexMap);
-        }
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        boolean isTest = false;
 
-        for (int i = 0; i < nums1.length; i++) {
-            if (nums1[i] == max) {
-                nums1[i] = -1;
-            } else {
-                nums1[i] = findGreater(nums1[i], indexMap, nums2);
+        // 1. Map will contain mapping of an element and its next greater element
+        // Eg: [1 3 4 2] ==> map: {1=3, 3=4}
+        for (Integer element: nums2) {
+            // 1.1. Keep removing all elements from stack until first greater element
+            // than current element is found
+            while (!stack.isEmpty() && stack.peek() < element) {
+                map.put(stack.pop(), element);
             }
+            stack.push(element);
         }
-        if (t) {
-            System.out.println("final nums1: " + Arrays.toString(nums1));
+        if (isTest) {
+            System.out.println("nums1: " + Arrays.toString(nums1) + ", nums2: " + Arrays.toString(nums2) + "\nmap: " + map);
         }
-        return nums1;
-    }
 
-    private int findGreater(int elem, Map<Integer, Integer> map, int[] arr) {
-        int startIndex = map.get(elem);
-        for (int i = startIndex + 1; i < arr.length; i++) {
-            if (arr[i] > elem) {
-                return arr[i];
-            }
+        // 2. Iterate through all elements in nums1 and check if its element is in the map.
+        // If yes, that means there exists a first greater element to the right of the current element,
+        // else there is no first greater element to the right of the current element.
+        int len = nums1.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
-        return -1;
-    }
 
-    private int getMax(int[] n) {
-        int max = n[0];
-        for (int x: n) {
-            max = Math.max(max, x);
+        if (isTest) {
+            System.out.println("ans: " + Arrays.toString(ans));
         }
-        return max;
+        return ans;
     }
 }
