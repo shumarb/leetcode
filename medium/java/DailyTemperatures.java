d3ca3876369bffccf8cf89f489d96ad9aaa5cdd5
Umar <shumarb@outlook.com>
2025-01-30 10:52:28 +0800

    Worked on solution to consolidate understanding, adding comments to explain logic

--- medium/java/DailyTemperatures.java
+++ medium/java/DailyTemperatures.java
@@ -2,23 +2,28 @@
 
 import java.util.Stack;
 
-class DailyTemperatures class Solution {
+class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] answer = new int[n];
-        Stack<Integer> nextGreaterElementIndexStack = new Stack<> ();
+        int len = temperatures.length;
+        int[] answer = new int[len];
+        Stack<Integer> nextGreaterElementIndexStack = new Stack<>();
 
-        for (int i = n - 1; i >=0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
+            // 1. Remove elements from stack until element greater than current element is found
             while (!nextGreaterElementIndexStack.isEmpty() && temperatures[nextGreaterElementIndexStack.peek()] <= temperatures[i]) {
                 nextGreaterElementIndexStack.pop();
             }
 
-            if (nextGreaterElementIndexStack.isEmpty()) {
-                answer[i] = 0;
-            } else {
+            // 2. If the stack is not empty, the top element of stack is index of next greater element of current element
+            // so take index of next greater element (top element of stack) - current index
+            // If stack is empty, there is no element greater than current element from the right of current element,
+            // hence answer[i] = 0, but since answer array has been initialised, we don't have to manually set answer[i] to 0.
+            if (!nextGreaterElementIndexStack.isEmpty()) {
                 answer[i] = nextGreaterElementIndexStack.peek() - i;
             }
 
+            // 3. Lastly, add current index into stack as it could be the index of next greater element
+            // of elements in temperatures array from indices [0, i - 1]
             nextGreaterElementIndexStack.push(i);
         }
 
