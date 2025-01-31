5c71ded6555e4ed63806a2c9efe2bf83c208b54d
Umar <shumarb@outlook.com>
2025-02-01 04:59:07 +0800

    Add Two Numbers: Updated solution on single iteratation of linked lists and checks to prevent exceptions being thrown, improvement in runtime and rank

--- medium/java/AddTwoNumbers.java
+++ medium/java/AddTwoNumbers.java
@@ -1,4 +1,5 @@
 // Question: https://leetcode.com/problems/add-two-numbers/description/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,62 +12,33 @@
  */
 class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode current = head;
+        ListNode l1Current = l1;
+        ListNode l2Current = l2;
+        int carry = 0;
         boolean isTest = false;
 
-        String l1String = formsString(l1);
-        String l2String = formsString(l2);
-        String sum = computeSum(l1String, l2String);
-        if (isTest) {
-            System.out.println("l1String: " + l1String);
-            System.out.println("l2String: " + l2String);
-            System.out.println("sum: " + sum);
-        }
+        while (l1Current != null || l2Current != null | carry != 0) {
+            int val1 = (l1Current != null) ? l1Current.val : 0;
+            int val2 = (l2Current != null) ? l2Current.val : 0;
+            int sum = val1 + val2 + carry;
 
-        ListNode head = new ListNode(Character.getNumericValue(sum.charAt(0)));
-        ListNode curr = head;
-        for (int i = 1; i < sum.length(); i++) {
-            ListNode newNode = new ListNode(Character.getNumericValue(sum.charAt(i)));
-            curr.next = newNode;
-            curr = curr.next;
-        }
-
-        return head;
-    }
-
-    private String computeSum(String firstString, String secondString) {
-        StringBuilder result = new StringBuilder();
-        int firstPointer = firstString.length() - 1;
-        int secondPointer = secondString.length() - 1;
-        int carryOver = 0;
-
-        while (firstPointer >= 0 || secondPointer >= 0) {
-            int firstStringDigit = firstPointer >= 0 ? Character.getNumericValue(firstString.charAt(firstPointer)) : 0;
-            int secondStringDigit = secondPointer >= 0 ? Character.getNumericValue(secondString.charAt(secondPointer)) : 0;
-            int sum = carryOver + firstStringDigit + secondStringDigit;
-            if (sum < 9) {
-                result.append(sum);
-                carryOver = 0;
-            } else {
-                result.append(sum % 10);
-                carryOver = sum / 10;
+            if (isTest) {
+                System.out.println("val1: " + val1 + ", val2: " + val2 + ", carry: " + carry + " ==> sum: " + sum);
             }
-            firstPointer--;
-            secondPointer--;
+
+            current.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            current = current.next;
+            if (l1Current != null) {
+                l1Current = l1Current.next;
+            }
+            if (l2Current != null) {
+                l2Current = l2Current.next;
+            }
         }
 
-        if (carryOver != 0) {
-            result.append(carryOver);
-        }
-        return result.toString();
-    }
-
-    private String formsString(ListNode list) {
-        StringBuilder number = new StringBuilder();
-        ListNode curr = list;
-        while (curr != null) {
-            number.append(curr.val);
-            curr = curr.next;
-        }
-        return number.reverse().toString();
+        return head.next;
     }
 }
