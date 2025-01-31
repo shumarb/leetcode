043288da99af00fbca3a5743666222e2e6a08190
Umar <shumarb@outlook.com>
2025-02-01 04:27:10 +0800

    Add Two Numbers: New submission without use of Big Integer, with significant reduction in runtime and slight improvement in rank

--- medium/java/AddTwoNumbers.java
+++ medium/java/AddTwoNumbers.java
@@ -1,7 +1,4 @@
 // Question: https://leetcode.com/problems/add-two-numbers/description/
-
-import java.math.BigInteger;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -14,50 +11,62 @@ import java.math.BigInteger;
  */
 class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        boolean t = false;
+        boolean isTest = false;
 
-        update(l1, sb1);
-        update(l2, sb2);
-        if (t) {
-            System.out.println("sb1: " + sb1 + "\nsb2: " + sb2);
-        }
-
-        BigInteger b1 = new BigInteger(sb1.toString());
-        BigInteger b2 = new BigInteger(sb2.toString());
-        if (t) {
-            System.out.println("b1: " + b1 + "\nb2: " + b2);
-        }
-        String sum = b1.add(b2).toString();
-        if (t) {
+        String l1String = formsString(l1);
+        String l2String = formsString(l2);
+        String sum = computeSum(l1String, l2String);
+        if (isTest) {
+            System.out.println("l1String: " + l1String);
+            System.out.println("l2String: " + l2String);
             System.out.println("sum: " + sum);
         }
 
-        head.val = Character.getNumericValue(sum.charAt(sum.length() - 1));
+        ListNode head = new ListNode(Character.getNumericValue(sum.charAt(0)));
         ListNode curr = head;
-        if (t) {
-            System.out.println("head: " + head.val);
-        }
-        if (sum.length() > 1) {
-            for (int i = sum.length() - 2; i >= 0; i--) {
-                ListNode newN = new ListNode(Character.getNumericValue(sum.charAt(i)));
-                curr.next = newN;
-                curr = curr.next;
-            }
+        for (int i = 1; i < sum.length(); i++) {
+            ListNode newNode = new ListNode(Character.getNumericValue(sum.charAt(i)));
+            curr.next = newNode;
+            curr = curr.next;
         }
 
         return head;
     }
 
-    private void update(ListNode l, StringBuilder s) {
-        ListNode curr = l;
+    private String computeSum(String firstString, String secondString) {
+        StringBuilder result = new StringBuilder();
+        int firstPointer = firstString.length() - 1;
+        int secondPointer = secondString.length() - 1;
+        int carryOver = 0;
+
+        while (firstPointer >= 0 || secondPointer >= 0) {
+            int firstStringDigit = firstPointer >= 0 ? Character.getNumericValue(firstString.charAt(firstPointer)) : 0;
+            int secondStringDigit = secondPointer >= 0 ? Character.getNumericValue(secondString.charAt(secondPointer)) : 0;
+            int sum = carryOver + firstStringDigit + secondStringDigit;
+            if (sum < 9) {
+                result.append(sum);
+                carryOver = 0;
+            } else {
+                result.append(sum % 10);
+                carryOver = sum / 10;
+            }
+            firstPointer--;
+            secondPointer--;
+        }
+
+        if (carryOver != 0) {
+            result.append(carryOver);
+        }
+        return result.toString();
+    }
+
+    private String formsString(ListNode list) {
+        StringBuilder number = new StringBuilder();
+        ListNode curr = list;
         while (curr != null) {
-            int v = curr.val;
-            s.append(Character.forDigit(v, 10));
+            number.append(curr.val);
             curr = curr.next;
         }
-        s = s.reverse();
+        return number.reverse().toString();
     }
 }
