c2d563c04abae714614ea18c7b43baacc54418f7
Umar <shumarb@outlook.com>
2025-02-03 02:19:14 +0800

    Find Target Indices After Sorting Array: Updated submission with comments to consolidate understanding

--- easy/java/FindTargetIndicesAfterSortingArray.java
+++ easy/java/FindTargetIndicesAfterSortingArray.java
@@ -8,9 +8,19 @@ class FindTargetIndicesAfterSortingArray {
         int countTarget = 0;
 
         for (int element: nums) {
+            /**
+             1. Count number of elements < target
+             to identify start index of target when nums is sorted
+             */
             if (element < target) {
                 startIndexOfTarget++;
             }
+
+            /**
+             2. Count number of times target is in nums
+             to know range of indices whose element is equal to target
+             from aforementioned start index
+             */
             if (element == target) {
                 countTarget++;
             }
@@ -21,6 +31,15 @@ class FindTargetIndicesAfterSortingArray {
         }
 
         while (countTarget != 0) {
+            /**
+             3. Assuming nums is sorted,
+             the target element is stored in a consecutive manner starting frmo startIndexOfTarget,
+             so add incrementally from startIndexOfTarget by number of times target appears.
+             Eg: [3 1 2 4 2], target = 2 ==> sorted: [1 2 2 3 4], target = 2.
+             - start index of target (2) in sorted nums: 1,
+             - number of times target appears is 2,
+             ==> target indices are 1 & 2 ==> result {1, 2}.
+             */
             result.add(startIndexOfTarget++);
             countTarget--;
         }
