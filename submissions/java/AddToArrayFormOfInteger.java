4208d83567fe5f00208d7124f9dc750bc6a44113
Umar <shumarb@outlook.com>
2025-02-05 15:06:26 +0800

    Add To Array Form Of Integer: Submission of updated solution with significantly-improved performance, added submissions to Math section

--- submissions/java/AddToArrayFormOfInteger.java
+++ submissions/java/AddToArrayFormOfInteger.java
@@ -2,30 +2,48 @@
 
 class AddToArrayFormOfInteger {
     public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
         boolean isTest = false;
         if (isTest) {
-            System.out.println("num: " + Arrays.toString(num) + " | k: " + k);
+            System.out.println("num: " + Arrays.toString(num) + ", k: " + k);
         }
 
-        int carryOver = 0;
+        int len = getBiggerLength(num.length, k);
         int i = num.length - 1;
-        List<Integer> result = new ArrayList<>();
-        while (i >= 0 || k > 0 || carryOver > 0) {
+        int carry = 0;
+        int[] sumOfNumAndK = new int[len + 1];
+        int j = len;
+
+        while (i >= 0 || k > 0 || carry > 0) {
             int numDigit = (i >= 0) ? num[i] : 0;
             int kDigit = k % 10;
-
-            int sum = numDigit + kDigit + carryOver;
-            carryOver = sum / 10;
-            result.add(0, sum % 10);
-
+            int sum = numDigit + kDigit + carry;
+            if (isTest) {
+                System.out.println("numDigit: " + numDigit + ", kDigit: " + kDigit + "\nsum: " + sum);
+            }
+            sumOfNumAndK[j--] = sum % 10;
+            carry = sum / 10;
             i--;
             k /= 10;
         }
 
+        int startIndex = (sumOfNumAndK[0] == 0) ? 1 : 0;
+        for (i = startIndex; i < len + 1; i++) {
+            result.add(sumOfNumAndK[i]);
+        }
         if (isTest) {
-            System.out.println("result: " + result);
+            System.out.println("sumOfNumAndK: " + Arrays.toString(sumOfNumAndK) + "\nresult: " + result);
         }
 
         return result;
+    }
+
+    private int getBiggerLength(int arrLength, int k) {
+        int numOfDigitsOfK = 0;
+        while (k != 0) {
+            numOfDigitsOfK++;
+            k /= 10;
+        }
+        return Math.max(arrLength, numOfDigitsOfK);
     }
 }
