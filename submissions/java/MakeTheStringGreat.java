f78bb1a133652e87cbe259ec263606053f778eb8
Umar <shumarb@outlook.com>
2025-02-09 19:05:44 +0800

    New submission: Make The String Great

---
+++ submissions/java/MakeTheStringGreat.java
@@ -0,0 +1,55 @@
+// Question: https://leetcode.com/problems/make-the-string-great/description/
+
+class MakeTheStringGreat {
+    public String makeGood(String s) {
+        StringBuilder result = new StringBuilder();
+        boolean isTest = false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char letter: s.toCharArray()) {
+            if (isTest) {
+                System.out.println("check: " + letter + " | stack so far: " + stack);
+            }
+            if (stack.isEmpty()) {
+                stack.push(letter);
+            } else {
+                if (isSameLetterInDifferentCases(stack.peek(), letter)) {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            }
+        }
+        if (isTest) {
+            System.out.println("final stack: " + stack);
+        }
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        result.reverse();
+        if (isTest) {
+            System.out.println("result: " + result);
+        }
+
+        return result.toString();
+    }
+
+    private boolean isSameLetterInDifferentCases(char firstLetter, char secondLetter) {
+        /**
+         A a ==> true
+         r R ==> true
+         i i ==> false
+         Q Q ==> false
+         */
+        if (firstLetter == secondLetter) {
+            return false;
+        }
+
+        if (Character.isUpperCase(firstLetter)) {
+            return secondLetter == Character.toLowerCase(firstLetter);
+        }
+
+        return secondLetter == Character.toUpperCase(firstLetter);
+    }
+}
