688dc3432f11406256627ee1dce31d5d145cf17e
Umar <shumarb@outlook.com>
2025-02-11 15:08:10 +0800

    New submission: Remove Duplicates from Sorted List

---
+++ submissions/java/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,26 @@
+// Question: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null) {
+            while (current.next != null && current.val == current.next.val) {
+                current.next = current.next.next;
+            }
+            current = current.next;
+        }
+
+        return head;
+    }
+}
