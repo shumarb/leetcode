d3ec26de327a1dc91ccc3d138c270b75089ebc1d
Umar <shumarb@outlook.com>
2025-02-17 23:25:39 +0800

    Sort Characters By Frequency: Updated solution and performance after submission

--- submissions/java/SortCharactersByFrequency.java
+++ submissions/java/SortCharactersByFrequency.java
@@ -3,33 +3,20 @@
 class SortCharactersByFrequency {
     public String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap<> ();
-        boolean t = false;
 
         for (char x: s.toCharArray()) {
             map.put(x, 1 + map.getOrDefault(x, 0));
         }
         List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
         list.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
-        if (t) {
-            System.out.println("s: " + s);
-            System.out.println("map: " + map);
-            System.out.println("list: " + list);
-        }
-
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<Character, Integer> entry: list) {
             char c = entry.getKey();
-            if (t) {
-                System.out.println(entry);
-                System.out.println("letter: " + c);
-            }
             for (int i = 0; i < entry.getValue(); i++) {
                 sb.append(c);
             }
         }
-        if (t) {
-            System.out.println("sb: " + sb.toString());
-        }
+
         return sb.toString();
     }
 }
