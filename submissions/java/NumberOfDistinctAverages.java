d2bc4c734570595aa425d63dab7e090c7ff68756
Umar <shumarb@outlook.com>
2025-02-19 15:35:09 +0800

    Number of Distinct Averages: Updated solution, with shorter code resulting in same performance

--- submissions/java/NumberOfDistinctAverages.java
+++ submissions/java/NumberOfDistinctAverages.java
@@ -5,100 +5,37 @@ class NumberOfDistinctAverages {
         Set<Double> averages = new HashSet<>();
         boolean isTest = false;
 
-        while (countNegatives(nums) != nums.length) {
+        if (isTest) {
+            System.out.println("before sort, nums: " + Arrays.toString(nums));
+        }
+        Arrays.sort(nums);
+        if (isTest) {
+            System.out.println("after sort, nums: " + Arrays.toString(nums));
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            double average = (nums[low] + nums[high]) / 2.0;
             if (isTest) {
-                System.out.println("nums so far: " + Arrays.toString(nums));
-            }
-            int maximum = getMaximum(nums);
-            int minimum = getMinimum(nums);
-            double average = (maximum + minimum) / 2.0;
-            if (isTest) {
-                System.out.println("maximum: " + maximum + " | minimum: " + minimum + " | average: " + average);
+                System.out.println("elements: " + nums[low] + " & " + nums[high] + " | average: " + average);
             }
             averages.add(average);
             if (isTest) {
                 System.out.println("averages so far: " + averages);
-                System.out.println("nums before removals: " + Arrays.toString(nums));
             }
-            remove(maximum, minimum, nums);
-            if (isTest) {
-                System.out.println("nums after removals: " + Arrays.toString(nums));
+
+            if (nums[low] == nums[high]) {
+                high--;
+            } else {
+                low++;
+                high--;
             }
         }
         if (isTest) {
-            System.out.println("averages final: " + averages);
+            System.out.println("averages: " + averages);
         }
 
         return averages.size();
-    }
-
-    private void remove(int maximum, int minimum, int[] nums) {
-        if (maximum == minimum) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == maximum) {
-                    nums[i] = -1;
-                    break;
-                }
-            }
-
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == maximum) {
-                    nums[i] = -1;
-                    break;
-                }
-            }
-
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == minimum) {
-                    nums[i] = -1;
-                    break;
-                }
-            }
-        }
-    }
-
-    private int getMaximum(int[] nums) {
-        int maximum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != -1) {
-                maximum = nums[i];
-                break;
-            }
-        }
-
-        for (int number: nums) {
-            if (number != -1) {
-                maximum = Math.max(maximum, number);
-            }
-        }
-        return maximum;
-    }
-
-    private int getMinimum(int[] nums) {
-        int minimum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != -1) {
-                minimum = nums[i];
-                break;
-            }
-        }
-
-        for (int number: nums) {
-            if (number != -1) {
-                minimum = Math.min(minimum, number);
-            }
-        }
-        return minimum;
-    }
-
-    private int countNegatives(int[] nums) {
-        int count = 0;
-        for (int number: nums) {
-            if (number == -1) {
-                count++;
-            }
-        }
-        return count;
     }
 }
