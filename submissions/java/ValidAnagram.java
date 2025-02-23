e843e119c358405268bdffc588122904c3070a53
Umar <shumarb@outlook.com>
2025-02-23 12:33:34 +0800

    Valid Anagram: Updated solution, resubmission resulted in improved performance

--- submissions/java/ValidAnagram.java
+++ submissions/java/ValidAnagram.java
@@ -1,16 +1,26 @@
+// Question: https://leetcode.com/problems/valid-anagram/description/
+
 class ValidAnagram {
     public boolean isAnagram(String s, String t) {
+        // 1. Invalid anagram for strings of different lengths.
         if (s.length() != t.length()) {
             return false;
         }
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-
-        return Arrays.equals(sArray, tArray);
+        /** 2.  Count letters in each string, and check if both are equal one another.
+         If both are equal to another, result is valid anagrams.
+         Otherwise, result is invalid anagrams.
+         */
+        int[] sLetterFrequency = countLettersInString(s);
+        int[] tLetterFrequency = countLettersInString(t);
+        return Arrays.equals(sLetterFrequency, tLetterFrequency);
     }
 
+    private int[] countLettersInString(String str) {
+        int[] letterFrequency = new int[26];
+        for (char letter: str.toCharArray()) {
+            letterFrequency[letter - 'a']++;
+        }
+        return letterFrequency;
+    }
 }
