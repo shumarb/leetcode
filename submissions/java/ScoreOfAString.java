1978156e5cbabb3fee8c36cc3a4c61d9d45bafbc
Umar <shumarb@outlook.com>
2025-02-24 16:16:24 +0800

    New submission: Score of a String

---
+++ submissions/java/ScoreOfAString.java
@@ -0,0 +1,12 @@
+// Question: https://leetcode.com/problems/score-of-a-string/description/
+
+class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs((s.charAt(i) - 'a') - (s.charAt(i + 1) - 'a'));
+        }
+
+        return score;
+    }
+}
