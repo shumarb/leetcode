2e2d07c5a8c196e6836ce26ad04da4e511744654
Umar <shumarb@outlook.com>
2025-02-24 23:02:50 +0800

    K Items With the Maximum Sum: Updated solution, resubmission resulted in improved performance and space complexity

--- submissions/java/KItemsWithTheMaximumSum.java
+++ submissions/java/KItemsWithTheMaximumSum.java
@@ -2,29 +2,35 @@
 
 class KItemsWithTheMaximumSum {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        List<Integer> elements = new ArrayList<>();
-        boolean isTest = false;
-        int maximumSum = 0;
-
-        populate(elements, -1, numNegOnes);
-        populate(elements, 0, numZeros);
-        populate(elements, 1, numOnes);
-        if (isTest) {
-            System.out.println("numNegOnes: " + numNegOnes + ", numZeros: " + numZeros + ", numOnes: " + numOnes + ", k: " + k);
-            System.out.println("elements: " + elements);
+        // 1. Edge case: k == 0
+        if (k == 0) {
+            return 0;
         }
 
-        int j = elements.size() - 1;
-        for (int i = 0; i < k; i++) {
-            maximumSum += elements.get(j--);
+        int maximumSum = 0;
+        for (int i = 0; i < numOnes; i++) {
+            maximumSum += 1;
+            k -= 1;
+            if (k == 0) {
+                return maximumSum;
+            }
+        }
+
+        for (int i = 0; i < numZeros; i++) {
+            k -= 1;
+            if (k == 0) {
+                return maximumSum;
+            }
+        }
+
+        for (int i = 0; i < numNegOnes; i++) {
+            maximumSum -= 1;
+            k -= 1;
+            if (k == 0) {
+                return maximumSum;
+            }
         }
 
         return maximumSum;
-    }
-
-    private void populate(List<Integer> elements, int number, int count) {
-        for (int i = 0; i < count; i++) {
-            elements.add(number);
-        }
     }
 }
