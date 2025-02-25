4a970ca907abaabbc2bdf69df6863131dfb72c0f
Umar <shumarb@outlook.com>
2025-02-25 12:59:12 +0800

    Min Stack: Updated comments, resubmission resulted in same performance

--- submissions/java/MinStack.java
+++ submissions/java/MinStack.java
@@ -10,10 +10,13 @@ class MinStack {
 
     public void push(int val) {
         /**
-         1.  If stack is empty, the first value in stack is the minimum.
-         2.  If stack is not empty, retrieve minimum value in stack
-         by checking minimum value after insertion of latest element
-         before current element.
+         1. The stack contains integer arrays of size 2:
+         - first element (at index 0) is val
+         - second element (at index 1) represents the minimum element in the stack after inserting val.
+
+         2.  If stack is empty, val is the first value to be inserted in the stack, so set it as the minimum.
+         3.  If stack is not empty, retrieve minimum value in stack (second element of top-most entry),
+         comparing it with val, followed by inserting val and minimum element in the stack so far.
          */
         minimum = stack.isEmpty() ? val : Math.min(stack.peek()[1], val);
         stack.push(new int[]{val, minimum});
