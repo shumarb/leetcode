d15bb75fc443401e8df765fea3e2a9fed64fd8a1
Umar <shumarb@outlook.com>
2025-02-26 12:41:40 +0800

    Number of Good Pairs: Refactored solution by following hint, resubmission resulted in improved performance

--- submissions/java/NumberOfGoodPairs.java
+++ submissions/java/NumberOfGoodPairs.java
@@ -2,14 +2,34 @@
 
 class NumberOfGoodPairs {
     public int numIdenticalPairs(int[] nums) {
+        int maximum = findMaximum(nums);
+        int n = nums.length;
+        int[] numberFrequency = new int[maximum + 1];
+        boolean isTest = false;
+
+        for (int element: nums) {
+            numberFrequency[element]++;
+        }
+        if (isTest) {
+            System.out.println("nums: " + Arrays.toString(nums) + ", numberFrequency: " + Arrays.toString(numberFrequency));
+        }
+
         int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+        for (int i = 1; i <= maximum; i++) {
+            int frequency = numberFrequency[i];
+            if (frequency > 1) {
+                count += frequency * (frequency - 1) / 2;
             }
         }
+
         return count;
+    }
+
+    private int findMaximum(int[] nums) {
+        int maximum = nums[0];
+        for (int element: nums) {
+            maximum = Math.max(maximum, element);
+        }
+        return maximum;
     }
 }
