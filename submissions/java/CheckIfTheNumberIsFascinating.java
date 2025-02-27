8ad1cf330b883677275e42e6cc622d25600fdf58
Umar <shumarb@outlook.com>
2025-02-28 02:27:21 +0800

    New submission: Refactored solution, resubmission resulted in improved performance

--- submissions/java/CheckIfTheNumberIsFascinating.java
+++ submissions/java/CheckIfTheNumberIsFascinating.java
@@ -2,17 +2,13 @@
 
 class CheckIfTheNumberIsFascinating {
     public boolean isFascinating(int n) {
-        boolean isTest = false;
         int[] digitFrequency = new int[10];
 
         for (int i = 1; i <= 3; i++) {
             int value = n * i;
-            String nStr = String.valueOf(value);
-            for (char digit: nStr.toCharArray()) {
-                digitFrequency[Character.getNumericValue(digit)]++;
-            }
-            if (isTest) {
-                System.out.println("originalN: " + value + ", nStr: " + nStr + " digitFrequency: " + Arrays.toString(digitFrequency));
+            while (value != 0) {
+                digitFrequency[value % 10]++;
+                value /= 10;
             }
         }
 
