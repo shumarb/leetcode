439c86afec66d2af75da070f1c49b98b02b1f5d2
Umar <shumarb@outlook.com>
2025-02-28 01:02:38 +0800

    Contains Duplicate: Refactored solution, resubmission resulted in improved performance

--- submissions/java/ContainsDuplicate.java
+++ submissions/java/ContainsDuplicate.java
@@ -4,10 +4,9 @@ class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<> ();
         for (int element: nums) {
-            if (set.contains(element)) {
+            if (!set.add(element)) {
                 return true;
             }
-            set.add(element);
         }
         return false;
     }
