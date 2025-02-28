30d356c75a7188aac85b59700f028fbb5c0b44a0
Umar <shumarb@outlook.com>
2025-03-01 01:27:44 +0800

    Detect Capital: Refactored solution, resubmission resulted in improved performance

--- submissions/java/DetectCapital.java
+++ submissions/java/DetectCapital.java
@@ -3,24 +3,28 @@
 class DetectCapital {
     public boolean detectCapitalUse(String word) {
         /** 1.  Count number of uppercase letters in word,
-                and use it to check if >= 1 of the conditions is fulfilled.
+         and use it to check if >= 1 of the conditions is fulfilled.
          */
         int countUpperCaseLetters = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isUpperCase(word.charAt(i))) {
+        int length = word.length();
+
+        for (int i = 0; i < length; i++) {
+            char letter = word.charAt(i);
+            if (letter >= 65 && letter <= 90) {
                 countUpperCaseLetters++;
             }
         }
 
         /**
-             2. Case 1: All letters in word are upper case.
-             3. Case 2: No letters in word are upper case.
+         2. Case 1: All letters in word are upper case.
+         3. Case 2: No letters in word are upper case.
          */
-        if (countUpperCaseLetters == word.length() || countUpperCaseLetters == 0) {
+        if (countUpperCaseLetters == length || countUpperCaseLetters == 0) {
             return true;
         }
 
         // 4. Case 4: Only 1 uppercase letter, which is the first letter.
-        return countUpperCaseLetters == 1 && Character.isUpperCase(word.charAt(0));
+        char firstLetter = word.charAt(0);
+        return countUpperCaseLetters == 1 && (firstLetter >= 65 && firstLetter <= 90);
     }
 }
