223162888a0ffd4169bfc98619f34b6ceb2d7211
Umar <shumarb@outlook.com>
2025-03-09 12:01:44 +0800

    Number of 1 Bits: Refactored solution, resubmission resulted in same performance

--- submissions/java/NumberOf1Bits.java
+++ submissions/java/NumberOf1Bits.java
@@ -2,13 +2,6 @@
 
 class NumberOf1Bits {
     public int hammingWeight(int n) {
-        int count = 0;
-        while (n != 0) {
-            if (n % 2 == 1) {
-                count++;
-            }
-            n /= 2;
-        }
-        return count;
+        return Integer.bitCount(n);
     }
 }
