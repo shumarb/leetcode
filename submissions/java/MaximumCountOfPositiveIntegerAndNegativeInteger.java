7f825aeb6d0f23e8e3b3faa637e2e04b04d528e6
Umar <shumarb@outlook.com>
2025-03-11 22:53:05 +0800

    Maximum Count of Positive Integer and Negative Integer: Refactored solution, resubmission resulted in same performance and improved space complexity

--- submissions/java/MaximumCountOfPositiveIntegerAndNegativeInteger.java
+++ submissions/java/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -1,65 +1,73 @@
 // Question: https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/description/
 
 class MaximumCountOfPositiveIntegerAndNegativeInteger {
-    private boolean t = false;
-
     public int maximumCount(int[] nums) {
-        int indexP = getFirstP(nums);
-        int indexN = getLastN(nums);
-        int countP = (indexP == -1 ? 0 : nums.length - indexP);
-        int countN = (indexN == -1 ? 0 : indexN + 1);
-        return Math.max(countP, countN);
+        boolean isTest = false;
+
+        int lastNegativeIndex = findLastNegativeIndex(nums);
+        int firstPositiveIndex = findFirstPositiveIndex(nums);
+        if (isTest) {
+            System.out.println("nums: " + Arrays.toString(nums));
+            System.out.println("last negative index: " + lastNegativeIndex);
+            System.out.println("first positive index: " + firstPositiveIndex);
+        }
+
+        // 1. Array comprises on non-positive values.
+        if (lastNegativeIndex == -1 && firstPositiveIndex != -1) {
+            return nums.length - firstPositiveIndex;
+
+            // 2. Array comprises of non-negative values.
+        } else if (firstPositiveIndex == -1 && lastNegativeIndex != -1) {
+            return lastNegativeIndex + 1;
+
+            // 3. Array comprises of zeroes.
+        } else if (lastNegativeIndex == -1 && firstPositiveIndex == -1) {
+            return 0;
+
+        } else {
+            /**
+             4.  Array comprises of positive and negative values, and may contain zeroes,
+             hence determine the number of negative elements (lastNegativeIndex + 1)
+             and the number of positive elements (nums.length - firstPostiveIndex),
+             and return the maximum out of those 2 values.
+             */
+            return Math.max(lastNegativeIndex + 1, nums.length - firstPositiveIndex);
+        }
     }
 
-    private int getLastN(int[] a) {
-        int index = -1;
+    private int findFirstPositiveIndex(int[] nums) {
+        // [-4, -3, -1, 0, 1, 3, 5, 7]
         int low = 0;
-        int high = a.length - 1;
+        int high = nums.length - 1;
+        int index = -1;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (t) {
-                System.out.println("indices: " + low + ", " + mid + ", " + high);
-                System.out.println("elements: " + a[low] + ", " + a[mid] + ", " + a[high]);
-            }
-            if (a[mid] >= 0) {
-                high = mid - 1;
-            } else {
-                // current element is negative but may not be last negative
-                // so take not of its index and search for negative element in indices [mid + 1, high]
-                index = mid;
+            if (nums[mid] <= 0) {
                 low = mid + 1;
+            } else {
+                index = mid;
+                high = mid - 1;
             }
         }
 
-        if (t) {
-            System.out.println("last negative index: " + index);
-        }
         return index;
     }
 
-    private int getFirstP(int[] a) {
+    private int findLastNegativeIndex(int[] nums) {
         int low = 0;
-        int high = a.length - 1;
+        int high = nums.length - 1;
         int index = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (t) {
-                System.out.println("indices: " + low + ", " + mid + ", " + high);
-                System.out.println("elements: " + a[low] + ", " + a[mid] + ", " + a[high]);
-            }
-            if (a[mid] <= 0) {
-                low = mid + 1;
-            } else {
-                // current element is positive but it's index may not be first positive
-                // so take note of index and search for first positive in index [low, mid - 1]
-                index = mid;
+            if (nums[mid] >= 0) {
                 high = mid - 1;
+            } else {
+                index = mid;
+                low = mid + 1;
             }
         }
 
-        if (t) {
-            System.out.println("first positive index: " + index);
-        }
         return index;
     }
 }
