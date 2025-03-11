54e29a6710152726885067b8ce7ffa36dc4ec5db
Umar <shumarb@outlook.com>
2025-03-11 23:13:12 +0800

    Maximum Count of Positive Integer and Negative Integer: Refactored solution, resubmission resulted in same performance

--- submissions/java/MaximumCountOfPositiveIntegerAndNegativeInteger.java
+++ submissions/java/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -4,70 +4,50 @@ class MaximumCountOfPositiveIntegerAndNegativeInteger {
     public int maximumCount(int[] nums) {
         boolean isTest = false;
 
-        int lastNegativeIndex = findLastNegativeIndex(nums);
-        int firstPositiveIndex = findFirstPositiveIndex(nums);
+        int countNegatives = countNegatives(nums);
+        int countPositives = countPositives(nums);
         if (isTest) {
             System.out.println("nums: " + Arrays.toString(nums));
-            System.out.println("last negative index: " + lastNegativeIndex);
-            System.out.println("first positive index: " + firstPositiveIndex);
+            System.out.println("number of negatives: " + countNegatives);
+            System.out.println("number of positives: " + countPositives);
         }
 
-        // 1. Array comprises on non-positive values.
-        if (lastNegativeIndex == -1 && firstPositiveIndex != -1) {
-            return nums.length - firstPositiveIndex;
-
-            // 2. Array comprises of non-negative values.
-        } else if (firstPositiveIndex == -1 && lastNegativeIndex != -1) {
-            return lastNegativeIndex + 1;
-
-            // 3. Array comprises of zeroes.
-        } else if (lastNegativeIndex == -1 && firstPositiveIndex == -1) {
-            return 0;
-
-        } else {
-            /**
-             4.  Array comprises of positive and negative values, and may contain zeroes,
-             hence determine the number of negative elements (lastNegativeIndex + 1)
-             and the number of positive elements (nums.length - firstPostiveIndex),
-             and return the maximum out of those 2 values.
-             */
-            return Math.max(lastNegativeIndex + 1, nums.length - firstPositiveIndex);
-        }
+        return Math.max(countPositives, countNegatives);
     }
 
-    private int findFirstPositiveIndex(int[] nums) {
-        // [-4, -3, -1, 0, 1, 3, 5, 7]
+    private int countPositives(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
-        int index = -1;
+        int count = 0;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] <= 0) {
                 low = mid + 1;
             } else {
-                index = mid;
+                count = nums.length - mid;
                 high = mid - 1;
             }
         }
 
-        return index;
+        return count;
     }
 
-    private int findLastNegativeIndex(int[] nums) {
+    private int countNegatives(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
-        int index = -1;
+        int count = 0;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] >= 0) {
                 high = mid - 1;
             } else {
-                index = mid;
+                count = mid + 1;
                 low = mid + 1;
             }
         }
 
-        return index;
+        return count;
     }
 }
