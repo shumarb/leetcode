0d0d3e83ba293bc9b1b685d974e1b79f162e635f
Umar <shumarb@outlook.com>
2025-03-12 23:25:11 +0800

    Employee Importance: Shortened code, resulting in same performance after resubmission

--- submissions/java/EmployeeImportance.java
+++ submissions/java/EmployeeImportance.java
@@ -11,43 +11,30 @@ class Employee {
 
 class EmployeeImportance {
     public int getImportance(List<Employee> employees, int id) {
-        int importance = 0;
-        Employee[] employeeArray = populate(employees);
-
-        Employee target = findEmployee(id, employeeArray);
-        Queue<Employee> queue = new LinkedList<>();
-        queue.offer(target);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Employee current = queue.poll();
-                importance += current.importance;
-                if (current.subordinates.size() > 0) {
-                    for (int subordinateId: current.subordinates) {
-                        Employee subordinate = findEmployee(subordinateId, employeeArray);
-                        queue.offer(subordinate);
-                    }
-                }
-            }
-        }
-
-        return importance;
-    }
-
-    private Employee findEmployee(int id, Employee[] employeeArray) {
-        return (employeeArray[id] == null) ? null : employeeArray[id];
-    }
-
-    private Employee[] populate(List<Employee> employees) {
         int maximumId = -1;
         for (Employee e: employees) {
             maximumId = Math.max(maximumId, e.id);
         }
+
         Employee[] employeeArray = new Employee[maximumId + 1];
         for (Employee e: employees) {
             employeeArray[e.id] = e;
         }
-        return employeeArray;
+
+        return dfs(employeeArray[id], employeeArray);
+    }
+
+    private int dfs(Employee current, Employee[] employeeArray) {
+        if (current == null) {
+            return 0;
+        }
+
+        int importance = current.importance;
+        for (int subordinateId: current.subordinates) {
+            Employee subordinate = employeeArray[subordinateId];
+            importance += dfs(subordinate, employeeArray);
+        }
+
+        return importance;
     }
 }
