40c3f14bc98709835769b7d707b0a5430a380172
Umar <shumarb@outlook.com>
2025-03-15 11:34:28 +0800

    Maximum Gap: Added check on difference between maximum and minimum so that algorithm is applied on array with at least 2 distinct values, resubmission resulted in same performance

--- submissions/java/MaximumGap.java
+++ submissions/java/MaximumGap.java
@@ -16,18 +16,23 @@ class MaximumGap {
             minimum = Math.min(number, minimum);
         }
 
-        // 2. Calculate bucket size & number of buckets.
+        // 2. No maximum gap if all elements are the same.
+        if (maximum == minimum) {
+            return 0;
+        }
+
+        // 3. Calculate bucket size & number of buckets.
         int bucketSize = Math.max(1, (maximum - minimum) / (nums.length - 1));
         int bucketCount = (maximum - minimum) / bucketSize + 1;
 
-        // 3. Create buckets to keep track of maximum & minimum values per bucket.
+        // 4. Create buckets to keep track of maximum & minimum values per bucket.
         int[] bucketMinimum = new int[bucketCount];
         int[] bucketMaximum = new int[bucketCount];
         Arrays.fill(bucketMinimum, Integer.MAX_VALUE);
         Arrays.fill(bucketMaximum, Integer.MIN_VALUE);
 
         /**
-         4.  Store each number into correct bucket,
+         5.  Store each number into correct bucket,
          and determine maximum and minimum values in bucket
          after inserting the number into the bucket.
          */
@@ -42,7 +47,7 @@ class MaximumGap {
             System.out.println("bucketMaximum: " + Arrays.toString(bucketMaximum));
         }
 
-        // 5. Find maximum gap by comparing previous bucket's maximum and current bucket's minimum.
+        // 6. Find maximum gap by comparing previous bucket's maximum and current bucket's minimum.
         maximumGap = 0;
         int previousMaximum = minimum;
         for (int i = 0; i < bucketCount; i++) {
