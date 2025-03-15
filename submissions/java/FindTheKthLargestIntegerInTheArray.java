ded919149998fab954dafeb1b4999bfc9c110112
Umar <shumarb@outlook.com>
2025-03-15 18:55:02 +0800

    Find the Kth Largest Integer in the Array: Used Arrays.sort instead of heap, improved runtime from 79ms to 16ms

--- submissions/java/FindTheKthLargestIntegerInTheArray.java
+++ submissions/java/FindTheKthLargestIntegerInTheArray.java
@@ -1,26 +1,16 @@
 // Question: https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/description/
 
-import java.math.BigInteger;
-
 class FindTheKthLargestIntegerInTheArray {
     public String kthLargestNumber(String[] nums, int k) {
-        PriorityQueue<BigInteger> minHeap = new PriorityQueue<>();
         boolean isTest = false;
-
-        for (int i = 0; i < k; i++) {
-            minHeap.offer(new BigInteger(nums[i]));
-        }
-        for (int i = k; i < nums.length; i++) {
-            BigInteger currentElement = new BigInteger(nums[i]);
-            if (currentElement.compareTo(minHeap.peek()) > 0) {
-                minHeap.poll();
-                minHeap.offer(currentElement);
-            }
-        }
         if (isTest) {
-            System.out.println("minHeap: " + minHeap + " | k: " + k + " | kth largest: " + String.valueOf(minHeap.peek()));
+            System.out.println("k: " + k + "\nbefore: " + Arrays.toString(nums));
+        }
+        Arrays.sort(nums, (a, b) -> Integer.compare(a.length(), b.length()) == 0 ? a.compareTo(b) : Integer.compare(a.length(), b.length()));
+        if (isTest) {
+            System.out.println("\nafter: " + Arrays.toString(nums));
         }
 
-        return minHeap.peek().toString();
+        return nums[nums.length - k];
     }
 }
