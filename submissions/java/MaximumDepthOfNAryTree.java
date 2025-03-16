dd8700fae85b4bbce7d2e3b29910f89522005bce
Umar <shumarb@outlook.com>
2025-03-17 04:37:05 +0800

    Maximum Depth of N-Ary Tree: Refactored solution, reduced runtime by 2ms

--- submissions/java/MaximumDepthOfNAryTree.java
+++ submissions/java/MaximumDepthOfNAryTree.java
@@ -21,28 +21,15 @@ class Node {
 
 class MaximumDepthOfNAryTree {
     public int maxDepth(Node root) {
-        // 1. Empty tree.
         if (root == null) {
             return 0;
         }
 
-        Queue<Node> queue = new LinkedList<>();
-        int depth = 0;
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            depth++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Node currentNode = queue.poll();
-                if (currentNode.children != null) {
-                    for (Node child: currentNode.children) {
-                        queue.offer(child);
-                    }
-                }
-            }
+        int maximumLevel = 0;
+        for (Node child: root.children) {
+            maximumLevel = Math.max(maximumLevel, maxDepth(child));
         }
 
-        return depth;
+        return maximumLevel + 1;
     }
 }
