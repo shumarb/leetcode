c7a828a47eafdb53b3b5d530982f159e4ef58f42
Umar <shumarb@outlook.com>
2025-03-19 12:10:26 +0800

    K-th Smallest Prime Fraction: Used custom Triple class, reducing runtime from 359ms to 344ms and improving rank from ~38% to ~44%

--- submissions/java/KthSmallestPrimeFaction.java
+++ submissions/java/KthSmallestPrimeFaction.java
@@ -2,26 +2,37 @@
 
 class KthSmallestPrimeFaction {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<double[]> maxHeap = new PriorityQueue<>(
-                (a, b) -> Double.compare(b[2], a[2])
+        PriorityQueue<Triple> maxHeap = new PriorityQueue<>(
+                (a, b) -> Double.compare(b.getValue(), a.getValue())
         );
-        boolean isTest = false;
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                maxHeap.offer(new double[] {arr[i], arr[j], (double) arr[i] / (double) arr[j]});
+                maxHeap.offer(new Triple(new int[] {arr[i], arr[j]}, (double) arr[i] / (double) arr[j]));
                 if (maxHeap.size() > k) {
                     maxHeap.poll();
                 }
             }
         }
-        if (isTest) {
-            System.out.println("max heap:");
-            for (double[] entry: maxHeap) {
-                System.out.println(" * n: " + entry[0] + ", d: " + entry[1] + ", value: " + entry[2]);
-            }
-        }
 
-        return new int[] {(int) maxHeap.peek()[0], (int) maxHeap.peek()[1]};
+        return maxHeap.peek().getPoint();
+    }
+}
+
+class Triple {
+    int[] point;
+    double value;
+
+    public Triple(int[] point, double value) {
+        this.point = point;
+        this.value = value;
+    }
+
+    public int[] getPoint() {
+        return point;
+    }
+
+    public double getValue() {
+        return value;
     }
 }
