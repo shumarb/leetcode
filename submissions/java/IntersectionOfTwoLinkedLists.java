48d269300edcb29dc31d8721663d08a0cc78f7cb
Umar <shumarb@outlook.com>
2025-03-20 15:52:00 +0800

    Intersection of Two Linked Lists: Used iterative check after adjusting head node (if required) instead of brute force, reducing runtime from 593ms to 1ms, and performance from 5.04% to 99.90%

--- submissions/java/IntersectionOfTwoLinkedLists.java
+++ submissions/java/IntersectionOfTwoLinkedLists.java
@@ -18,21 +18,62 @@ public class IntersectionOfTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         boolean isTest = false;
 
+        int countA = countNodes(headA);
+        int countB = countNodes(headB);
+
         if (isTest) {
+            System.out.println("countA: " + countA + "\ncountB: " + countB);
             display("A: ", headA);
             display("B: ", headB);
         }
 
-        ListNode current = headA;
-        while (current != null) {
-            check(current, headB);
-            if (isIntersectionFound) {
-                break;
+        if (countA == countB) {
+            findIntersection(headA, headB);
+        } else {
+            if (countA > countB) {
+                headA = adjustHead(headA, countA - countB);
+            } else {
+                headB = adjustHead(headB, countB - countA);
             }
-            current = current.next;
+            if (isTest) {
+                System.out.println("headA: " + headA.val + "\nheadB: " + headB.val);
+            }
+            findIntersection(headA, headB);
         }
 
         return intersectionNode;
+    }
+
+    private ListNode adjustHead(ListNode head, int count) {
+        ListNode current = head;
+        for (int i = 0; i < count; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private void findIntersection(ListNode headA, ListNode headB) {
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+        while (currentA != null && currentB != null) {
+            if (currentA == currentB) {
+                intersectionNode = currentA;
+                isIntersectionFound = true;
+                break;
+            }
+            currentA = currentA.next;
+            currentB = currentB.next;
+        }
+    }
+
+    private int countNodes(ListNode head) {
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
 
     private void check(ListNode check, ListNode head) {
