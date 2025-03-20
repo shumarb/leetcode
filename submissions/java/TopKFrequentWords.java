8f54a69e07303e4b736098ae721f82f5b7e7c7db
Umar <shumarb@outlook.com>
2025-03-20 23:22:29 +0800

    Top K Frequent Words: Refactored solution, resubmission resulted in same performance

--- submissions/java/TopKFrequentWords.java
+++ submissions/java/TopKFrequentWords.java
@@ -1,70 +1,54 @@
 // Question: https://leetcode.com/problems/top-k-frequent-words/description/
 
-package java;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-
 class TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
-        boolean t = false;
-        List<String> ans = new ArrayList<> ();
-        Map<String, Integer> map = new HashMap<>();
+        boolean isTest = false;
+        List<String> result = new ArrayList<> ();
+        Map<String, Integer> map = new HashMap<> ();
 
-        for (String w: words) {
-            map.put(w, 1 + map.getOrDefault(w, 0));
+        for (String word: words) {
+            map.put(word, 1 + map.getOrDefault(word, 0));
         }
-        if (t) {
+        if (isTest) {
             System.out.println("map: " + map);
         }
 
         /**
-         Given ordering: Decreasing value, Increasing key
-         Arranging of elements:
-         - Max heap: Prioritizes larger values first, and for equal values, prioritize smaller keys (lexicographically).
-         - Min heap: Prioritizes smaller values first, and for equal values, prioritize larger keys (lexicographically).
+         1.  Given ordering: Decreasing value, Increasing key.
+             Arranging of elements:
+             - Max heap: Prioritizes larger values first, and for equal values, prioritize lexigraphically smaller keys.
+             - Min heap: Prioritizes smaller values first, and for equal values, prioritize lexigraphically larger keys.
          */
         PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<>(
-                (a, b) -> a.getValue() == b.getValue()
+                (a, b) -> Integer.compare(a.getValue(), b.getValue()) == 0
                         ? b.getKey().compareTo(a.getKey())
-                        : a.getValue() - b.getValue()
+                        : Integer.compare(a.getValue(), b.getValue())
         );
-        for (Map.Entry<String, Integer> e: map.entrySet()) {
-            minHeap.offer(e);
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            minHeap.offer(entry);
             if (minHeap.size() > k) {
                 minHeap.poll();
             }
         }
-        if (t) {
+        if (isTest) {
             System.out.println("minHeap: " + minHeap);
         }
 
         /**
-         MinHeap ordering from root to leaves in increasing order of values:
-         kth, (k-1)th, (k-2)th,...2nd, 1st
-         so ans is ordered as kth, (k-1)th, (k-2)th,...2nd, 1st
-         hence reverse ans via collections.
+         2.  MinHeap ordering from root to leaves in increasing order of values:
+             kth, (k-1)th, (k-2)th,...2nd, 1st
+             so ans is ordered as kth, (k-1)th, (k-2)th,...2nd, 1st
+             hence reverse ans via collections.
 
-         eg: [9 2 4 5 7], k = 3
-         minHeap of size 3: [5, 7, 9], root is kth smallest
-         add root of minHeap until empty --> ans [5 7 9]
-         then reverse the order to get descending order --> [9 7 5].
+             eg: [9 2 4 5 7], k = 3
+             minHeap of size 3: [5, 7, 9], root is kth smallest
+             add root of minHeap until empty --> ans [5 7 9]
+             then reverse the order to get descending order --> [9 7 5].
          */
-        int i = k - 1;
         while (!minHeap.isEmpty()) {
-            ans.add(minHeap.poll().getKey());
+            result.add(0, minHeap.poll().getKey());
         }
-        if (t) {
-            System.out.println("before reverse: " + ans);
-        }
-        Collections.reverse(ans);
-        if (t) {
-            System.out.println("after reverse: " + ans);
-        }
-        return ans;
+
+        return result;
     }
 }
