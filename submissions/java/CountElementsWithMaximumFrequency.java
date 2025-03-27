3523dfa92d29d42ec16e67bef16fcf07a6161e2f
Umar <shumarb@outlook.com>
2025-03-27 09:06:47 +0800

    Count Elements With Maximum Frequency: Refactored solution for improved performance (runtime: 1ms --> 0ms; rank: 99.75% --> 100%

--- submissions/java/CountElementsWithMaximumFrequency.java
+++ submissions/java/CountElementsWithMaximumFrequency.java
@@ -2,30 +2,26 @@
 
 class CountElementsWithMaximumFrequency {
     public int maxFrequencyElements(int[] nums) {
-        int maximum = nums[0];
+        int countMaxFrequencyElements = 0;
+        int maxFrequency = 0;
+        int maximum = 0;
+
         for (int number: nums) {
             maximum = Math.max(maximum, number);
         }
-        int[] numberFrequency = new int[maximum + 1];
-        int countMaxFrequencyElements = 0;
-        int maxFrequency = Integer.MIN_VALUE;
-        boolean isTest = false;
 
+        int[] numberFrequency = new int[maximum + 1];
         for (int number: nums) {
             numberFrequency[number]++;
             maxFrequency = Math.max(maxFrequency, numberFrequency[number]);
         }
-        if (isTest) {
-            System.out.println("nums: " + Arrays.toString(nums) + "\nnumberFrequency: " + Arrays.toString(numberFrequency));
-            System.out.println("maxFrequency: " + maxFrequency);
-        }
 
-        for (int element: numberFrequency) {
-            if (element == maxFrequency) {
-                countMaxFrequencyElements++;
+        for (int frequency: numberFrequency) {
+            if (frequency == maxFrequency) {
+                countMaxFrequencyElements += frequency;
             }
         }
 
-        return maxFrequency * countMaxFrequencyElements;
+        return countMaxFrequencyElements;
     }
 }
