07afcd77c952558bc47db348abfcc596866f8967
Umar <shumarb@outlook.com>
2025-03-27 20:15:37 +0800

    Maximum Difference Between Increasing Elements: Updated minimum value and executed maximum difference check appropriately over using brute force for improved performance (runtime: 4ms --> 0ms; rank: ~39% --> 100%)

--- submissions/java/MaximumDifferenceBetweenIncreasingElements.java
+++ submissions/java/MaximumDifferenceBetweenIncreasingElements.java
@@ -3,9 +3,14 @@
 class MaximumDifferenceBetweenIncreasingElements {
     public int maximumDifference(int[] nums) {
         long maximumDifference = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                maximumDifference = Math.max(maximumDifference, (long) nums[j] - (long) nums[i]);
+        long minimum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            long current = nums[i];
+            if (minimum < current) {
+                maximumDifference = Math.max(maximumDifference, current - minimum);
+            }
+            if (current < minimum) {
+                minimum = current;
             }
         }
         return maximumDifference == 0 ? -1 : (int) maximumDifference;
