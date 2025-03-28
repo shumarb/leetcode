46a68116a0348b941a4c7462639f000d7c205042
Umar <shumarb@outlook.com>
2025-03-28 12:12:50 +0800

    Calculate Amount Paid in Taxes: Refactored solution, resubmission resulted in same performance

--- submissions/java/CalculateAmountPaidInTaxes.java
+++ submissions/java/CalculateAmountPaidInTaxes.java
@@ -3,12 +3,16 @@
 class CalculateAmountPaidInTaxes {
     public double calculateTax(int[][] brackets, int income) {
         double tax = 0;
-        int numBrackets = brackets.length;
+        double incomeValue = income;
+        double previousUpper = 0;
 
-        for (int i = 0; i < numBrackets && income > 0; i++) {
-            int amount = i == 0 ? brackets[i][0] : brackets[i][0] - brackets[i - 1][0];
-            tax += (double) brackets[i][1] / 100.0 * (double) Math.min(amount, income);
-            income -= amount;
+        for (int i = 0; i < brackets.length && incomeValue > 0; i++) {
+            double rate = (double) brackets[i][1] / 100.0;
+            double currentUpper = brackets[i][0];
+            double taxableAmount = currentUpper - previousUpper;
+            tax += Math.min(taxableAmount, incomeValue) * rate;
+            incomeValue -= taxableAmount;
+            previousUpper = currentUpper;
         }
 
         return tax;
