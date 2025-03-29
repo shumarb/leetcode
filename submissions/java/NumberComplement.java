7464004e9516d92c939fb88f3d4fd6d1b6ec1aed
Umar <shumarb@outlook.com>
2025-03-30 02:17:37 +0800

    Number Complement: Refactored solution, resubmission resulted in same performance and improved space complexity (41.93% --> 84.33%)

--- submissions/java/NumberComplement.java
+++ submissions/java/NumberComplement.java
@@ -2,28 +2,19 @@
 
 class NumberComplement {
     public int findComplement(int num) {
-        boolean isTest = false;
+        if (num == 0) {
+            return 1;
+        }
+
         double complement = 0;
         int power = 0;
 
         while (num != 0) {
-            int bit = num % 2;
-            int reverseBit = 0;
-            if (bit == 1) {
-                reverseBit = 0;
-            } else {
-                reverseBit = 1;
+            if (num % 2 == 0) {
+                complement += Math.pow(2, power);
             }
-
-            if (reverseBit == 1) {
-                complement += Math.pow(2.0, power);
-            }
-
             power++;
             num /= 2;
-        }
-        if (isTest) {
-            System.out.println("complement: " + complement);
         }
 
         return (int) complement;
