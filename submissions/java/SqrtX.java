5a16200823c77259be78220faded61ea39fc35c5
Umar <shumarb@outlook.com>
2025-04-02 13:42:31 +0800

    Sqrt(x): Re-attempted, resubmission resulted in same rank and decrease in rank

--- submissions/java/SqrtX.java
+++ submissions/java/SqrtX.java
@@ -6,30 +6,31 @@ class SqrtX {
             return x;
         }
 
-        // 1. Use of long to ensure no integer overflow
-        // during computation
+        // 1. Use of long to ensure no integer overflow during computation.
         long left = 1;
 
-        // 2. Sqrt(x) guaranteed to be <= (x / 2)
-        long right = (long) x / 2;
+        // 2. Sqrt(x) guaranteed to be <= (x / 2).
+        long right = x / 2;
 
-        // 3. Largest value < x such that (value * value) <= x
+        // 3. Largest value < x such that (value * value) <= x.
         long val = 0;
 
         while (left <= right) {
             long mid = left + (right - left) / 2;
-            // 1. Obtain exact answer of sqrt(x)
+            // 4. Obtain exact answer of sqrt(x).
             if (mid * mid == x) {
                 return (int) mid;
 
-                // 2. Obtain floor of sqrt(x)
-                // This could be largest value < x such that (value * value) <= x
-                // so take note if its value and check from [mid + 1, high]
+            /**
+                5.  Obtain floor of sqrt(x)
+                    This could be largest value < x such that (value * value) <= x
+                    so take note if its value and check from [mid + 1, high].
+            */
             } else if (mid * mid < x) {
                 val = mid;
                 left = mid + 1;
 
-                // 3. mid > sqrt(x), so exclude all values from right onwards
+                // 6. mid > sqrt(x), so exclude all values from right onwards.
             } else {
                 right = mid - 1;
             }
