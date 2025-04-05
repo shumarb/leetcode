1c82b54a8f3b92e7ca915549c3949e4791ed1573
Umar <shumarb@outlook.com>
2025-04-05 19:48:55 +0800

    Summary Ranges: Refactored solution and reattempted to reinforce understanding, resubmission resulted in same performance

--- submissions/java/SummaryRanges.java
+++ submissions/java/SummaryRanges.java
@@ -2,22 +2,13 @@
 
 class SummaryRanges {
     public List<String> summaryRanges(int[] nums) {
-        // 1. Edge case: no numbers.
-        if (nums.length == 0) {
-            return new ArrayList<>();
-        }
-
         List<String> result = new ArrayList<>();
-        boolean isTest = false;
         int i = 0;
+        int len = nums.length;
 
-        while (i < nums.length) {
+        while (i < len) {
             int start = nums[i];
-            if (isTest) {
-                System.out.println("i: " + i + ", start: " + nums[i]);
-            }
-
-            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+            while (i + 1 < len && nums[i + 1] == 1 + nums[i]) {
                 i++;
             }
             if (start == nums[i]) {
@@ -25,15 +16,7 @@ class SummaryRanges {
             } else {
                 result.add(start + "->" + nums[i]);
             }
-            if (isTest) {
-                System.out.println(" * result so far: " + result);
-                System.out.println("--------------------------------");
-            }
-
             i++;
-        }
-        if (isTest) {
-            System.out.println("result: " + result);
         }
 
         return result;
