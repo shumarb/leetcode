2556de41ffe2d2ea82bcc5a73e5a78db582f9391
Umar <shumarb@outlook.com>
2025-04-05 10:52:55 +0800

    Decrypt String from Alphabet to Integer Mapping: Changed iteration from end-to-start to start-to-end for reduce code, resubmission resulted in same performance and improved space complexity (42MB --> 41.3MB)

--- submissions/java/DecryptStringFromAlphabetToIntegerMapping.java
+++ submissions/java/DecryptStringFromAlphabetToIntegerMapping.java
@@ -4,38 +4,29 @@ class DecryptStringFromAlphabetToIntegerMapping {
     public String freqAlphabets(String s) {
         StringBuilder result = new StringBuilder();
         boolean isTest = false;
-        int i = s.length() - 1;
+        int i = 0;
 
-        while (i >= 0) {
+        while (i < s.length()) {
             if (isTest) {
-                System.out.println("------------------");
-                System.out.println(" * i: " + i);
-                System.out.println(" * before, result: " + result.toString());
+                System.out.println("i: " + i + "\nbefore | result: " + result.toString());
             }
-
-            if (i - 2 >= 0 && s.charAt(i) == '#') {
-                int number = Integer.parseInt(s.substring(i - 2, i));
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                int number = Integer.parseInt(s.substring(i, i + 2));
                 result.append((char) ('a' + number - 1));
-                i -= 3;
-                if (isTest) {
-                    System.out.println(" * found # at index " + i);
-                    System.out.println(" * number: " + number);
-                }
+                i += 3;
             } else {
                 result.append((char) ('a' + s.charAt(i) - '1'));
-                i--;
+                i++;
             }
-
             if (isTest) {
-                System.out.println(" * after, result: " + result.toString());
+                System.out.println("after | result: " + result.toString());
+                System.out.println("------------------------------------------");
             }
         }
-
         if (isTest) {
-            System.out.println("------------------");
             System.out.println("s: " + s + ", result: " + result.toString());
         }
 
-        return result.reverse().toString();
+        return result.toString();
     }
 }
