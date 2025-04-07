1e27ddf05edbea36234db7e819a42bb84f887cc2
Umar <shumarb@outlook.com>
2025-04-07 18:49:30 +0800

    Merge Intervals: Used Integer.compare() instead of integer substraction for improved performance (runtime: 8ms --> 7ms, rank: 86.80% --> 97.71%

--- submissions/java/MergeIntervals.java
+++ submissions/java/MergeIntervals.java
@@ -2,18 +2,17 @@
 
 class MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
+        result.add(intervals[0]);
         int[] current = intervals[0];
-        result.add(current);
-
         for (int[] interval: intervals) {
             if (interval[0] <= current[1]) {
                 current[1] = Math.max(current[1], interval[1]);
             } else {
-                current = interval;
                 result.add(interval);
+                current = interval;
             }
         }
 
