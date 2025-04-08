a7d0bad947131958d1b5838625f85ac77e752920
Umar <shumarb@outlook.com>
2025-04-09 01:29:30 +0800

    Element Appearing More Than 25% In Sorted Array: Used iterative count instead of frequency array, resubmission resulted in same performance and improved space complexity (O(N) --> O(1); 28.42% --> 87.58%)

--- submissions/java/ElementAppearingMoreThan25PercentInSortedArray.java
+++ submissions/java/ElementAppearingMoreThan25PercentInSortedArray.java
@@ -2,13 +2,18 @@
 
 class ElementAppearingMoreThan25PercentInSortedArray {
     public int findSpecialInteger(int[] arr) {
-        int maximum = arr[arr.length - 1];
-        int[] numberFrequency = new int[maximum + 1];
-        for (int number: arr) {
-            numberFrequency[number]++;
-            if (numberFrequency[number] > arr.length / 4) {
-                return number;
+        int len = arr.length;
+        int start = 0;
+        while (start < len) {
+            int count = 1;
+            int end = start + 1;
+            while (end < len && arr[end] == arr[start]) {
+                end++;
             }
+            if ((end - start) > len / 4) {
+                return arr[start];
+            }
+            start = end;
         }
         return -1;
     }
