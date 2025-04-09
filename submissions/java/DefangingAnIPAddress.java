27348d43f7de68d429e2af938ab25bd97f38edfb
Umar <shumarb@outlook.com>
2025-04-09 13:06:49 +0800

    Defanging an IP Address: Used String API replace method instead of StringBuilder and iterating all characters for same performance and improved space complexity (34.62% --> 61.09%)

--- submissions/java/DefangingAnIPAddress.java
+++ submissions/java/DefangingAnIPAddress.java
@@ -2,16 +2,6 @@
 
 class DefangingAnIPAddress {
     public String defangIPaddr(String address) {
-        StringBuilder result = new StringBuilder();
-
-        for (char c: address.toCharArray()) {
-            if (c == '.') {
-                result.append("[.]");
-            } else {
-                result.append(c);
-            }
-        }
-
-        return result.toString();
+        return address.replace(".", "[.]");
     }
 }
