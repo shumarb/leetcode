79a8f8a41e3ad9781989ffd51fa8655187617296
Umar <shumarb@outlook.com>
2025-04-11 00:40:17 +0800

    Jump Game: Re-attempt to reinforce understanding, resubmission resulted in same performance

--- submissions/java/JumpGame.java
+++ submissions/java/JumpGame.java
@@ -4,11 +4,17 @@ class JumpGame {
     public boolean canJump(int[] nums) {
         int maximumIndex = 0;
         for (int i = 0; i < nums.length; i++) {
+            /**
+                 1.  Current index can't be reached
+                     and current index is not last element.
+             */
             if (i > maximumIndex) {
                 return false;
             }
+
             maximumIndex = Math.max(maximumIndex, i + nums[i]);
         }
+
         return true;
     }
 }
