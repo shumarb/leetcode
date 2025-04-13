59a828871e5f2e57ddc2176d6f0be1a24963b8aa
Umar <shumarb@outlook.com>
2025-04-13 22:53:09 +0800

    Distant Barcodes: Used max heap over manual check of frequency array for most frequent element different from last selected element for improved performance (Runtime: 1529ms --> 16ms; Rank: 5.76% --> 81.02%

--- submissions/java/DistantBarcodes.java
+++ submissions/java/DistantBarcodes.java
@@ -1,14 +1,18 @@
 // Question: https://leetcode.com/problems/distant-barcodes/description/
 
-class DistantBarcodes {
+class DistantBarcodes class Solution {
     public int[] rearrangeBarcodes(int[] barcodes) {
+        // 1. Edge case: Array with single element, so return array.
         if (barcodes.length == 1) {
             return barcodes;
         }
 
-        boolean isTest = false;
+        /**
+         2.  Count frequency of each element,
+             and populate max heap of elements
+             ordered in descending order of frequency.
+         */
         int maximum = barcodes[0];
-
         for (int number: barcodes) {
             maximum = Math.max(maximum, number);
         }
@@ -17,29 +21,49 @@ class DistantBarcodes {
             numberFrequency[number]++;
         }
 
-        int current = getMostFrequentElement(numberFrequency, -1);
-        barcodes[0] = current;
-        numberFrequency[current]--;
-        for (int i = 1; i < barcodes.length; i++) {
-            current = getMostFrequentElement(numberFrequency, barcodes[i - 1]);
-            barcodes[i] = current;
-            numberFrequency[current]--;
-        }
-
-        return barcodes;
-    }
-
-    private int getMostFrequentElement(int[] numberFrequency, int banned) {
-        int maximumFrequency = 0;
-        int candidate = -1;
-
-        for (int i = 0; i < numberFrequency.length; i++) {
-            if (i != banned && numberFrequency[i] > maximumFrequency) {
-                maximumFrequency = numberFrequency[i];
-                candidate = i;
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> Integer.compare(b[1], a[1])
+        );
+        for (int i = 1; i <= maximum; i++) {
+            if (numberFrequency[i] > 0) {
+                maxHeap.offer(new int[] {i, numberFrequency[i]});
             }
         }
 
-        return candidate;
+        /**
+             3.  While element has at least 2 elements,
+                 obtain first 2, and fill next 2 elements of barcodes
+                 as one of each. This ensures all adjacent pairs
+                 contain unique numbers.
+
+                 Then decrement frequency of each number used by 1,
+                 and if its remainding frequency > 1, add into the heap:
+                 [element, (element frequency - 1)].
+         */
+        int index = 0;
+        while (maxHeap.size() > 1) {
+            int[] first = maxHeap.poll();
+            int[] second = maxHeap.poll();
+            barcodes[index++] = first[0];
+            barcodes[index++] = second[0];
+            first[1]--;
+            second[1]--;
+            if (first[1] > 0) {
+                maxHeap.offer(new int[] {first[0], first[1]});
+            }
+            if (second[1] > 0) {
+                maxHeap.offer(new int[] {second[0], second[1]});
+            }
+        }
+
+        /**
+         4.  Edge case: Heap contains single element,
+             so set last element as this element.
+         */
+        if (!maxHeap.isEmpty()) {
+            barcodes[index] = maxHeap.poll()[0];
+        }
+
+        return barcodes;
     }
 }
