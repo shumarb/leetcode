0545ca78c7998a0ac26ab25335bc1b0ecec638b4
Umar <shumarb@outlook.com>
2025-04-15 13:05:01 +0800

    Minimum Operations to Make Columns Strictly Increasing: Used regular traversal and computing absolute difference to count increment instead of creating new list of values of columns and manually checking it for improved performance (Runtime: 14ms --> 1ms; Rank: 4.88% --> 100%)

--- submissions/java/MinimumOperationsToMakeColumnsStrictlyIncreasing.java
+++ submissions/java/MinimumOperationsToMakeColumnsStrictlyIncreasing.java
@@ -2,32 +2,16 @@
 
 class MinimumOperationsToMakeColumnsStrictlyIncreasing {
     public int minimumOperations(int[][] grid) {
-        boolean isTest = false;
-        int count = 0;
-        int numRow = grid.length;
-        int numColumn = grid[0].length;
-
-        for (int i = 0; i < numColumn; i++) {
-            List<Integer> column = new ArrayList<>();
-            for (int j = 0; j < numRow; j++) {
-                column.add(grid[j][i]);
-            }
-            if (isTest) {
-                System.out.println("column: " + column);
-            }
-            for (int k = 1; k < column.size(); k++) {
-                int current = column.get(k);
-                int previous = column.get(k - 1);
-                while (current <= previous) {
-                    if (isTest) {
-                        System.out.println(" * current: " + current + ", previous: " + previous);
-                    }
-                    column.set(k, current + 1);
-                    current = current + 1;
-                    count++;
+        int countOperations = 0;
+        for (int j = 0; j < grid[0].length; j++) {
+            for (int i = 1; i < grid.length; i++) {
+                if (grid[i][j] <= grid[i - 1][j]) {
+                    int countIncrement = 1 + Math.abs(grid[i][j] - grid[i - 1][j]);
+                    grid[i][j] += countIncrement;
+                    countOperations += countIncrement;
                 }
             }
         }
-        return count;
+        return countOperations;
     }
 }
