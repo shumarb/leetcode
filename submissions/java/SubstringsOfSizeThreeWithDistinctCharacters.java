3951a0059fd99b029d8e39e5bccee89264b495a9
Umar <shumarb@outlook.com>
2025-04-23 00:25:49 +0800

    Substrings of Size Three with Distinct Characters: Implemented iterative solution without sliding windows for same performance and reduced lines of code (30+ --> 18)

--- submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -2,37 +2,17 @@
 
 class SubstringsOfSizeThreeWithDistinctCharacters {
     public int countGoodSubstrings(String s) {
-        if (s.length() < 3) {
-            return 0;
-        }
-
         int countGoodSubstrings = 0;
-        int[] letterFrequency = new int[26];
-        for (int i = 0; i < 3; i++) {
-            letterFrequency[s.charAt(i) - 'a']++;
-        }
-        if (isOnlyThreeDistinctLetters(letterFrequency)) {
-            countGoodSubstrings++;
-        }
 
-        for (int i = 3; i < s.length(); i++) {
-            letterFrequency[s.charAt(i - 3) - 'a']--;
-            letterFrequency[s.charAt(i) - 'a']++;
-            if (isOnlyThreeDistinctLetters(letterFrequency)) {
+        for (int i = 0; i <= s.length() - 3; i++) {
+            char firstLetter = s.charAt(i);
+            char secondLetter = s.charAt(i + 1);
+            char thirdLetter = s.charAt(i + 2);
+            if (firstLetter != secondLetter && firstLetter != thirdLetter && secondLetter != thirdLetter) {
                 countGoodSubstrings++;
             }
         }
 
         return countGoodSubstrings;
-    }
-
-    private boolean isOnlyThreeDistinctLetters(int[] letterFrequency) {
-        int count = 0;
-        for (int i = 0; i < letterFrequency.length; i++) {
-            if (letterFrequency[i] == 1) {
-                count++;
-            }
-        }
-        return count == 3;
     }
 }
