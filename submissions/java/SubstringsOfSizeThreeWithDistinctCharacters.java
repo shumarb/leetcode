ec5f4165726e287fd2e0e8839d3c6d74e329a184
Umar <shumarb@outlook.com>
2025-04-24 02:09:13 +0800

    Substrings of Size Three with Distinct Characters: Refactored solution, resulting in same performance

--- submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -3,16 +3,11 @@
 class SubstringsOfSizeThreeWithDistinctCharacters {
     public int countGoodSubstrings(String s) {
         int countGoodSubstrings = 0;
-
         for (int i = 0; i <= s.length() - 3; i++) {
-            char firstLetter = s.charAt(i);
-            char secondLetter = s.charAt(i + 1);
-            char thirdLetter = s.charAt(i + 2);
-            if (firstLetter != secondLetter && firstLetter != thirdLetter && secondLetter != thirdLetter) {
+            if (s.charAt(i) != s.charAt(i + 1) && s.charAt(i) != s.charAt(i + 2) && s.charAt(i + 1) != s.charAt(i + 2)) {
                 countGoodSubstrings++;
             }
         }
-
         return countGoodSubstrings;
     }
 }
