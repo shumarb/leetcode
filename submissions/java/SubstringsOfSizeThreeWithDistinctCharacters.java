34f2c85d6de72cfdd64b93a90dd4c394833f46b0
Umar <shumarb@outlook.com>
2025-04-24 02:50:55 +0800

    Substrings of Size Three with Distinct Characters: Stored characters in variables for comparison, resulting in improved performance (Runtime: 1ms --> 0ms; Rank: 92.26% --> 100%)

--- submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ submissions/java/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -4,7 +4,10 @@ class SubstringsOfSizeThreeWithDistinctCharacters {
     public int countGoodSubstrings(String s) {
         int countGoodSubstrings = 0;
         for (int i = 0; i <= s.length() - 3; i++) {
-            if (s.charAt(i) != s.charAt(i + 1) && s.charAt(i) != s.charAt(i + 2) && s.charAt(i + 1) != s.charAt(i + 2)) {
+            int first = s.charAt(i);
+            int second = s.charAt(i + 1);
+            int third = s.charAt(i + 2);
+            if (first != second && first != third && second != third) {
                 countGoodSubstrings++;
             }
         }
