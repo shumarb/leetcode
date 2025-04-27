489186eb834ac31bbb35fd9de90b0ecf4d86b191
Umar <shumarb@outlook.com>
2025-04-28 00:41:20 +0800

    Shortest Distance to a Character: Updated previous values for left and right passes to reduce iterations by 2 for same performance

--- submissions/java/ShortestDistanceToACharacter.java
+++ submissions/java/ShortestDistanceToACharacter.java
@@ -4,28 +4,17 @@ class ShortestDistanceToACharacter {
     public int[] shortestToChar(String s, char c) {
         boolean isTest = false;
         int len = s.length();
-        int previous = -1;
+        int previous = Integer.MIN_VALUE / 2;
         int[] result = new int[len];
 
         for (int i = 0; i < len; i++) {
             if (s.charAt(i) == c) {
                 previous = i;
-                break;
             }
-        }
-        for (int i = 0; i < len; i++) {
-            if (s.charAt(i) == c) {
-                previous = i;
-            }
-            result[i] = Math.abs(i - previous);
+            result[i] = i - previous;
         }
 
-        for (int i = len - 1; i >= 0; i--) {
-            if (s.charAt(i) == c) {
-                previous = i;
-                break;
-            }
-        }
+        previous = Integer.MAX_VALUE / 2;
         for (int i = len - 1; i >= 0; i--) {
             if (s.charAt(i) == c) {
                 previous = i;
