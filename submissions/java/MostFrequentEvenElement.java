ac93cc6664323f0b3444bebdd4ac717d56ac4ce5
Umar <shumarb@outlook.com>
2025-04-28 02:23:20 +0800

    Most Frequent Even Element: Used frequency array and single-pass algorithm for improved performance (Runtime: 12ms --> 6ms; Rank: 93.70% --> 99.88%)

--- submissions/java/MostFrequentEvenElement.java
+++ submissions/java/MostFrequentEvenElement.java
@@ -1,52 +1,23 @@
 // Question: https://leetcode.com/problems/most-frequent-even-element/description/
 
-import java.util.Arrays;
-
 class MostFrequentEvenElement {
     public int mostFrequentEven(int[] nums) {
-        boolean isEvenExist = false;
-        for (int x: nums) {
-            if (x % 2 == 0) {
-                isEvenExist = true;
-                break;
+        int frequency = 0;
+        int mostFrequentEvenNumber = -1;
+
+        int[] evenNumberFrequency = new int[100001];
+        for (int number: nums) {
+            if (number % 2 == 0) {
+                evenNumberFrequency[number]++;
+                if (evenNumberFrequency[number] > frequency) {
+                    mostFrequentEvenNumber = number;
+                    frequency = evenNumberFrequency[number];
+                } else if (evenNumberFrequency[number] == frequency) {
+                    mostFrequentEvenNumber = Math.min(mostFrequentEvenNumber, number);
+                }
             }
         }
-        if (!isEvenExist) {
-            return -1;
-        }
 
-        boolean t = false;
-        int max = getMax(nums);
-        if (t) {
-            System.out.println("nums: " + Arrays.toString(nums) + "\nmax: " + max);
-        }
-        int[] freq = new int[max + 1];
-        for (int x: nums) {
-            freq[x]++;
-        }
-        if (t) {
-            System.out.println("freq: " + Arrays.toString(freq));
-        }
-
-        int smallest = 0;
-        int count = freq[0];
-        for (int i = 2; i < freq.length; i += 2) {
-            if (freq[i] > count) {
-                smallest = i;
-                count = freq[i];
-            }
-            if (freq[i] == count) {
-                smallest = Math.min(i, smallest);
-            }
-        }
-        return smallest;
-    }
-
-    private int getMax(int[] n) {
-        int max = n[0];
-        for (int x: n) {
-            max = Math.max(max, x);
-        }
-        return max;
+        return mostFrequentEvenNumber == -1 ? -1 : mostFrequentEvenNumber;
     }
 }
