007e22c9d3823136bfe5282d0a552604ffa53db5
Umar <shumarb@outlook.com>
2025-05-07 16:32:05 +0800

    Two Sum: Updated performance after resubmission

--- submissions/java/TwoSum.java
+++ submissions/java/TwoSum.java
@@ -3,7 +3,8 @@
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
                 return new int[] {i, map.get(complement)};
