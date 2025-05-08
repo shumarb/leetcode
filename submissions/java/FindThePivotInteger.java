5e97edca71efce605ff36f8535b5b15153c510fc
Umar <shumarb@outlook.com>
2025-05-09 00:09:10 +0800

    Find the Pivot Integer: Used left sum right sum checks in single iteration for improved performance (Runtime: 2ms --> 1ms; Rank: 25.08% --> 70.60%)

--- submissions/java/FindThePivotInteger.java
+++ submissions/java/FindThePivotInteger.java
@@ -2,37 +2,15 @@
 
 class FindThePivotInteger {
     public int pivotInteger(int n) {
-        boolean isTest = false;
-        int[] arr = new int[n];
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        if (isTest) {
-            System.out.println("arr: " + Arrays.toString(arr));
-        }
-
-        prefix[0] = arr[0];
-        suffix[n - 1] = arr[n - 1];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + arr[i];
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + arr[i];
-        }
-        if (isTest) {
-            System.out.println("prefix: " + Arrays.toString(prefix));
-            System.out.println("suffix: " + Arrays.toString(suffix));
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (prefix[i] == suffix[i]) {
-                return arr[i];
+        int total = (n * (n + 1)) / 2;
+        int leftSum = 0;
+        for (int x = 1; x <= n; x++) {
+            leftSum += x;
+            int rightSum = total - leftSum + x;
+            if (leftSum == rightSum) {
+                return x;
             }
         }
-
         return -1;
     }
 }
