1c2c09f609b784adcd78435724b53295b3cc466f
Umar <shumarb@outlook.com>
2025-05-10 21:16:40 +0800

    Number of Equivalent Domino Pairs: Used frequency array over hash map for improved performance (Runtime: 11ms --> 3ms; Rank: 51.92% --> 82.93%)

--- submissions/java/NumberOfEquivalentDominoPairs.java
+++ submissions/java/NumberOfEquivalentDominoPairs.java
@@ -2,33 +2,19 @@
 
 class NumberOfEquivalentDominoPairs {
     public int numEquivDominoPairs(int[][] dominoes) {
-        Map<Integer, Integer> map = new HashMap<>();
-        boolean isTest = false;
         int count = 0;
+        int[] numberFrequency = new int[100];
 
         for (int[] entry: dominoes) {
             int first = entry[0];
             int second = entry[1];
             int key = Math.min(first, second) * 10 + Math.max(first, second);
-            map.put(key, 1 + map.getOrDefault(key, 0));
+            numberFrequency[key]++;
         }
-
-        if (isTest) {
-            System.out.println("dominoes:");
-            for (int[] entry: dominoes) {
-                System.out.println(" * " + Arrays.toString(entry));
-            }
-            System.out.println("---------------------------------------------------------------------------------");
-            System.out.println("map:");
-            for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-                System.out.println(" * " + entry.getKey() + " --> " + entry.getValue());
-            }
-        }
-
-        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            int value = entry.getValue();
+        for (int i = 10; i <= 99; i++) {
+            int value = numberFrequency[i];
             if (value > 1) {
-                count += value * (value - 1) / 2;
+                count += (value * (value - 1) / 2);
             }
         }
 
