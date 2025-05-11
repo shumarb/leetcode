b8eac44a8ea069fcddfa2c359366b60890dc8222
Umar <shumarb@outlook.com>
2025-05-11 12:42:22 +0800

    3Sum: Removed isTest and display of array for same perforamnce

--- submissions/java/ThreeSum.java
+++ submissions/java/ThreeSum.java
@@ -3,14 +3,9 @@
 class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        boolean isTest = false;
         int n = nums.length;
 
         Arrays.sort(nums);
-        if (isTest) {
-            System.out.println("sorted nums: " + Arrays.toString(nums));
-        }
-
         for (int i = 0; i < n - 2; i++) {
             // 1. Skip duplicates.
             if (i > 0 && nums[i] == nums[i - 1]) {
@@ -25,6 +20,7 @@ class ThreeSum {
                 int sum = nums[j] + nums[k];
                 if (sum == target) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
                     // 2. Skip duplicates.
                     while (j < k && nums[j] == nums[j + 1]) {
                         j++;
