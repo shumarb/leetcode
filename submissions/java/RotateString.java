ae2a79e304067173a7bea97e177fa42a9c363f66
Umar <shumarb@outlook.com>
2025-05-12 14:05:07 +0800

    Rotate String: Refactored solution; resubmission resulted in same performance

--- submissions/java/RotateString.java
+++ submissions/java/RotateString.java
@@ -2,35 +2,34 @@
 
 class RotateString {
     public boolean rotateString(String s, String goal) {
-        int k = goal.length();
+        int goalLen = goal.length();
         int sLen = s.length();
 
         /**
          1.  Edge case: Goal and s are different length,
              so it is impossible to construct goal from s.
          */
-        if (k != sLen) {
+        if (sLen != goalLen) {
             return false;
         }
 
-        StringBuilder doubledS = new StringBuilder();
+        StringBuilder sDoubled = new StringBuilder();
         boolean isTest = false;
 
-        for (char letter: s.toCharArray()) {
-            doubledS.append(letter);
-        }
-        for (char letter: s.toCharArray()) {
-            doubledS.append(letter);
+        for (int i = 0; i < 2; i++) {
+            for (char letter: s.toCharArray()) {
+                sDoubled.append(letter);
+            }
         }
         if (isTest) {
-            System.out.println("s: " + s + "\ngoal: " + goal + "\ndoubledS: " + doubledS);
+            System.out.println("s: " + s + "\ngoal: " + goal + "\nsDoubled: " + sDoubled);
             System.out.println("-----------------------------------------------------");
         }
 
-        int len = doubledS.length();
-        String doubledSStr = doubledS.toString();
-        for (int i = 0; i <= len - k; i++) {
-            if (doubledSStr.substring(i, i + k).equals(goal)) {
+        int len = sDoubled.length();
+        String sDoubledString = sDoubled.toString();
+        for (int i = 0; i <= len - goalLen; i++) {
+            if (sDoubledString.substring(i, i + goalLen).equals(goal)) {
                 return true;
             }
         }
