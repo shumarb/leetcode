d6a127ca4aebed8d6ebbc82b49cd813ed2cec0a3
Umar <shumarb@outlook.com>
2025-05-16 00:28:12 +0800

    Count Partitions with Even Sum Difference: Used left and right sum comparisons in single-pass iteration instead of prefix and suffix arrays for improved performance (RuntimeL 1ms --> 0ms; Rank: 44.42% --> 100%)

--- submissions/java/CountPartitionsWithEvenSumDifference.java
+++ submissions/java/CountPartitionsWithEvenSumDifference.java
@@ -4,32 +4,30 @@ class CountPartitionsWithEvenSumDifference {
     public int countPartitions(int[] nums) {
         boolean isTest = false;
         int countPartitions = 0;
+        int leftSum = 0;
         int len = nums.length;
-        int[] prefix = new int[len];
-        int[] suffix = new int[len];
+        int total = 0;
 
-        prefix[0] = nums[0];
-        suffix[len - 1] = nums[len - 1];
-        for (int i = 1; i < len; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-        for (int i = len - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + nums[i];
-        }
-        if (isTest) {
-            System.out.println("nums:     " + Arrays.toString(nums));
-            System.out.println("prefix:   " + Arrays.toString(prefix));
-            System.out.println("suffix:   " + Arrays.toString(suffix));
-            System.out.println("----------------------------------------------------------------------");
+        for (int number: nums) {
+            total += number;
         }
 
+        /**
+         1.  Traverse from start to penultimate element
+         to ensure there are 2 partitions checked:
+         [0, i] & and [i + 1...n - 1].
+         */
         for (int i = 0; i < len - 1; i++) {
-            if ((prefix[i] - suffix[i + 1]) % 2 == 0) {
+            leftSum += nums[i];
+            int rightSum = total - leftSum;
+            if ((leftSum - rightSum) % 2 == 0) {
                 countPartitions++;
             }
-        }
-        if (isTest) {
-            System.out.println("countPartitions: " + countPartitions);
+            if (isTest) {
+                System.out.println("i: " + i + "\n * leftSum: " + leftSum + " | rightSum: " + rightSum);
+                System.out.println(" * difference: " + (leftSum - rightSum) + "\n * countPartitions: " + countPartitions);
+                System.out.println("--------------------------------------------------------------------------------------");
+            }
         }
 
         return countPartitions;
