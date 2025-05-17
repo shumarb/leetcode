c8f3d1edd47a4cbd435648fc1f1c2f00acd92baf
Umar <shumarb@outlook.com>
2025-05-17 17:53:43 +0800

    Hamming Distance: Used integer division and comparion of bits for improved performance (Runtime: 1ms --> 0ms; Rank: 7.77% --> 100%; Space Complexity: O(N) --> O(1))

--- submissions/java/HammingDistance.java
+++ submissions/java/HammingDistance.java
@@ -2,41 +2,14 @@
 
 class HammingDistance {
     public int hammingDistance(int x, int y) {
-        Queue<Integer> xBits = getBits(x);
-        Queue<Integer> yBits = getBits(y);
-        boolean isTest = false;
         int count = 0;
-
-        if (isTest) {
-            System.out.println("x: " + x + ", xBits: " + xBits);
-            System.out.println("y: " + x + ", yBits: " + yBits);
-        }
-
-        while (!xBits.isEmpty() && !yBits.isEmpty()) {
-            if (xBits.poll() != yBits.poll()) {
+        while (x != 0 || y != 0) {
+            if (x % 2 != y % 2) {
                 count++;
             }
+            x /= 2;
+            y /= 2;
         }
-        while (!xBits.isEmpty()) {
-            if (xBits.poll() == 1) {
-                count++;
-            }
-        }
-        while (!yBits.isEmpty()) {
-            if (yBits.poll() == 1) {
-                count++;
-            }
-        }
-
         return count;
-    }
-
-    private Queue<Integer> getBits(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        while (n != 0) {
-            queue.offer(n % 2);
-            n /= 2;
-        }
-        return queue;
     }
 }
