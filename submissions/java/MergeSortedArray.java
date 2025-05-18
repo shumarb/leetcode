98063de334364e2e3c8890a7ad3638f9981bf553
Umar <shumarb@outlook.com>
2025-05-18 15:41:57 +0800

    Merge Sorted Array: Iterated from last to first elements of arrays for improved space complexity (0(N) --> O(1))

--- submissions/java/MergeSortedArray.java
+++ submissions/java/MergeSortedArray.java
@@ -3,29 +3,25 @@
 class MergeTwoSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         boolean isTest = false;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int[] nums1Copy = Arrays.copyOf(nums1, m);
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
         if (isTest) {
             System.out.println("nums1: " + Arrays.toString(nums1));
             System.out.println("nums2: " + Arrays.toString(nums2));
-            System.out.println("m: " + m + "\nn: " + n);
-            System.out.println("nums1Copy: " + Arrays.toString(nums1Copy));
         }
 
-        while (i < m && j < n) {
-            if (nums1Copy[i] <= nums2[j]) {
-                nums1[k++] = nums1Copy[i++];
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[k++] = nums2[j++];
+                nums1[k--] = nums2[j--];
             }
         }
-        while (i < m) {
-            nums1[k++] = nums1Copy[i++];
-        }
-        while (j < n) {
-            nums1[k++] = nums2[j++];
+
+        // 1. Copy remaining elements of nums2 if required.
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
         if (isTest) {
             System.out.println("----------------------------------------");
