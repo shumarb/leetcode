6642d065e1086f40ff3f8002de3cbc3b263846b0
Umar <shumarb@outlook.com>
2025-05-18 14:15:27 +0800

    Replace Elements in an Array: Replaced HashMap with Array for improved performance (Runtime: 41ms --> 14ms; Rank: 79.62% --> 98.85%)

--- submissions/java/ReplaceElementsInAnArray.java
+++ submissions/java/ReplaceElementsInAnArray.java
@@ -2,16 +2,23 @@
 
 class ReplaceElementsInAnArray {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        Map<Integer, Integer> elementIndexMap = new HashMap<>();
         boolean isTest = false;
+        int[] elementIndexMap;
+        int largest = 0;
         int len = nums.length;
 
+        for (int number: nums) {
+            largest = Math.max(largest, number);
+        }
+        for (int[] row: operations) {
+            largest = Math.max(largest, Math.max(row[0], row[1]));
+        }
+        elementIndexMap = new int[largest + 1];
         for (int i = 0; i < len; i++) {
-            elementIndexMap.put(nums[i], i);
+            elementIndexMap[nums[i]] = i;
         }
         if (isTest) {
             System.out.println("before, nums: " + Arrays.toString(nums));
-            System.out.println("elementIndexMap: " + elementIndexMap);
             System.out.println("operations:");
             for (int[] row: operations) {
                 System.out.println(" * " + row[0] + " --> " + row[1]);
@@ -22,11 +29,9 @@ class ReplaceElementsInAnArray {
         for (int[] operation: operations) {
             int oldValue = operation[0];
             int newValue = operation[1];
-            int index = elementIndexMap.get(oldValue);
-
+            int index = elementIndexMap[oldValue];
             nums[index] = newValue;
-            elementIndexMap.remove(oldValue);
-            elementIndexMap.put(newValue, index);
+            elementIndexMap[newValue] = index;
         }
         if (isTest) {
             System.out.println("after, nums: " + Arrays.toString(nums));
