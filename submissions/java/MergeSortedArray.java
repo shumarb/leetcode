80f64f6862f02a94aea83e0bd9528b2dbd900335
Umar <shumarb@outlook.com>
2025-05-18 15:37:53 +0800

    Merge Sorted Array: Used two pointers instead of Arrays.sort() for improved performance (Runtime: 1ms --> 0ms; Rank: 29.79% --> 100%)

--- submissions/java/MergeSortedArray.java
+++ submissions/java/MergeSortedArray.java
@@ -3,20 +3,33 @@
 class MergeTwoSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         boolean isTest = false;
-        if (isTest) {
-            System.out.println("before | nums1: " + Arrays.toString(nums1) + "\nnums2: " + Arrays.toString(nums2));
-            System.out.println("m: " + m + ", n: " + n);
-        }
+        int i = 0;
+        int j = 0;
         int k = 0;
-        for (int i = m; i < (m + n); i++) {
-            nums1[i] = nums2[k++];
+        int[] nums1Copy = Arrays.copyOf(nums1, m);
+        if (isTest) {
+            System.out.println("nums1: " + Arrays.toString(nums1));
+            System.out.println("nums2: " + Arrays.toString(nums2));
+            System.out.println("m: " + m + "\nn: " + n);
+            System.out.println("nums1Copy: " + Arrays.toString(nums1Copy));
+        }
+
+        while (i < m && j < n) {
+            if (nums1Copy[i] <= nums2[j]) {
+                nums1[k++] = nums1Copy[i++];
+            } else {
+                nums1[k++] = nums2[j++];
+            }
+        }
+        while (i < m) {
+            nums1[k++] = nums1Copy[i++];
+        }
+        while (j < n) {
+            nums1[k++] = nums2[j++];
         }
         if (isTest) {
-            System.out.println("after | nums1: " + Arrays.toString(nums1));
-        }
-        Arrays.sort(nums1);
-        if (isTest) {
-            System.out.println("final | nums1: " + Arrays.toString(nums1));
+            System.out.println("----------------------------------------");
+            System.out.println("nums1: " + Arrays.toString(nums1));
         }
     }
 }
