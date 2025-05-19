a456d3428b09381b2dca5847763052be5379ad84
Umar <shumarb@outlook.com>
2025-05-19 22:42:30 +0800

    Sum of Even Numbers After Queries: Used running sum computation over brute force computation for improved performance (Runtime: 2359ms --> 4ms; Rank: 11.69% --> 99.66%)

--- submissions/java/SumOfEvenNumbersAfterQueries.java
+++ submissions/java/SumOfEvenNumbersAfterQueries.java
@@ -2,38 +2,65 @@
 
 class SumOfEvenNumbersAfterQueries {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-        int[] result = new int[nums.length];
-        int j = 0;
         boolean isTest = false;
+        int len = nums.length;
+        int sumOfEvenNumbers = 0;
+        int[] result = new int[len];
+
+        for (int number: nums) {
+            if (number % 2 == 0) {
+                sumOfEvenNumbers += number;
+            }
+        }
         if (isTest) {
-            System.out.println("before, nums: " + Arrays.toString(nums));
-            System.out.println("----------------------------------------------");
+            System.out.println("before, nums: " + Arrays.toString(nums) + "\nsumOfEvenNumbers: " + sumOfEvenNumbers);
+            System.out.println("-----------------------------------------------");
         }
 
-        for (int i = 0; i < queries.length; i++) {
-            int[] query = queries[i];
-            nums[query[1]] += query[0];
+        int j = 0;
+        for (int[] query: queries) {
+            int value = query[0];
+            int index = query[1];
+            int previousValue = nums[index];
+            int newValue = previousValue + value;
             if (isTest) {
-                System.out.println(" * after iteration #" + (i + 1) + ": " + Arrays.toString(nums));
+                System.out.println("j: " + j + "\nquery: " + Arrays.toString(query) + " | index: " + index);
+                System.out.println("previousValue: " + previousValue + " | newValue: " + newValue);
+                System.out.println(" * before, nums: " + Arrays.toString(nums) + ", sumOfEvenNumbers: " + sumOfEvenNumbers);
             }
-            result[j++] = getSumEvenNumbers(nums);
+
+            // 1. Update element of nums at index.
+            nums[index] = newValue;
+
+            // 2. Update sumOfEvenNumbers accordingly.
+            // 2.1 Odd -> Even: Increase sumOfEvenNumbers by newValue.
+            if (previousValue % 2 != 0 && newValue % 2 == 0) {
+                sumOfEvenNumbers += newValue;
+            }
+
+            // 2.2. Even --> Odd: Decrease sumOfEvenNumbers by previousValue.
+            else if (previousValue % 2 == 0 && newValue % 2 != 0) {
+                sumOfEvenNumbers -= previousValue;
+            }
+
+            // 2.3 Even --> Even: Increase sumOfEvenNumbers by (newValue - previousValue).
+            else if (previousValue % 2 == 0 && newValue % 2 == 0) {
+                sumOfEvenNumbers += (newValue - previousValue);
+            }
+
+            result[j++] = sumOfEvenNumbers;
+            if (isTest) {
+                System.out.println(" * after, nums: " + Arrays.toString(nums) + ", sumOfEvenNumbers: " + sumOfEvenNumbers);
+                System.out.println(" * so far, result: " + Arrays.toString(result));
+                System.out.println("-----------------------------------------------");
+            }
         }
+
         if (isTest) {
-            System.out.println("----------------------------------------------");
             System.out.println("after, nums: " + Arrays.toString(nums));
             System.out.println("result: " + Arrays.toString(result));
         }
 
         return result;
-    }
-
-    private int getSumEvenNumbers(int[] nums) {
-        int sumEvenNumbers = 0;
-        for (int number: nums) {
-            if (number % 2 == 0) {
-                sumEvenNumbers += number;
-            }
-        }
-        return sumEvenNumbers;
     }
 }
