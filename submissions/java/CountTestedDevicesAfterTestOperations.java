13e80df4e134c0c9e672da8db8c69907f23c7fb0
Umar <shumarb@outlook.com>
2025-05-21 01:14:29 +0800

    Count Tested Devices After Test Operations: Used decrement counter over manually updating array values for improved performance (Runtime: 3ms -> 1ms; Rank: 1.14% -> 100%)

--- submissions/java/CountTestedDevicesAfterTestOperations.java
+++ submissions/java/CountTestedDevicesAfterTestOperations.java
@@ -2,25 +2,17 @@
 
 class CountTestedDevicesAfterTestOperations {
     public int countTestedDevices(int[] batteryPercentages) {
+        int countDecrement = 0;
         int countTestedDevices = 0;
         int len = batteryPercentages.length;
 
         for (int i = 0; i < len; i++) {
-            if (batteryPercentages[i] > 0) {
+            if (batteryPercentages[i] - countDecrement > 0) {
                 countTestedDevices++;
-                batteryPercentages = update(batteryPercentages, i + 1);
+                countDecrement++;
             }
         }
 
         return countTestedDevices;
-    }
-
-    private int[] update(int[] batteryPercentages, int startIndex) {
-        int len = batteryPercentages.length;
-        int[] result = Arrays.copyOf(batteryPercentages, len);
-        for (int i = startIndex; i < len; i++) {
-            result[i] = Math.max(0, result[i] - 1);
-        }
-        return result;
     }
 }
