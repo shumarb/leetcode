ba700b7def2df2fb29811fc6fed0919260895851
Umar <shumarb@outlook.com>
2025-05-20 17:17:58 +0800

    Last Visited Integers: Refactored solution; resubmission resulted in same perforamnce

--- submissions/java/LastVisitedIntegers.java
+++ submissions/java/LastVisitedIntegers.java
@@ -4,10 +4,10 @@ class LastVisitedIntegers {
     public List<Integer> lastVisitedIntegers(int[] nums) {
         List<Integer> ans = new ArrayList<>();
         List<Integer> seen = new ArrayList<>();
-        int k = 0;
-        int lastElement = Integer.MIN_VALUE;
-        int len = nums.length;
         boolean isTest = false;
+        int countSeen = 0;
+        int k = 0;
+        int len = nums.length;
 
         if (isTest) {
             System.out.println("nums: " + Arrays.toString(nums));
@@ -16,30 +16,28 @@ class LastVisitedIntegers {
         for (int i = 0; i < len; i++) {
             int number = nums[i];
             if (isTest) {
-                System.out.println("---------------------------------------------------------");
-                System.out.println("i: " + i + " | number: " + number );
-                System.out.println(" * before | k: " + k + ", seen: " + seen + ", ans: " + ans + ", lastElement: " + lastElement);
+                System.out.println("------------------------------------------");
+                System.out.println("i: " + i + ", number: " + number);
+                System.out.println(" * before | k: " + k + ", seen: " + seen + ", ans: " + ans);
             }
             if (number > 0) {
-                // 1. Reset k to 0 as number (positive) breaks chain of consecutive -1's.
+                // 1. Reset k to 0 as chain of consecutive -1's is broken.
                 k = 0;
                 seen.add(0, number);
             } else {
-                if (k == 0 || lastElement == -1) {
-                    k++;
-                } if (k <= seen.size()) {
+                k++;
+                if (k <= seen.size()) {
                     ans.add(seen.get(k - 1));
-                } else if (k > seen.size()) {
+                } else {
                     ans.add(-1);
                 }
             }
-            lastElement = number;
             if (isTest) {
-                System.out.println(" * after | k: " + k + ", seen: " + seen + ", ans: " + ans + ", lastElement: " + lastElement);
+                System.out.println(" * after | k: " + k + ", seen: " + seen + ", ans: " + ans);
             }
         }
-
         if (isTest) {
+            System.out.println("------------------------------------------");
             System.out.println("ans: " + ans);
         }
 
