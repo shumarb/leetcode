1e6ccb6d3ec79a971c3a412267c9b34c2d232aa6
Umar <shumarb@outlook.com>
2025-05-21 15:49:44 +0800

    Find All Lonely Numbers in the Array: Replaced set and map with frequency array for improved performance (Runtime: 100ms -> 15ms; Rank: 5.67% -> 98.92%)

--- submissions/java/FindAllLonelyNumbersInTheArray.java
+++ submissions/java/FindAllLonelyNumbersInTheArray.java
@@ -3,19 +3,37 @@
 class FindAllLonelyNumbersInTheArray {
     public List<Integer> findLonely(int[] nums) {
         List<Integer> result = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        boolean isTest = false;
+        int largest = nums[0];
 
         for (int number: nums) {
-            set.add(number);
-            map.put(number, 1 + map.getOrDefault(number, 0));
+            largest = Math.max(largest, number);
         }
+        int[] numberFrequency = new int[largest + 2];
+        for (int number: nums) {
+            numberFrequency[number]++;
+        }
+        if (isTest) {
+            System.out.println("nums: " + Arrays.toString(nums) + "\nnumberFrequency: " + Arrays.toString(numberFrequency));
+        }
+
         for (int number: nums) {
             int left = number - 1;
             int right = number + 1;
-            if (map.get(number) == 1 && !set.contains(left) && !set.contains(right)) {
-                result.add(number);
+
+            // 1. Edge case: number == 0.
+            if (number == 0) {
+                if (numberFrequency[number] == 1 && numberFrequency[right] == 0) {
+                    result.add(number);
+                }
+            } else {
+                if (numberFrequency[number] == 1 && numberFrequency[left] == 0 && numberFrequency[right] == 0) {
+                    result.add(number);
+                }
             }
+        }
+        if (isTest) {
+            System.out.println("result: " + result);
         }
 
         return result;
