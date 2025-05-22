b50e2cc8b45bd50ee8d4ac6078f481387a5daa15
Umar <shumarb@outlook.com>
2025-05-22 14:07:27 +0800

    Maximum Number of Balloons: Refactored solution for improved performance (Runtime: 3ms -> 2ms; Rank: 75.90% -> 92.99%)

--- submissions/java/MaximumNumberOfBalloons.java
+++ submissions/java/MaximumNumberOfBalloons.java
@@ -1,34 +1,45 @@
 // Question: https://leetcode.com/problems/maximum-number-of-balloons/description/
 
-class MaximumNumberOfBalloons {
+class MaximumNumberOfBalloon {
     public int maxNumberOfBalloons(String text) {
-        int[] letterFrequency = new int[26];
-        int maximumCount = 0;
+        String word = "balloon";
         boolean isTest = false;
+        int count = Integer.MAX_VALUE;
+        int[] textFrequency = new int[26];
+        int[] wordFrequency = new int[26];
 
         for (char letter: text.toCharArray()) {
-            if (isValid(letter)) {
-                letterFrequency[letter - 'a']++;
+            textFrequency[letter - 'a']++;
+        }
+        for (char letter: word.toCharArray()) {
+            wordFrequency[letter - 'a']++;
+        }
+        /**
+         1.  Divide frequency by 2 for letters 'l' and 'o'
+         as these letters are counted as a pair.
+         */
+        wordFrequency['l' - 'a'] /= 2;
+        wordFrequency['o' - 'a'] /= 2;
+        textFrequency['l' - 'a'] /= 2;
+        textFrequency['o' - 'a'] /= 2;
+        if (isTest) {
+            System.out.println("text: " + text + "\nword: " + word);
+            System.out.println("textFrequency: " + Arrays.toString(textFrequency));
+            System.out.println("wordFrequency: " + Arrays.toString(wordFrequency));
+        }
+
+        for (char letter: word.toCharArray()) {
+            // 2. Can't form "balloon" if text has insufficient letters.
+            if (textFrequency[letter - 'a'] == 0 || textFrequency[letter - 'a'] < wordFrequency[letter - 'a']) {
+                return 0;
+            } else {
+                count = Math.min(count, textFrequency[letter - 'a'] / wordFrequency[letter - 'a']);
             }
         }
         if (isTest) {
-            System.out.println("text: " + text + "\nletterFrequency: " + Arrays.toString(letterFrequency));
+            System.out.println("count: " + count);
         }
 
-        return countMaximum(letterFrequency);
-    }
-
-    private int countMaximum(int[] letterFrequency) {
-        int count = Integer.MAX_VALUE;
-        count = Math.min(count, letterFrequency['b' - 'a']);
-        count = Math.min(count, letterFrequency['a' - 'a']);
-        count = Math.min(count, letterFrequency['l' - 'a'] / 2);
-        count = Math.min(count, letterFrequency['o' - 'a'] / 2);
-        count = Math.min(count, letterFrequency['n' - 'a']);
         return count;
-    }
-
-    private boolean isValid(char letter) {
-        return letter == 'b' || letter == 'a' || letter == 'l' || letter == 'o' || letter == 'n';
     }
 }
