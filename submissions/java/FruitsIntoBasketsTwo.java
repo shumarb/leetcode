73ffe07a847ce9947d2e455513f6ada06707cd3f
Umar <shumarb@outlook.com>
2025-05-23 10:53:29 +0800

    New submission: Fruits into Baskets II

---
+++ submissions/java/FruitsIntoBasketsTwo.java
@@ -0,0 +1,46 @@
+// Question: https://leetcode.com/problems/fruits-into-baskets-ii/description/
+
+class FruitsIntoBasketsTwo {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        boolean isTest = false;
+        int count = 0;
+
+        if (isTest) {
+            System.out.println("before:\nfruits: " + Arrays.toString(fruits));
+            System.out.println("baskets: " + Arrays.toString(baskets));
+            System.out.println("--------------------------------");
+        }
+
+        for (int i = 0; i < fruits.length; i++) {
+            int fruit = fruits[i];
+            for (int j = 0; j < baskets.length; j++) {
+                int basket = baskets[j];
+                if (isTest) {
+                    System.out.println("fruit: " + fruit + " | basket: " + basket);
+                }
+                if (basket >= fruit) {
+                    baskets[j] = -1;
+                    if (isTest) {
+                        System.out.println(" * insert");
+                    }
+                    break;
+                }
+            }
+            if (isTest) {
+                System.out.println("--------------------------------");
+            }
+        }
+        if (isTest) {
+            System.out.println("after:\nfruits: " + Arrays.toString(fruits));
+            System.out.println("baskets: " + Arrays.toString(baskets));
+        }
+
+        for (int i = 0; i < baskets.length; i++) {
+            if (baskets[i] != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
