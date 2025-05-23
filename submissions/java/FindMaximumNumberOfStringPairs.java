cbc0ff94bd2f2f9636d62c586d39d606a555bb30
Umar <shumarb@outlook.com>
2025-05-24 01:49:39 +0800

    Find Maximum Number of String Pairs: Used 2 sets and one-pass iteration over brute force (nested loops) for improved performance (Runtime: 6ms -> 3ms; Rank: 50.57% -> 78.35%)

--- submissions/java/FindMaximumNumberOfStringPairs.java
+++ submissions/java/FindMaximumNumberOfStringPairs.java
@@ -2,18 +2,19 @@
 
 class /FindMaximumNumberOfStringPairs {
     public int maximumNumberOfStringPairs(String[] words) {
+        Set<String> checked = new HashSet<>();
+        Set<String> set = new HashSet<>();
         int count = 0;
-        for (int i = 0; i < words.length - 1; i++) {
-            for (int j = i + 1; j < words.length; j++) {
-                StringBuilder first = new StringBuilder();
-                for (char letter: words[i].toCharArray()) {
-                    first.append(letter);
-                }
-                if (first.reverse().toString().equals(words[j])) {
-                    count++;
-                }
+
+        for (String word: words) {
+            StringBuilder wordComplement = new StringBuilder(word);
+            if (!checked.contains(word) && set.contains(wordComplement.reverse().toString())) {
+                count++;
             }
+            checked.add(word);
+            set.add(word);
         }
+
         return count;
     }
 }
