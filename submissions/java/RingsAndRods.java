7a51ddeb6029a6ed13710a4e5f385f4253bf37ec
Umar <shumarb@outlook.com>
2025-05-27 15:40:34 +0800

    Rings and Rods: Used frequency array over hashmap for improved performance (Runtime: 1ms -> 0ms; Rank: 59.81% -> 100%)

--- submissions/java/RingsAndRods.java
+++ submissions/java/RingsAndRods.java
@@ -2,8 +2,10 @@
 
 class RingsAndRods {
     public int countPoints(String rings) {
-        Map<Integer, Set<Character>> map = new HashMap<>();
         boolean isTest = false;
+        int[] blueRods = new int[10];
+        int[] greenRods = new int[10];
+        int[] redRods = new int[10];
         int countPoints = 0;
 
         for (int i = 0; i < rings.length(); i += 2) {
@@ -12,28 +14,26 @@ class RingsAndRods {
             if (isTest) {
                 System.out.println(letter + " -> " + rod);
             }
-            if (!map.containsKey(rod)) {
-                Set<Character> set = new HashSet<>();
-                set.add(letter);
-                map.put(rod, set);
-            } else {
-                Set<Character> set = map.get(rod);
-                if (!set.contains(letter)) {
-                    set.add(letter);
-                    if (set.size() == 3) {
-                        countPoints++;
-                    }
-                }
+            if (letter == 'B') {
+                blueRods[rod]++;
+            } else if (letter == 'G') {
+                greenRods[rod]++;
+            } else if (letter == 'R') {
+                redRods[rod]++;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if (blueRods[i] > 0 && greenRods[i] > 0 && redRods[i] > 0) {
+                countPoints++;
             }
         }
 
         if (isTest) {
             System.out.println("-------------------------------------------------------------");
             System.out.println("rings: " + rings);
-            System.out.println("map:");
-            for (Map.Entry<Integer, Set<Character>> entry: map.entrySet()) {
-                System.out.println(entry.getKey() + " -> " + entry.getValue());
-            }
+            System.out.println("blueRods: " + Arrays.toString(blueRods));
+            System.out.println("greenRods: " + Arrays.toString(greenRods));
+            System.out.println("redRods: " + Arrays.toString(redRods));
             System.out.println("-------------------------------------------------------------");
             System.out.println("countPoints: " + countPoints);
         }
