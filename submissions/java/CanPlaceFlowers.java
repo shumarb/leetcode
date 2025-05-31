b472f4768e63bcb5a07386d2c46c8d98d4eccb36
Umar <shumarb@outlook.com>
2025-05-31 20:19:28 +0800

    Can Place Flowers: Added edge cases for same performance

--- submissions/java/CanPlaceFlowers.java
+++ submissions/java/CanPlaceFlowers.java
@@ -8,6 +8,17 @@ class CanPlaceFlowers {
         if (isTest) {
             System.out.println("before | flowerbed: " + Arrays.toString(flowerbed) + ", n: " + n);
         }
+
+        // 1. Edge case: n > number of flowers.
+        if (n > len) {
+            return false;
+        }
+
+        // 2. Edge case: Flowerbed has 1 plot and n == 1.
+        if (len == 1 && n == 1) {
+            return flowerbed[0] == 0;
+        }
+
         for (int i = 0; i < len && n > 0; i++) {
             if (flowerbed[i] == 0) {
                 boolean isLeftEmpty = (i == 0 || flowerbed[i - 1] == 0);
