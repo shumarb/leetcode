7e9dcfde0f0f9169f9fd75bc5c9dd2a5225b1ac7
Umar <shumarb@outlook.com>
2025-05-31 14:10:40 +0800

    Delete Columns to Make Sorted: Stored word length and word count in variables to reduce number of checks for same performance

--- submissions/java/DeleteColumnsToMakeSorted.java
+++ submissions/java/DeleteColumnsToMakeSorted.java
@@ -3,14 +3,18 @@
 class DeleteColumnsToMakeSorted {
     public int minDeletionSize(String[] strs) {
         int minDeletionSize = 0;
-        for (int j = 0; j < strs[0].length(); j++) {
-            for (int i = 0; i < strs.length - 1; i++) {
+        int wordCount = strs.length;
+        int wordLength = strs[0].length();
+
+        for (int j = 0; j < wordLength; j++) {
+            for (int i = 0; i < wordCount - 1; i++) {
                 if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
                     minDeletionSize++;
                     break;
                 }
             }
         }
+
         return minDeletionSize;
     }
 }
