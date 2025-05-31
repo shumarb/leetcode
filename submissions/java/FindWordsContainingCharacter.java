55eda18c0e3c30984d38263233583e80f74e9cbb
Umar <shumarb@outlook.com>
2025-06-01 00:57:12 +0800

    Find Words Containing Character: Used String class indexOf method over frequency array for improved performance (Runtime: 2ms -> 1ms; Rank: 47.06% -> 100%)

--- submissions/java/FindWordsContainingCharacter.java
+++ submissions/java/FindWordsContainingCharacter.java
@@ -2,10 +2,9 @@
 
 class FindWordsContainingCharacter {
     public List<Integer> findWordsContaining(String[] words, char x) {
-        String xStr = Character.toString(x);
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
-            if (words[i].contains(xStr)) {
+            if (words[i].indexOf(x) != -1) {
                 result.add(i);
             }
         }
