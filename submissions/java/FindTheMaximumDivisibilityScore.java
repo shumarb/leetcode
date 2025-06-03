92a53fe1083ee3240295ea40e02ca6d3aef6be14
Umar <shumarb@outlook.com>
2025-06-03 12:21:22 +0800

    Find the Maximum Divisibility Score: Set frequency check in 2nd for loop rather than doing it at every frequency update for improved performance (Runtime: 178ms -> 176ms; Rank: 54.36% -> 82.56%)

--- submissions/java/FindTheMaximumDivisibilityScore.java
+++ submissions/java/FindTheMaximumDivisibilityScore.java
@@ -5,26 +5,25 @@ class FindTheMaximumDivisibilityScore {
         boolean isTest = false;
         int maxFrequency = 0;
         int result = Integer.MAX_VALUE;
-        int[] divScores = new int[divisors.length];
+        int[] frequency = new int[divisors.length];
 
         for (int i = 0; i < divisors.length; i++) {
             for (int number: nums) {
                 if (number % divisors[i] == 0) {
-                    divScores[i]++;
-                    maxFrequency = Math.max(divScores[i], maxFrequency);
+                    frequency[i]++;
                 }
             }
         }
-        for (int i = 0; i < divScores.length; i++) {
-            int divScore = divScores[i];
-            if (maxFrequency == divScores[i] && divisors[i] < result) {
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > maxFrequency || (frequency[i] == maxFrequency && divisors[i] < result)) {
+                maxFrequency = frequency[i];
                 result = divisors[i];
             }
         }
         if (isTest) {
             System.out.println("nums: " + Arrays.toString(nums));
             System.out.println("divisors: " + Arrays.toString(divisors));
-            System.out.println("divScores: " + Arrays.toString(divScores));
+            System.out.println("frequency: " + Arrays.toString(frequency));
             System.out.println("result: " + result);
         }
 
