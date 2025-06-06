018d5979af890df46a395b6734efd4e825d04c31
Umar <shumarb@outlook.com>
2025-06-06 09:08:45 +0800

    Find the Key of the Numbers: Used frequency array for improved performance (Runtime: 2ms -> 1ms; Rank: 37.83% -> 94.40%)

--- submissions/FindTheKeyOfTheNumbers.java
+++ submissions/FindTheKeyOfTheNumbers.java
@@ -2,36 +2,26 @@
 
 class FindTheKeyOfTheNumbers {
     public int generateKey(int num1, int num2, int num3) {
-        int key = 0;
         boolean isTest = false;
+        int key = 0;
+        int[] digits = new int[4];
 
-        String num1Str = getString(num1);
-        String num2Str = getString(num2);
-        String num3Str = getString(num3);
         if (isTest) {
-            System.out.println(num1 + " --> " + num1Str);
-            System.out.println(num2 + " --> " + num2Str);
-            System.out.println(num3 + " --> " + num3Str);
+            System.out.println("num1: " + num1 + "\nnum2: " + num2 + "\nnum3: " + num3);
+        }
+        for (int i = 3; i >= 0; i--) {
+            digits[i] = Math.min(num1 % 10, Math.min(num2 % 10, num3 % 10));
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+        }
+        for (int digit: digits) {
+            key = key * 10 + digit;
+        }
+        if (isTest) {
+            System.out.println("digits: " + Arrays.toString(digits) + "\nkey: " + key);
         }
 
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            int firstDigit = Character.getNumericValue(num1Str.charAt(i));
-            int secondDigit = Character.getNumericValue(num2Str.charAt(i));
-            int thirdDigit = Character.getNumericValue(num3Str.charAt(i));
-            result.append(Math.min(thirdDigit, Math.min(firstDigit, secondDigit)));
-        }
-
-        return Integer.parseInt(result.toString());
-    }
-
-
-    private String getString(int num) {
-        StringBuilder result = new StringBuilder();
-        result.append(num);
-        while (result.length() < 4) {
-            result.insert(0, '0');
-        }
-        return result.toString();
+        return key;
     }
 }
