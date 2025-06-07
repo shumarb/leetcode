6acac066f12957cf6a964929352394bfbfc129e2
Umar <shumarb@outlook.com>
2025-06-07 21:07:34 +0800

    Maximum Sum of Distinct Subarrays With Length K: Refactored solution resulted in same performance

--- submissions/MaximumSumOfDistinctSubarraysWithLengthK.java
+++ submissions/MaximumSumOfDistinctSubarraysWithLengthK.java
@@ -2,48 +2,50 @@
 
 class MaximumSumOfDistinctSubarraysWithLengthK {
     public long maximumSubarraySum(int[] nums, int k) {
-        long runningSum = 0;
-        long maximumSum = 0;
         int countDistinct = 0;
         int largest = nums[0];
+        int len = nums.length;
         int[] frequency;
+        long currentSubarraySum = 0;
+        long maximumSubarraySum = 0;
 
         for (int number: nums) {
             largest = Math.max(largest, number);
         }
         frequency = new int[largest + 1];
+
         for (int i = 0; i < k; i++) {
             int current = nums[i];
-            runningSum += current;
-            if (frequency[current] == 0) {
+            frequency[current]++;
+            currentSubarraySum += current;
+            if (frequency[current] == 1) {
                 countDistinct++;
             }
-            frequency[current]++;
         }
         if (countDistinct == k) {
-            maximumSum = Math.max(maximumSum, runningSum);
+            maximumSubarraySum = currentSubarraySum;
         }
 
-        for (int i = k; i < nums.length; i++) {
+        for (int i = k; i < len; i++) {
             int remove = nums[i - k];
-            runningSum -= remove;
+            currentSubarraySum -= remove;
             frequency[remove]--;
             if (frequency[remove] == 0) {
                 countDistinct--;
             }
 
             int incoming = nums[i];
-            runningSum += incoming;
-            if (frequency[incoming] == 0) {
+            currentSubarraySum += incoming;
+            frequency[incoming]++;
+            if (frequency[incoming] == 1) {
                 countDistinct++;
             }
-            frequency[incoming]++;
 
             if (countDistinct == k) {
-                maximumSum = Math.max(maximumSum, runningSum);
+                maximumSubarraySum = Math.max(maximumSubarraySum, currentSubarraySum);
             }
         }
 
-        return maximumSum;
+        return maximumSubarraySum;
     }
 }
