54388f6723d049c71789f4a8e427acb365c28023
Umar <shumarb@outlook.com>
2025-06-09 00:06:54 +0800

    Length of Last Word: Refactored solution for same performance

--- submissions/LengthOfLastWord.java
+++ submissions/LengthOfLastWord.java
@@ -3,11 +3,10 @@
 class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         int count = 0;
-        int i = s.length() - 1;
+        int i;
 
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
+        s = s.trim();
+        i = s.length() - 1;
         while (i >= 0 && s.charAt(i) != ' ') {
             count++;
             i--;
