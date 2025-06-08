151e699befa095419340ed55b2627ffc902d4a5f
Umar <shumarb@outlook.com>
2025-06-08 11:27:33 +0800

    Lexicographically Minimum String After Removing Stars: Replaced list with array for improved performance (Runtime: 110ms -> 101ms; Rank: 85.63% -> 86.78%)

--- submissions/LexicographicallyMinimumStringAfterRemovingStars.java
+++ submissions/LexicographicallyMinimumStringAfterRemovingStars.java
@@ -2,26 +2,25 @@
 
 class LexicographicallyMinimumStringAfterRemovingStars {
     public String clearStars(String s) {
+        Stack<Integer>[] lastIndex = (Stack<Integer>[]) new Stack[26];
         StringBuilder result = new StringBuilder();
         boolean isTest = false;
         char[] parts = s.toCharArray();
-        List<Stack<Integer>> lastIndex = new ArrayList<>();
 
         if (isTest) {
             System.out.println("s: " + s + "\nparts: " + Arrays.toString(parts));
         }
         for (int i = 0; i < 26; i++) {
-            lastIndex.add(new Stack<>());
+            lastIndex[i] = new Stack<>();
         }
         for (int i = 0; i < parts.length; i++) {
             char current = parts[i];
             if (current != '*') {
-                lastIndex.get(current - 'a').push(i);
+                lastIndex[current - 'a'].push(i);
             } else {
                 for (int j = 0; j < 26; j++) {
-                    if (!lastIndex.get(j).isEmpty()) {
-                        int indexOfUpdate = lastIndex.get(j).pop();
-                        parts[indexOfUpdate] = '*';
+                    if (!lastIndex[j].isEmpty()) {
+                        parts[lastIndex[j].pop()] = '*';
                         break;
                     }
                 }
