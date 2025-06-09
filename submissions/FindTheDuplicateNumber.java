9c4115969098c04874eaee46d7d0cb27c2695c07
Umar <shumarb@outlook.com>
2025-06-09 18:45:55 +0800

    Find the Duplicate Number: Added explanations for same performance

--- submissions/FindTheDuplicateNumber.java
+++ submissions/FindTheDuplicateNumber.java
@@ -5,21 +5,35 @@ class FindTheDuplicateNumber {
         boolean isTest = false;
 
         for (int i = 0; i < nums.length; i++) {
-            int absoluteElement = Math.abs(nums[i]);
+            /**
+             1.  Each element represents an index in the array.
+                 If it is negative, retrieve its absolute value.
+             */
+            int indexToCheck = Math.abs(nums[i]);
             if (isTest) {
                 System.out.println("-----------------------------");
-                System.out.println("i: " + i + " | absoluteElement: " + absoluteElement);
+                System.out.println("i: " + i + " | indexToCheck: " + indexToCheck);
                 System.out.println(" * before: " + Arrays.toString(nums));
             }
-            if (nums[absoluteElement] < 0) {
-                return absoluteElement;
+
+            /**
+             2.  If the element at indexToCheck is negative,
+                 indexToCheck has been checked before,
+                 thus the duplicate number is indexToCheck.
+             */
+            if (nums[indexToCheck] < 0) {
+                return indexToCheck;
             }
-            nums[absoluteElement] *= -1;
+
+            /**
+             3.  Set element to its negative to denote this index has been checked.
+             */
+            nums[indexToCheck] *= -1;
             if (isTest) {
                 System.out.println(" * after:  " + Arrays.toString(nums));
             }
         }
 
-        return -1; // dummy value.
+        return -1; // 4. dummy value.
     }
 }
