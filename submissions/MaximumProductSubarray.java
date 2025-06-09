d043ce3ef14abcbbb80fa8e105dd6864294404fe
Umar <shumarb@outlook.com>
2025-06-10 01:30:30 +0800

    Maximum Product Subarray: Used prefix and suffix arrays for improved performance (Runtime: 271ms -> 1ms; Rank: 8.01% -> 90.84%)

--- submissions/MaximumProductSubarray.java
+++ submissions/MaximumProductSubarray.java
@@ -1,22 +1,41 @@
 // Question: https://leetcode.com/problems/maximum-product-subarray/description/
 
-class MaximumProductSubarray {
+class MaximumProductSubarray class Solution {
     public int maxProduct(int[] nums) {
+        boolean isTest = false;
         int len = nums.length;
         int maxProduct = nums[0];
+        int[] prefix = new int[len];
+        int[] suffix = new int[len];
+        int product = 1;
 
-        for (int number: nums) {
-            maxProduct = Math.max(number, maxProduct);
+        prefix[0] = nums[0];
+        suffix[len - 1] = nums[len - 1];
+        for (int i = 1; i < len; i++) {
+            if (prefix[i - 1] == 0) {
+                prefix[i] = 1 * nums[i];
+            } else {
+                prefix[i] = nums[i] * prefix[i - 1];
+            }
+        }
+        product = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            if (suffix[i + 1] == 0) {
+                suffix[i] = 1 * nums[i];
+            } else {
+                suffix[i] = nums[i] * suffix[i + 1];
+            }
+        }
+        if (isTest) {
+            System.out.println("prefix: " + Arrays.toString(prefix));
+            System.out.println("nums:   " + Arrays.toString(nums));
+            System.out.println("suffix: " + Arrays.toString(suffix));
         }
         for (int i = 0; i < len; i++) {
-            int product = nums[i];
-            for (int j = i + 1; j < len; j++) {
-                int current = nums[j];
-                product *= current;
-                maxProduct = Math.max(maxProduct, product);
-            }
+            maxProduct = Math.max(maxProduct, Math.max(prefix[i], suffix[i]));
         }
 
         return maxProduct;
     }
+
 }
