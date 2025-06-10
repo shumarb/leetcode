f878484e6f1b685fc889821a2d176ad141b1ea9e
Umar <shumarb@outlook.com>
2025-06-10 11:50:24 +0800

    Non-overlapping Intervals: Refactored solution for improved performance (Runtime: 48ms -> 46ms; Rank: 59.58% -> 82.48%)

--- submissions/NonOverlappingIntervals.java
+++ submissions/NonOverlappingIntervals.java
@@ -3,40 +3,27 @@
 class NonOverlappingIntervals {
     public int eraseOverlapIntervals(int[][] intervals) {
         boolean isTest = false;
-        int count = 0;
-        int len = intervals.length;
+        int countNonOverlappingIntervals = 1; // Count first interval.
+        int latestEndTime = 0;
+        int totalIntervals = intervals.length;
 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        if (isTest) {
-            System.out.println("sorted intervals (by end time):");
-            for (int[] row: intervals) {
-                System.out.println(Arrays.toString(row));
-            }
-            System.out.println("-----------------------------------------------");
-        }
-
-        /**
-         1.  Given 2 intervals, exclude an interval when it's end time
-             is less than the start time of latest interval counted.
-             When no overlap occurs, set previous end time as most-recently-counted interval's end time,
-             and repeat comparison for remaining intervals.
-         */
-        int previousEndTime = intervals[0][1];
-        for (int i = 1; i < len; i++) {
-            if (intervals[i][0] < previousEndTime) {
-                if (isTest) {
-                    System.out.println(" * exclude: " + Arrays.toString(intervals[i]));
-                }
-                count++;
-            } else {
-                previousEndTime = intervals[i][1];
+        latestEndTime = intervals[0][1];
+        for (int i = 1; i < totalIntervals; i++) {
+            if (intervals[i][0] >= latestEndTime) {
+                countNonOverlappingIntervals++;
+                latestEndTime = intervals[i][1];
             }
         }
         if (isTest) {
-            System.out.println("-----------------------------------------------");
-            System.out.println("count: " + count);
+            System.out.println("sorted intervals by end time (ascending):");
+            for (int [] interval: intervals) {
+                System.out.println(" * " + Arrays.toString(interval));
+            }
+            System.out.println("---------------------------------------------------------------------");
+            System.out.println("totalIntervals: " + totalIntervals + "\ncountNonOverlappingIntervals: " + countNonOverlappingIntervals);
         }
 
-        return count;
+        return totalIntervals - countNonOverlappingIntervals;
     }
 }
