370d7c7ece036d9b822304fb66f6fa53a2d0eb80
Umar <shumarb@outlook.com>
2025-06-10 11:13:02 +0800

    Determine Color of a Chessboard Square: Refactored solution for cleaner code and improved performance

--- submissions/DetermineColorOfAChessboardSquare.java
+++ submissions/DetermineColorOfAChessboardSquare.java
@@ -1,25 +1,15 @@
 // Question: https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/
 
-class DetermineColorOfAChessboardSquare {
+class DetermineColorOfAChessboardSquare class Solution {
     public boolean squareIsWhite(String coordinates) {
         boolean isTest = false;
+        int column = coordinates.charAt(0) - 'a' + 1;
+        int row = coordinates.charAt(1) - '0';
 
-        // 1. Increase values by 1 given that coordinates are 1-based indexing.
-        int rowNumber = coordinates.charAt(1) - '1' + 1;
-        int columnNumber = coordinates.charAt(0) - 'a' + 1;
-        boolean[] row = getRow(rowNumber);
         if (isTest) {
-            System.out.println("coordinates: " + coordinates + " | rowNumber: " + rowNumber + ", columnNumber: " + columnNumber);
-            System.out.println("row: " + Arrays.toString(row));
+            System.out.println("coordinates: " + coordinates + " | column: " + column + " | row: " + row);
         }
 
-        // 2. Decrease column number by 1 due to accessing index in array (0-based indexing).
-        return row[columnNumber - 1];
-    }
-
-    private boolean[] getRow(int rowNumber) {
-        boolean[] oddRow = new boolean[] {false, true, false, true, false, true, false, true};
-        boolean[] evenRow = new boolean[] {true, false, true, false, true, false, true, false};
-        return rowNumber % 2 == 0 ? evenRow : oddRow;
+        return (row + column) % 2 != 0;
     }
 }
