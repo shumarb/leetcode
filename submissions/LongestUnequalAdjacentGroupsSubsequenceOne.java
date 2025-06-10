7ad7ee2293c5763186649783ec6884c8521b7fdf
Umar <shumarb@outlook.com>
2025-06-10 12:56:41 +0800

    Longest Unequal Adjacent Groups Subsequence I: Implemented one-pass algorithm with O(1) space complexity for same performance

--- submissions/LongestUnequalAdjacentGroupsSubsequenceOne.java
+++ submissions/LongestUnequalAdjacentGroupsSubsequenceOne.java
@@ -3,27 +3,18 @@
 class LongestUnequalAdjacentGroupsSubsequenceOne {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
         List<String> result = new ArrayList<>();
-        int len = words.length;
         boolean isTest = false;
-        boolean[] prefix = new boolean[len];
+        int len = groups.length;
 
-        prefix[0] = true;
+        result.add(words[0]);
         for (int i = 1; i < len; i++) {
             if (groups[i] != groups[i - 1]) {
-                prefix[i] = true;
-            } else {
-                prefix[i] = false;
-            }
-        }
-        for (int i = 0; i < len; i++) {
-            if (prefix[i]) {
                 result.add(words[i]);
             }
         }
         if (isTest) {
             System.out.println("words:  " + Arrays.toString(words));
             System.out.println("groups: " + Arrays.toString(groups));
-            System.out.println("prefix: " + Arrays.toString(prefix));
             System.out.println("result: " + result);
         }
 
