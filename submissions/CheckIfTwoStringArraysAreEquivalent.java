e47165111cb36e91d98481d0915e29a0cb1a3e2e
Umar <shumarb@outlook.com>
2025-06-10 10:50:20 +0800

    Check If Two String Arrays are Equivalent: Populated StringBuilders with each word instead of checking each word and populating it with each character for improved performance (Runtime: 1ms -> 0ms; Rank: 59.87% -> 100%)

--- submissions/CheckIfTwoStringArraysAreEquivalent.java
+++ submissions/CheckIfTwoStringArraysAreEquivalent.java
@@ -2,15 +2,21 @@
 
 class CheckIfTwoStringArraysAreEquivalent {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        return update(word1).equals(update(word2));
+        boolean isTest = false;
+        String word1Sb = update(word1);
+        String word2Sb = update(word2);
+        if (isTest) {
+            System.out.println("word1: " + Arrays.toString(word1) + " | word1Sb: " + word1Sb);
+            System.out.println("word2: " + Arrays.toString(word2) + " | word2Sb: " + word2Sb);
+        }
+
+        return word1Sb.equals(word2Sb);
     }
 
     private String update(String[] words) {
         StringBuilder result = new StringBuilder();
         for (String word: words) {
-            for (char letter: word.toCharArray()) {
-                result.append(letter);
-            }
+            result.append(word);
         }
         return result.toString();
     }
