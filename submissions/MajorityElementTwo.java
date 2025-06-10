960626624e4decf7c530c97993db99b43b5d1256
Umar <shumarb@outlook.com>
2025-06-10 15:02:16 +0800

    Majority Element II: Removed debugging code resulted in weaker performance

--- submissions/MajorityElementTwo.java
+++ submissions/MajorityElementTwo.java
@@ -1,33 +1,19 @@
-package java;// Question: https://leetcode.com/problems/majority-element-ii/description/
-
-import java.util.*;
+// Question: https://leetcode.com/problems/majority-element-ii/description/
 
 class MajorityElementTwo {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
-        boolean t = false;
 
-        for (int x: nums) {
-            map.put(x, 1 + map.getOrDefault(x, 0));
+        for (int number: nums) {
+            map.put(number, 1 + map.getOrDefault(number, 0));
         }
-        if (t) {
-            System.out.println("nums: " + Arrays.toString(nums));
-            System.out.println("map: " + map);
-            System.out.println("list: " + list);
-        }
-        for (Map.Entry<Integer, Integer> e: map.entrySet()) {
-            if (t) {
-                System.out.println("e: " + e);
-            }
-            if (e.getValue() > (nums.length / 3)) {
-                list.add(e.getKey());
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if (entry.getValue() > nums.length / 3) {
+                result.add(entry.getKey());
             }
         }
-        if (t) {
-            System.out.println("after | list: " + list);
-        }
-        
-        return list;
+
+        return result;
     }
 }
