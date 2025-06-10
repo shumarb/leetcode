fc592270c73e916a27dcffb7f0ef5b0b9c74dfc0
Umar <shumarb@outlook.com>
2025-06-10 11:08:28 +0800

    Check if Two Chessboard Squares Have the Same Colour: Checked sum of integer values of row and column coordinates for improved performance (Runtime: 1ms -> 0ms; Rank: 23.08% -> 100%)

--- submissions/CheckIfTwoChessboardSquaresHaveTheSameColour.java
+++ submissions/CheckIfTwoChessboardSquaresHaveTheSameColour.java
@@ -3,31 +3,16 @@
 class CheckIfTwoChessboardSquaresHaveTheSameColour {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
         boolean isTest = false;
+        int column1 = coordinate1.charAt(0) - 'a' + 1;
+        int column2 = coordinate2.charAt(0) - 'a' + 1;
+        int row1 = coordinate1.charAt(1) - '0';
+        int row2 = coordinate2.charAt(1) - '0';
 
-        int c1RowNumber = coordinate1.charAt(0) - 'a';
-        int c1ColumnNumber = coordinate1.charAt(1) - '1';
-        int c2RowNumber = coordinate2.charAt(0) - 'a';
-        int c2ColumnNumber = coordinate2.charAt(1) - '1';
         if (isTest) {
-            System.out.println("coordinate 1: " + coordinate1 + " | row: " + c1RowNumber + ", column: " + c1ColumnNumber);
-            System.out.println("coordinate 2: " + coordinate2 + " | row: " + c1RowNumber + ", column: " + c2ColumnNumber);
-            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("coordinate1: " + coordinate1 + " | column1: " + column1 + ", row1: " + row1);
+            System.out.println("coordinate2: " + coordinate2 + " | column2: " + column2 + ", row2: " + row2);
         }
 
-        int[] c1Row = getRow(c1RowNumber);
-        int[] c2Row = getRow(c2RowNumber);
-        if (isTest) {
-            System.out.println("c1Row:" + Arrays.toString(c1Row));
-            System.out.println("c2Row:" + Arrays.toString(c2Row));
-            System.out.println("-----------------------------------------------------------------------------");
-        }
-
-        return c1Row[c1ColumnNumber] == c2Row[c2ColumnNumber];
-    }
-
-    private int[] getRow(int rowNumber) {
-        int[] oddRow = new int[] {1, 0, 1, 0, 1, 0, 1, 0};
-        int[] evenRow = new int[] {0, 1, 0, 1, 0, 1, 0, 1};
-        return rowNumber % 2 == 0 ? evenRow : oddRow;
+        return (row1 + column1 + row2 + column2) % 2 == 0;
     }
 }
