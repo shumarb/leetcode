914282315399e05ac7c71f0cb69955c2d823dc4c
Umar <shumarb@outlook.com>
2025-06-10 11:15:14 +0800

    Determine Color of a Chessboard Square: Added explanation

--- submissions/DetermineColorOfAChessboardSquare.java
+++ submissions/DetermineColorOfAChessboardSquare.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/
 
-class DetermineColorOfAChessboardSquare class Solution {
+class DetermineColorOfAChessboardSquare {
     public boolean squareIsWhite(String coordinates) {
         boolean isTest = false;
         int column = coordinates.charAt(0) - 'a' + 1;
@@ -10,6 +10,10 @@ class DetermineColorOfAChessboardSquare class Solution {
             System.out.println("coordinates: " + coordinates + " | column: " + column + " | row: " + row);
         }
 
-        return (row + column) % 2 != 0;
+        /**
+         1.  Square colour is determined by 1-based sum of row and column integer values,
+             If the sum is odd, the square is white, else it is black.
+         */
+        return (row + column) % 2 == 1;
     }
 }
