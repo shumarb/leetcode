ffaa9c1d860b55c92eb277d2d712433e6d80bcf3
Umar <shumarb@outlook.com>
2025-06-11 11:58:36 +0800

    Alternating Groups I: Refactored solution resulted in same performance

--- submissions/AlternatingGroupsOne.java
+++ submissions/AlternatingGroupsOne.java
@@ -4,27 +4,34 @@ class AlternatingGroupsOne {
     public int numberOfAlternatingGroups(int[] colors) {
         boolean isTest = false;
         int count = 0;
+        int i = 1;
         int len = colors.length;
 
-        if (colors[len - 2] != colors[len - 1] && colors[len - 1] != colors[0] && colors[len - 2] == colors[0]) {
-            if (isTest) {
-                System.out.println(" * check: " + (len - 2) + ", " + (len - 1) + ", 0");
-            }
+        if (isTest) {
+            System.out.println("colors: " + Arrays.toString(colors));
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("compare indices: " + (len - 1) + ", 0, 1");
+        }
+        if (colors[len - 1] != colors[0] && colors[0] != colors[1]) {
             count++;
         }
-        if (colors[len - 1] != colors[0] && colors[len - 1] == colors[1] && colors[0] != colors[1]) {
-            if (isTest) {
-                System.out.println(" * check: " + (len - 1) + ", 0, 1");
-            }
+
+        if (isTest) {
+            System.out.println("compare indices: " + (len - 2) + ", " + (len - 1) + ", 0");
+            System.out.println("----------------------------------------------------------------");
+        }
+        if (colors[len - 2] != colors[len - 1] && colors[len - 1] != colors[0]) {
             count++;
         }
-        for (int i = 0; i <= len - 3; i++) {
-            if (colors[i] != colors[i + 1] && colors[i] == colors[i + 2] && colors[i + 1] != colors[i + 2]) {
-                if (isTest) {
-                    System.out.println(" * check: " + i + ", " + (i + 1) + ", " + (i + 2));
-                }
+
+        while (i < len - 1) {
+            if (isTest) {
+                System.out.println("compare indices: " + (i - 1) + ", " + i + ", " + (i + 1));
+            }
+            if (colors[i - 1] != colors[i] && colors[i] != colors[i + 1]) {
                 count++;
             }
+            i++;
         }
 
         return count;
