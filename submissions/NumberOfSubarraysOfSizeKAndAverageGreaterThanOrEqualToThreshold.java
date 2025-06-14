0636da856f71c0b90143ebdfc7ada674992a6d5a
Umar <shumarb@outlook.com>
2025-06-14 11:21:22 +0800

    Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold: Increased runningSum by (incoming - remove) elements for improved performance (Runtime: 3ms -> 2ms; Rank: 89.56% -> 99.85%)

--- submissions/NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
+++ submissions/NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
@@ -2,22 +2,18 @@
 
 class NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        double runningSum = 0;
         int count = 0;
+        int runningSum = 0;
 
         for (int i = 0; i < k; i++) {
-            runningSum += (double) arr[i];
+            runningSum += arr[i];
         }
-        if (runningSum >= (double) (k * threshold)) {
+        if (runningSum >= k * threshold) {
             count++;
         }
-
         for (int i = k; i < arr.length; i++) {
-            double remove = arr[i - k];
-            runningSum -= remove;
-            double incoming = arr[i];
-            runningSum += incoming;
-            if (runningSum >= (double) (k * threshold)) {
+            runningSum += (arr[i] - arr[i - k]);
+            if (runningSum >= k * threshold) {
                 count++;
             }
         }
