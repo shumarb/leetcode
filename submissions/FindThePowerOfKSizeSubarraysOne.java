969b56acdcbf9b1d84abe178bbcab0e8f5cc7a2b
Umar <shumarb@outlook.com>
2025-06-14 13:02:07 +0800

    Find the Power of K-Size Subarrays I: Used count of consecutive differences of ones for improved performance (Runtime: 2ms -> 1ms; Rank: 62.24% -> 100%)

--- submissions/FindThePowerOfKSizeSubarraysOne.java
+++ submissions/FindThePowerOfKSizeSubarraysOne.java
@@ -2,20 +2,34 @@
 
 class FindThePowerOfKSizeSubarraysOne {
     public int[] resultsArray(int[] nums, int k) {
+        int countConsecutiveDifferenceOfOne = 0;
         int len = nums.length;
+        int m = 0;
         int[] result = new int[len - k + 1];
 
-        for (int i = 0; i <= len - k; i++) {
-            boolean isValid = true;
-            for (int j = 1; j < k; j++) {
-                int current = nums[i + j];
-                int previous = nums[i + j - 1];
-                if (current - previous != 1) {
-                    isValid = false;
-                    break;
-                }
+        for (int i = 1; i < k; i++) {
+            int current = nums[i];
+            int previous = nums[i - 1];
+            if (current - previous == 1) {
+                countConsecutiveDifferenceOfOne++;
             }
-            result[i] = isValid ? nums[i + k - 1] : -1;
+        }
+        result[m++] = (countConsecutiveDifferenceOfOne == k - 1) ? nums[k - 1] : -1;
+
+        for (int i = k; i < len; i++) {
+            int remove = nums[i - k];
+            int elementBeforeRemove = nums[i - k + 1];
+            if (elementBeforeRemove - remove == 1) {
+                countConsecutiveDifferenceOfOne--;
+            }
+
+            int incoming = nums[i];
+            int elementBeforeIncoming = nums[i - 1];
+            if (incoming - elementBeforeIncoming == 1) {
+                countConsecutiveDifferenceOfOne++;
+            }
+
+            result[m++] = (countConsecutiveDifferenceOfOne == k - 1) ? incoming : -1;
         }
 
         return result;
