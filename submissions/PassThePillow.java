b5baacb0e39ca8f6ba25dfecc7a073a9992a92e7
Umar <shumarb@outlook.com>
2025-06-18 00:23:29 +0800

    Pass the Pillow: Refactored solution resulted in same performance

--- submissions/PassThePillow.java
+++ submissions/PassThePillow.java
@@ -2,8 +2,22 @@
 
 class PassThePillow {
     public int passThePillow(int n, int time) {
-        int completedCycles = time / (n - 1);
-        int remainder = time % (n - 1);
-        return completedCycles % 2 == 1 ? n - remainder: remainder + 1;
+        boolean isIncrement = true;
+        int current = 1;
+
+        while (time-- > 0) {
+            if (isIncrement) {
+                current++;
+            } else {
+                current--;
+            }
+            if (current == n) {
+                isIncrement = false;
+            } else if (current == 1) {
+                isIncrement = true;
+            }
+        }
+
+        return current;
     }
 }
