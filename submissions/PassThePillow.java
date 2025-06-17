2460d185e62d0cfa35132c0bac7cfe8e12257a1b
Umar <shumarb@outlook.com>
2025-06-18 00:31:56 +0800

    Pass the Pillow: Reduced number of checks via modulo division of time and completed cycles resulted in same performance

--- submissions/PassThePillow.java
+++ submissions/PassThePillow.java
@@ -3,7 +3,14 @@
 class PassThePillow {
     public int passThePillow(int n, int time) {
         boolean isIncrement = true;
+        int completedCycles = 2 * (n - 1);
         int current = 1;
+
+        /**
+         1.  Modulo divide time by number of completed cycles
+             to reduce number of checks.
+         */
+        time %= completedCycles;
 
         while (time-- > 0) {
             if (isIncrement) {
