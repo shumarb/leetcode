337aaf125b6df52bbc81ffd780522b2704645490
Umar <shumarb@outlook.com>
2025-06-18 12:41:32 +0800

    Find the Child Who Has the Ball After K Seconds: Reduced k by number of completed cycles for optimisation; resubmission resulted in same performance

--- submissions/FindTheChildWhoHasTheBallAfterKSeconds.java
+++ submissions/FindTheChildWhoHasTheBallAfterKSeconds.java
@@ -4,11 +4,14 @@ class FindTheChildWhoHasTheBallAfterKSeconds {
     public int numberOfChild(int n, int k) {
         boolean isIncrement = true;
         boolean isTest = false;
+        int countCompletedCycles = 2 * (n - 1);
         int current = 0;
 
         if (isTest) {
             System.out.println("n: " + n + "\nk: " + k);
         }
+
+        k %= countCompletedCycles;
         while (k-- > 0) {
             if (isIncrement) {
                 current++;
