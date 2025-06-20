a435d04da31dec3515ba96922935b0c7289c77c6
Umar <shumarb@outlook.com>
2025-06-20 11:19:34 +0800

    Generate Tag for Video Caption: Added handling of edge cases for optimisation; Resubmission resulted in same runtime and lower rank (Rank: 62.27% -> 62.01%)

--- submissions/GenerateTagForVideoCaption.java
+++ submissions/GenerateTagForVideoCaption.java
@@ -3,20 +3,26 @@
 class GenerateTagForVideoCaption {
     public String generateTag(String caption) {
         String[] words = caption.trim().split("\\s+");
-        StringBuilder result = new StringBuilder("#");
+        StringBuilder tag = new StringBuilder("#");
+
+        if (words.length == 0) {
+            return "";
+        } else if (words.length == 1 && words[0] == "") {
+            return "#";
+        }
 
         if (!words[0].isEmpty()) {
-            result.append(words[0].toLowerCase());
+            tag.append(words[0].toLowerCase());
         }
-        for (int i = 1; i < words.length && result.length() < 100; i++) {
+        for (int i = 1; i < words.length && tag.length() < 100; i++) {
             if (words[i].isEmpty()) {
                 continue;
             }
             String word = words[i];
-            result.append(Character.toUpperCase(word.charAt(0)));
-            result.append(word.substring(1).toLowerCase());
+            tag.append(Character.toUpperCase(word.charAt(0)));
+            tag.append(word.substring(1).toLowerCase());
         }
 
-        return result.substring(0, Math.min(100, result.length()));
+        return tag.substring(0, Math.min(100, tag.length()));
     }
 }
