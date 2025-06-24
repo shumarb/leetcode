fb14a04d83780e7e3dc57d2f196446543ccaae50
Umar <shumarb@outlook.com>
2025-06-24 22:20:50 +0800

    New submission: Plus One

--- submissions/PlusOne.java
+++ submissions/PlusOne.java
@@ -2,45 +2,34 @@
 
 class PlusOne {
     public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        int[] result = new int[len];
-        boolean isTest = false;
-        if (isTest) {
-            System.out.println("digits: " + Arrays.toString(digits));
-        }
+        public int[] plusOne(int[] digits) {
+            boolean isTest = false;
+            int carry = 1;
+            int len = digits.length;
 
-        for (int i = len - 1; i >= 0; i--) {
-            // 1. Current digit is < 9, so increase it for final answer
-            if (digits[i] < 9) {
-                digits[i]++;
-                if (isTest) {
-                    System.out.println("returning: " + Arrays.toString(digits));
-                }
-                return digits;
+            for (int i = len - 1; i >= 0; i--) {
+                int sum = carry + digits[i];
+                digits[i] = sum % 10;
+                carry = sum / 10;
 
-                // 2. Since current digit == 9, there is a carry over of 1 to the next most-significant digit
-                // and current digit becomes 0
-            } else {
-                digits[i] = 0;
             }
             if (isTest) {
-                System.out.println("digits so far: " + Arrays.toString(digits));
+                System.out.println("digits: " + Arrays.toString(digits) + "\ncarry: " + carry);
             }
-        }
 
-        if (isTest) {
-            System.out.println("digits so far: " + Arrays.toString(digits));
-        }
+            if (carry == 0) {
+                return digits;
+            }
 
-        // 3. All digits in initial array are 9, so there is a carry-over of 1 for every digit
-        // hence, all digits are currently 0
-        // so create a new array is to be created where the most-significant bit is 1 and all others are 0.
-        int[] newNumber = new int[len + 1];
-        newNumber[0] = 1;
-        if (isTest) {
-            System.out.println("returning: " + Arrays.toString(newNumber));
-        }
+            int[] result = new int[len + 1];
+            result[0] = carry;
+            for (int i = 1; i < result.length; i++) {
+                result[i] = digits[i - 1];
+            }
+            if (isTest) {
+                System.out.println("result: " + Arrays.toString(result));
+            }
 
-        return newNumber;
+            return result;
+        }
     }
-}
