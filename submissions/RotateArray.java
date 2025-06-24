4a8ce3e9dd5e075d858b5aa3fe63ab3c7dbb86a5
Umar <shumarb@outlook.com>
2025-06-24 22:35:43 +0800

    Rotate Array: Reattempt resulted in same performance

--- submissions/RotateArray.java
+++ submissions/RotateArray.java
@@ -1,25 +1,33 @@
 // Question: https://leetcode.com/problems/rotate-array/description/
 
 class RotateArray {
-    private int[] nums;
-
     public void rotate(int[] nums, int k) {
-        this.nums = nums;
-        int n = nums.length;
+        boolean isTest = false;
+        int left = 0;
+        int len = nums.length;
+        int right = len - 1;
 
-        // 1. Modulo divide k by n to reduce redundant rotations.
-        k %= n;
+        if (isTest) {
+            System.out.println("k: " + k + "\nbefore, nums: " + Arrays.toString(nums));
+        }
 
-        reverse(0, n - 1);
-        reverse(0, k - 1);
-        reverse(k, n - 1);
+        // 1. Eliminate redundant rotations.
+        k %= len;
+        reverse(0, len - 1, nums);
+        reverse(0, k - 1, nums);
+        reverse(k, len - 1, nums);
+        if (isTest) {
+            System.out.println("after, nums:  " + Arrays.toString(nums));
+        }
     }
 
-    private void reverse(int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start++] = nums[end];
-            nums[end--] = temp;
+    private void reverse(int start, int end, int[] nums) {
+        int left = start;
+        int right = end;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
         }
     }
 }
