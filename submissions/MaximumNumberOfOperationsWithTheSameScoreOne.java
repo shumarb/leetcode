c4d54b55e3d232da9fd2eb03545a483f68024bfb
Umar <shumarb@outlook.com>
2025-06-25 16:24:11 +0800

    Maximum Number of Operations With the Same Score I: Iterated in nums array instead of creating a queue for improved performance (Runtiem: 1ms -> 0ms; Rank: 7.09% -> 100%)

--- submissions/MaximumNumberOfOperationsWithTheSameScoreOne.java
+++ submissions/MaximumNumberOfOperationsWithTheSameScoreOne.java
@@ -2,24 +2,26 @@
 
 class MaximumNumberOfOperationsWithTheSameScoreOne {
     public int maxOperations(int[] nums) {
-        Queue<Integer> queue = new LinkedList<>();
-        int countOperations = 0;
-        int score = 0;
+        boolean isTest = false;
+        int countOperations = 1;
+        int i = 2;
+        int score = nums[0] + nums[1];
 
-        for (int number: nums) {
-            queue.offer(number);
+        if (isTest) {
+            System.out.println("nums: " + Arrays.toString(nums));
+            System.out.println("-----------------------------------------------");
+            System.out.println("indices: 0, 1" + " | elements: " + nums[0] + ", " + nums[1]);
+            System.out.println("score: " + score + " | sum: " + (nums[0] + nums[1]));
         }
-        while (queue.size() > 1) {
-            int first = queue.poll();
-            int second = queue.poll();
-            if (score == 0) {
-                score = first + second;
-            } else {
-                if (first + second != score) {
-                    break;
-                }
+
+        while (i + 1 < nums.length && nums[i] + nums[i + 1] == score) {
+            if (isTest) {
+                System.out.println("-----------------------------------------------");
+                System.out.println("indices: " + i + ", " + (i + 1) + " | elements: " + nums[i] + ", " + nums[i + 1]);
+                System.out.println("score: " + score + " | sum: " + (nums[i] + nums[i + 1]));
             }
             countOperations++;
+            i += 2;
         }
 
         return countOperations;
