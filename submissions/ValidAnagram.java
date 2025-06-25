174a629ddf53a0ea3c88134c7591115094124830
Umar <shumarb@outlook.com>
2025-06-25 21:37:42 +0800

    Valid Anagram: Refactored solution resulted in improved performance (Runtime: 2ms -> 1ms; Rank: 97.84% -> 99.65%)

--- submissions/ValidAnagram.java
+++ submissions/ValidAnagram.java
@@ -2,25 +2,20 @@
 
 class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        // 1. Invalid anagram for strings of different lengths.
-        if (s.length() != t.length()) {
-            return false;
+        int[] frequency = new int[26];
+
+        for (char letter: s.toCharArray()) {
+            frequency[letter - 'a']++;
+        }
+        for (char letter: t.toCharArray()) {
+            frequency[letter - 'a']--;
+        }
+        for (int freq: frequency) {
+            if (freq != 0) {
+                return false;
+            }
         }
 
-        /** 2.  Count letters in each string, and check if both are equal one another.
-         If both are equal to another, result is valid anagrams.
-         Otherwise, result is invalid anagrams.
-         */
-        int[] sLetterFrequency = countLettersInString(s);
-        int[] tLetterFrequency = countLettersInString(t);
-        return Arrays.equals(sLetterFrequency, tLetterFrequency);
-    }
-
-    private int[] countLettersInString(String str) {
-        int[] letterFrequency = new int[26];
-        for (char letter: str.toCharArray()) {
-            letterFrequency[letter - 'a']++;
-        }
-        return letterFrequency;
+        return true;
     }
 }
