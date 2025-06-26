f0718ec1fd878512ba64a1ffe318a26245fe76c6
Umar <shumarb@outlook.com>
2025-06-27 01:20:00 +0800

    Find Eventual Safe States: Refactored solution resulted in same performance

--- submissions/FindEventualSafeStates.java
+++ submissions/FindEventualSafeStates.java
@@ -4,11 +4,11 @@ class FindEventualSafeStates {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> result = new ArrayList<>();
         int len = graph.length;
-        int[] visited = new int[len];
+        int[] safety = new int[len]; // 0: unvisted, 1: not confirmed; 2: safe.
         boolean isTest = false;
 
         for (int i = 0; i < len; i++) {
-            if (isSafe(i, graph, visited)) {
+            if (isSafe(i, graph, safety)) {
                 result.add(i);
             }
         }
@@ -18,35 +18,42 @@ class FindEventualSafeStates {
                 System.out.println(" * vertex " + i + " -> " + Arrays.toString(graph[i]));
             }
             System.out.println("--------------------------------------------------------");
-            System.out.println("visited: " + Arrays.toString(visited));
+            System.out.println("safety: " + Arrays.toString(safety));
+            System.out.println("result: " + result);
         }
 
         return result;
     }
 
-    private boolean isSafe(int i, int[][] graph, int[] visited) {
+    private boolean isSafe(int i, int[][] graph, int[] safety) {
         // 1. Current path is a cycle, hence it has no terminal node.
-        if (visited[i] == 1) {
+        if (safety[i] == 1) {
             return false;
         }
 
-        // 2. Current path has a terminal node, so its root vertex is safe.
-        if (visited[i] == 2) {
+        // 2. Current node is a terminal node.
+        if (graph[i].length == 0) {
+            safety[i] = 2;
             return true;
         }
 
-        // 3. Mark current node as visited but ongoing checks to do.
-        visited[i] = 1;
+        // 3. Current path has a terminal node, so its root vertex is safe.
+        if (safety[i] == 2) {
+            return true;
+        }
 
-        // 4. Visit unvisited neighbours of i and return false if current path is a cycle.
+        // 4. Mark current node as visited but ongoing checks to do.
+        safety[i] = 1;
+
+        // 5. Visit unvisited neighbours of i and return false if current path is a cycle.
         for (int j: graph[i]) {
-            if (!isSafe(j, graph, visited)) {
+            if (!isSafe(j, graph, safety)) {
                 return false;
             }
         }
 
-        // 5. Vertex i is safe because it forms a path that leads to a terminal path.
-        visited[i] = 2;
+        // 6. Vertex i is safe because it forms a path that leads to a terminal path.
+        safety[i] = 2;
 
         return true;
     }
