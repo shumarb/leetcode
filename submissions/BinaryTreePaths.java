011fa58c6ebb4ab20f6799a1af5dbcc57a9d6d4e
Umar <shumarb@outlook.com>
2025-06-27 10:34:14 +0800

    Binary Tree Paths: Used StringBuilder for path for improved performance (Runtime: 2ms -> 1ms; Rank: 79.95% -> 98.67%)

--- submissions/BinaryTreePaths.java
+++ submissions/BinaryTreePaths.java
@@ -16,52 +16,37 @@
  * }
  */
 class BinaryTreePaths {
-    private List<List<Integer>> allRootToLeafPaths = new ArrayList<>();
-
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<>();
         boolean isTest = false;
 
-        findAllRootToLeafPaths(root, new ArrayList<>());
-
-        for (List<Integer> path: allRootToLeafPaths) {
-            StringBuilder incoming = new StringBuilder();
-            for (int i = 0; i < path.size(); i++) {
-                incoming.append(path.get(i));
-                if (i < path.size() - 1) {
-                    incoming.append("->");
-                }
-            }
-            result.add(incoming.toString());
-        }
+        findBinaryTreePaths(root, new StringBuilder(), result);
         if (isTest) {
-            System.out.println("all root-to-leaf-paths:");
-            for (List<Integer> path: allRootToLeafPaths) {
-                System.out.println(" * " + path);
-            }
-            System.out.println("-------------------------------------------");
             System.out.println("result: " + result);
         }
 
         return result;
     }
 
-    private void findAllRootToLeafPaths(TreeNode current, List<Integer> path) {
+    private void findBinaryTreePaths(TreeNode current, StringBuilder path, List<String> result) {
         if (current == null) {
             return;
         }
 
-        path.add(current.val);
+        // 1. Store length of path before further checks for backtracking.
+        int len = path.length();
+        path.append(current.val);
 
-        // 1. Path from root to leaf found, so add it to list of all root-to-leaf-paths.
+        // 2. Leaf reached, so a binary path is found.
         if (current.left == null && current.right == null) {
-            allRootToLeafPaths.add(new ArrayList<>(path));
+            result.add(path.toString());
         } else {
-            findAllRootToLeafPaths(current.left, path);
-            findAllRootToLeafPaths(current.right,path);
+            path.append("->");
+            findBinaryTreePaths(current.left, path, result);
+            findBinaryTreePaths(current.right, path, result);
         }
 
-        // 2. Backtrack by eliminating last-inserted node and traverse previous path.
-        path.remove(path.size() - 1);
+        // 3. Backtrack by setting path as it's value before recently-added vertex.
+        path.setLength(len);
     }
 }
