2f36c86fd270c1a4ce15892d13ffdb3b76382f6d
Umar <shumarb@outlook.com>
2025-06-30 16:37:57 +0800

    Word Subset: Refactored solution for same performance

--- submissions/WordSubsets.java
+++ submissions/WordSubsets.java
@@ -4,14 +4,19 @@ class WordSubsets {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         List<String> result = new ArrayList<>();
         boolean isTest = false;
-
         int[] frequency = new int[26];
+
         for (String w: words2) {
             int[] count = new int[26];
             for (char letter: w.toCharArray()) {
                 count[letter - 'a']++;
             }
             for (int i = 0; i < 26; i++) {
+                /**
+                     1.  A word in words 1 is universal if, for each letter,
+                     its count in the word is at least the maximum frequency of that letter
+                     in any single word in words2.
+                 */
                 frequency[i] = Math.max(frequency[i], count[i]);
             }
         }
@@ -32,11 +37,10 @@ class WordSubsets {
                 System.out.println(" * frequency:  " + Arrays.toString(frequency));
                 System.out.println("-----------------------------------------------------------------------------------------");
             }
-            if (isValid(frequency, wFrequency)) {
+            if (isUniversal(frequency, wFrequency)) {
                 result.add(word);
             }
         }
-
         if (isTest) {
             System.out.println("result: " + result);
         }
@@ -44,7 +48,7 @@ class WordSubsets {
         return result;
     }
 
-    private boolean isValid(int[] word2Frequency, int[] word1Frequency) {
+    private boolean isUniversal(int[] word2Frequency, int[] word1Frequency) {
         for (int i = 0; i < 26; i++) {
             if (word2Frequency[i] > 0 && word1Frequency[i] < word2Frequency[i]) {
                 return false;
