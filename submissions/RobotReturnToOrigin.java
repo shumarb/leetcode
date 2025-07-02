839a5f6bd7952256183fdf2934e5802983dacac4
Umar <shumarb@outlook.com>
2025-07-02 11:09:49 +0800

    Robot Return to Origin: Updated frequency of each character and checked that number of up and downs are equal and number of left and right are equal for improved performance (Runtime: 5ms -> 2ms; Rank: 54.51% -> 100%)

--- submissions/RobotReturnToOrigin.java
+++ submissions/RobotReturnToOrigin.java
@@ -2,30 +2,11 @@
 
 class RobotReturnToOrigin {
     public boolean judgeCircle(String moves) {
-        boolean isTest = false;
-        int[] coordinates = new int[2];
-
-        if (isTest) {
-            System.out.println("moves: " + moves);
-            System.out.println("before, coordinates: " + Arrays.toString(coordinates));
-            System.out.println("----------------------------------------------------------");
-        }
+        int[] frequency = new int[26];
         for (char letter: moves.toCharArray()) {
-            if (letter == 'U') {
-                coordinates[0]++;
-            } else if (letter == 'D') {
-                coordinates[0]--;
-            } else if (letter == 'L') {
-                coordinates[1]--;
-            } else {
-                coordinates[1]++;
-            }
+            frequency[letter - 'A']++;
         }
-        if (isTest) {
-            System.out.println("after, coordinates: " + Arrays.toString(coordinates));
-            System.out.println("----------------------------------------------------------");
-        }
-
-        return coordinates[0] == 0 && coordinates[1] == 0;
+        return frequency['U' - 'A'] == frequency['D' - 'A']
+                && frequency['L' - 'A'] == frequency['R' - 'A'];
     }
 }
