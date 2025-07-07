2435e2c56c659b57ad74effb20c1761f6cddee25
Umar <shumarb@outlook.com>
2025-07-07 15:55:48 +0800

    Maximum Difference Between Node and Ancestor: Refactored solution resulted in improved performance (Runtime: 2ms -> 1ms; Rank: 12.75% -> 14.22%)

--- submissions/MaximumDifferenceBetweenNodeAndAncestor.java
+++ submissions/MaximumDifferenceBetweenNodeAndAncestor.java
@@ -16,40 +16,11 @@
  * }
  */
 class MaximumDifferenceBetweenNodeAndAncestor {
-    private List<List<Integer>> paths;
+    private int maxAncestorDiff = 0;
 
     public int maxAncestorDiff(TreeNode root) {
         boolean isTest = false;
-        int maxAncestorDiff = 0;
-        paths = new ArrayList<>();
-
         helper(root, new ArrayList<>());
-        if (isTest) {
-            System.out.println("paths:");
-            for (List<Integer> path: paths) {
-                System.out.println(" * " + path);
-            }
-            System.out.println("--------------------------------");
-        }
-        for (List<Integer> path: paths) {
-            int maximum = Integer.MIN_VALUE;
-            int minimum = Integer.MAX_VALUE;
-            for (int value: path) {
-                maximum = Math.max(maximum, value);
-                minimum = Math.min(minimum, value);
-            }
-            if (isTest) {
-                System.out.println(" * path: " + path);
-                System.out.println(" ** maximum: " + maximum + ", minimum: " + minimum);
-                System.out.println(" *** before, maxAncestorDiff: " + maxAncestorDiff + " | compare: " + (maximum - minimum));
-            }
-            maxAncestorDiff = Math.max(maxAncestorDiff, maximum - minimum);
-            if (isTest) {
-                System.out.println(" *** after, maxAncestorDiff: " + maxAncestorDiff);
-                System.out.println("--------------------------------");
-            }
-        }
-
         return maxAncestorDiff;
     }
 
@@ -60,12 +31,17 @@ class MaximumDifferenceBetweenNodeAndAncestor {
 
         path.add(node.val);
         if (node.left == null && node.right == null) {
-            paths.add(new ArrayList<>(path));
+            int maximum = Integer.MIN_VALUE;
+            int minimum = Integer.MAX_VALUE;
+            for (int value: path) {
+                maximum = Math.max(maximum, value);
+                minimum = Math.min(minimum, value);
+            }
+            maxAncestorDiff = Math.max(maxAncestorDiff, maximum - minimum);
         }
 
         helper(node.left, path);
         helper(node.right, path);
         path.removeLast();
-
     }
 }
