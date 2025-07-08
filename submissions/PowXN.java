0aef790284be6f2ec839ee5c2f3ec7c4c68c9ace
Umar <shumarb@outlook.com>
2025-07-08 20:12:16 +0800

    Pow(x, n): Refactored solution resulted in same performance

--- submissions/PowXN.java
+++ submissions/PowXN.java
@@ -14,7 +14,7 @@ class PowXN {
         return helper(x, n);
     }
 
-    private double helper(double x, long n) {
+    private double helper(double x, int n) {
         if (n == 0) {
             return 1;
         }
