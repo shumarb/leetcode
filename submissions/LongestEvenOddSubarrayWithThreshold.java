7768e79fee84424f5a66bcef8aee61e6c672d33f
Umar <shumarb@outlook.com>
2025-07-09 12:42:17 +0800

    Longest Even Odd Subarray With Threshold: Used two pointers for improved performance (Runtime: 11ms -> 4ms; Rank: 48.95% -> 98.16%)

--- submissions/LongestEvenOddSubarrayWithThreshold.java
+++ submissions/LongestEvenOddSubarrayWithThreshold.java
@@ -2,20 +2,20 @@
 
 class LongestEvenOddSubarrayWithThreshold {
     public int longestAlternatingSubarray(int[] nums, int threshold) {
+        int len = nums.length;
         int longest = 0;
 
-        for (int l = 0; l < nums.length; l++) {
-            if (nums[l] % 2 != 0 || nums[l] > threshold) {
-                continue;
-            }
-
-            // 1. Handle single-sized nums.
-            longest = Math.max(longest, 1);
-            for (int r = l + 1; r < nums.length; r++) {
-                if (nums[r] > threshold || (nums[r] % 2 == nums[r - 1] % 2)) {
-                    break;
+        for (int l = 0; l < len; ) {
+            if (nums[l] <= threshold && nums[l] % 2 == 0) {
+                int r = l + 1;
+                while (r < len && nums[r] <= threshold && nums[r - 1] % 2 != nums[r] % 2) {
+                    r++;
                 }
-                longest = Math.max(longest, r - l + 1);
+                longest = Math.max(longest, r - l);
+                l = r;
+
+            } else {
+                l++;
             }
         }
 
