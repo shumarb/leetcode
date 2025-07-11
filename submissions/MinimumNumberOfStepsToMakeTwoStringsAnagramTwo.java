e27ea3b4afe46adee0241dfe6bffe382678e3e88
Umar <shumarb@outlook.com>
2025-07-11 10:40:54 +0800

    Minimum Number of Steps to Make Two Strings Anagram II: Reduced number of frequency arrays used from 2 to 1 for same performance

--- submissions/MinimumNumberOfStepsToMakeTwoStringsAnagramTwo.java
+++ submissions/MinimumNumberOfStepsToMakeTwoStringsAnagramTwo.java
@@ -1,34 +1,25 @@
 // Question: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/description/
 
-class Solution {
+class MinimumNumberOfStepsToMakeTwoStringsAnagramTwo {
     public int minSteps(String s, String t) {
         boolean isTest = false;
-        int[] sFrequency = getFrequency(s);
-        int[] tFrequency = getFrequency(t);
         int minSteps = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (sFrequency[i] != tFrequency[i]) {
-                minSteps += Math.abs(sFrequency[i] - tFrequency[i]);
-            }
-        }
-        if (isTest) {
-            System.out.println("s: " + s + "\nt: " + t);
-            System.out.println("sFrequency: " + Arrays.toString(sFrequency));
-            System.out.println("tFrequency: " + Arrays.toString(tFrequency));
-            System.out.println("minSteps: " + minSteps);
-        }
-
-        return minSteps;
-    }
-
-    private int[] getFrequency(String s) {
         int[] frequency = new int[26];
 
         for (char letter: s.toCharArray()) {
             frequency[letter - 'a']++;
         }
+        for (char letter: t.toCharArray()) {
+            frequency[letter - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            minSteps += Math.abs(frequency[i]);
+        }
+        if (isTest) {
+            System.out.println("s: " + s + "\nt: " + t);
+            System.out.println("frequency: " + Arrays.toString(frequency) + "\nminSteps: " + minSteps);
+        }
 
-        return frequency;
+        return minSteps;
     }
 }
