97eb669bf728a97423022181c1b9983d68e2606f
Umar <shumarb@outlook.com>
2025-07-11 12:26:54 +0800

    Count Vowel Substrings of a String: Refactored solution for improved performance (Runtime: 95ms -> 15ms; Rank: 5.48% -> 38.53%)

--- submissions/CountVowelSubstringsOfAString.java
+++ submissions/CountVowelSubstringsOfAString.java
@@ -2,41 +2,31 @@
 
 class CountVowelSubstringsOfAString {
     public int countVowelSubstrings(String word) {
-        boolean isTest = false;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
         int countVowelSubstrings = 0;
         int len = word.length();
 
-        for (int i = 0; i < word.length(); i++) {
-            if ("aeiou".indexOf(word.charAt(i)) != -1) {
-                int j = i + 1;
-                while (j < len && "aeiou".indexOf(word.charAt(j)) != -1) {
-                    if (isValid(word.substring(i, j + 1))) {
-                        if (isTest) {
-                            System.out.println(" * valid: " + word.substring(i, j + 1));
-                        }
-                        countVowelSubstrings++;
-                    }
-                    j++;
+        if (len < 5) {
+            return countVowelSubstrings;
+        }
+        for (int i = 0; i < len; i++) {
+            if (!vowels.contains(word.charAt(i))) {
+                continue;
+            }
+
+            Set<Character> seen = new HashSet<>();
+            for (int j = i; j < len; j++) {
+                char c = word.charAt(j);
+                if (!vowels.contains(c)) {
+                    break;
+                }
+                seen.add(c);
+                if (seen.size() == 5) {
+                    countVowelSubstrings++;
                 }
             }
         }
 
         return countVowelSubstrings;
-    }
-
-    private boolean isValid(String word) {
-        int[] frequency = new int[26];
-
-        for (char letter: word.toCharArray()) {
-            frequency[letter - 'a']++;
-        }
-        for (char letter: word.toCharArray()) {
-            if ("aeiou".indexOf(letter) == -1 && frequency[letter - 'a'] > 0) {
-                return false;
-            }
-        }
-
-        return frequency['a' - 'a'] > 0 && frequency['e' - 'a'] > 0
-                && frequency['i' - 'a'] > 0 && frequency['o' - 'a'] > 0 && frequency['u' - 'a'] > 0;
     }
 }
