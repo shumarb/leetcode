c50c37e309bd85b155655278d86b493dc15fc3b9
Umar <shumarb@outlook.com>
2025-07-13 16:35:55 +0800

    Invalid Transactions: Replaced set with boolean array for improved runtime (925ms -> 899ms)

--- submissions/InvalidTransactions.java
+++ submissions/InvalidTransactions.java
@@ -3,14 +3,14 @@
 class InvalidTransactions {
     public List<String> invalidTransactions(String[] transactions) {
         List<String> result = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
+        boolean[] isInvalid = new boolean[transactions.length];
         boolean isTest = false;
         int len = transactions.length;
 
         for (int i = 0; i < len; i++) {
             String[] t1 = transactions[i].split(",");
             if (Integer.parseInt(t1[2]) > 1000) {
-                set.add(i);
+                isInvalid[i] = true;
             }
             for (int j = 0; j < len; j++) {
                 if (i == j) {
@@ -18,21 +18,21 @@ class InvalidTransactions {
                 }
                 String[] t2 = transactions[j].split(",");
                 if (t1[0].equals(t2[0]) && !t1[3].equals(t2[3]) && Math.abs(Integer.parseInt(t1[1]) - Integer.parseInt(t2[1])) <= 60) {
-                    set.add(i);
-                    set.add(j);
+                    isInvalid[i] = true;
+                    isInvalid[j] = true;
                 }
             }
         }
-        for (int index: set) {
-            result.add(transactions[index]);
+        for (int i = 0; i < len; i++) {
+            if (isInvalid[i]) {
+                result.add(transactions[i]);
+            }
         }
         if (isTest) {
             System.out.println("transactions:");
             for (int i = 0; i < len; i++) {
                 System.out.println(" * " + i + " --> " + transactions[i]);
             }
-            System.out.println("---------------------------------------------------------");
-            System.out.println("indices of invalid transactions: " + set);
             System.out.println("---------------------------------------------------------");
             System.out.println("result: " + result);
         }
