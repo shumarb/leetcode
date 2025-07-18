c5258c11cd071f2d433a22c51d6426e2b8951c37
Umar <shumarb@outlook.com>
2025-07-19 00:05:06 +0800

    Subarrays Distinct Element Sum of Squares I: Refactored solution resulted in same performance

--- submissions/SubarraysDistinctElementSumOfSquaresOne.java
+++ submissions/SubarraysDistinctElementSumOfSquaresOne.java
@@ -2,49 +2,30 @@
 
 class SubarraysDistinctElementSumOfSquaresOne {
     public int sumCounts(List<Integer> nums) {
-        boolean isTest = false;
+        int maximum = 0;
         int n = nums.size();
         int sumCounts = 0;
 
-        if (isTest) {
-            System.out.println("nums: " + nums);
+        for (int number: nums) {
+            maximum = Math.max(maximum, number);
         }
         for (int i = 0; i < n; i++) {
-            boolean[] isPresent = new boolean[101];
+            /**
+             1.  Use boolean array to keep track of distinct arrays
+                 in all subarrays starting from index i to n.
+             */
+            boolean[] isPresent = new boolean[maximum + 1];
             int countDistinct = 0;
             for (int j = i; j < n; j++) {
                 int element = nums.get(j);
                 if (!isPresent[element]) {
+                    isPresent[element] = true;
                     countDistinct++;
                 }
-                isPresent[element] = true;
                 sumCounts += (countDistinct * countDistinct);
             }
         }
-        if (isTest) {
-            System.out.println("-----------------------------");
-            System.out.println("sumCounts: " + sumCounts);
-        }
 
         return sumCounts;
-    }
-
-    private int countDistinct(List<Integer> list) {
-        boolean[] isPresent;
-        int countDistinct = 0;
-        int maximum = 0;
-
-        for (int number: list) {
-            maximum = Math.max(maximum, number);
-        }
-        isPresent = new boolean[maximum + 1];
-        for (int number: list) {
-            if (!isPresent[number]) {
-                isPresent[number] = true;
-                countDistinct++;
-            }
-        }
-
-        return countDistinct;
     }
 }
