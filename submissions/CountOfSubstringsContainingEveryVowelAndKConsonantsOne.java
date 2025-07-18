64379ca405bdb6c9d8b0774ba70dd1ee65feefb7
Umar <shumarb@outlook.com>
2025-07-19 00:22:28 +0800

    Count of Substrings Containing Every Vowel and K Consonants I: Refactored solution resulted in improved runtime

--- submissions/CountOfSubstringsContainingEveryVowelAndKConsonantsOne.java
+++ submissions/CountOfSubstringsContainingEveryVowelAndKConsonantsOne.java
@@ -1,16 +1,15 @@
 // Question: https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-i/description/
 
-class CountOfSubstringsContainingEveryVowelAndKConsonantsOne {
+class CountOfSubstringsContainingEveryVowelAndKConsonantsOne class Solution {
     public int countOfSubstrings(String word, int k) {
         boolean isTest = false;
         int countOfSubstrings = 0;
         int len = word.length();
 
         for (int i = 0; i < len; i++) {
-            StringBuilder current = new StringBuilder();
             for (int j = i; j < len; j++) {
-                current.append(word.charAt(j));
-                if (current.length() >= 5 && isValid(current.toString(), k)) {
+                String current = word.substring(i, j + 1);
+                if (current.length() >= 5 && isValid(current, k)) {
                     if (isTest) {
                         System.out.println(" * valid: " + current.toString());
                     }
@@ -25,6 +24,7 @@ class CountOfSubstringsContainingEveryVowelAndKConsonantsOne {
     private boolean isValid(String word, int k) {
         int[] frequency = new int[26];
         int countConsonants = 0;
+
         for (char c: word.toCharArray()) {
             frequency[c - 'a']++;
             if (!isVowel(c)) {
@@ -32,20 +32,15 @@ class CountOfSubstringsContainingEveryVowelAndKConsonantsOne {
             }
         }
 
-        if (countConsonants != k) {
-            return false;
-        }
-
-        if (frequency['a' - 'a'] == 0 || frequency['e' - 'a'] == 0 ||
+        if (countConsonants != k || frequency['a' - 'a'] == 0 || frequency['e' - 'a'] == 0 ||
                 frequency['i' - 'a'] == 0 || frequency['o' - 'a'] == 0 || frequency['u' - 'a'] == 0) {
             return false;
-
         }
 
         return true;
     }
 
     private boolean isVowel(char c) {
-        return "aeiou".indexOf(c) >= 0;
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
