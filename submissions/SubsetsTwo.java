fe272ae84b4a5198ca5b9b5b65fbb71776c4b9b5
Umar <shumarb@outlook.com>
2025-07-20 15:51:10 +0800

    Subsets: Refactored solution; resubmission resulted in same performance

--- submissions/SubsetsTwo.java
+++ submissions/SubsetsTwo.java
@@ -7,11 +7,11 @@ class SubsetsTwo {
         boolean isTest = false;
         result = new ArrayList<>();
 
-        Arrays.sort(nums);
+        nums = sort(nums);
         helper(new ArrayList<>(), nums, 0);
         if (isTest) {
-            System.out.println("nums: " + Arrays.toString(nums));
-            System.out.println("-------------------------\nresult:");
+            System.out.println("sorted nums: " + Arrays.toString(nums));
+            System.out.println("----------------------------\nresult:");
             for (List<Integer> subset: result) {
                 System.out.println(subset);
             }
@@ -26,15 +26,32 @@ class SubsetsTwo {
         for (int i = index; i < nums.length; i++) {
             /**
              1.  Skip current element if it is the
-                 the first element of an earlier subset
-                 that has been added to result.
+             the first element of an earlier subset
+             that has been added to result.
              */
             if (i > index && nums[i] == nums[i - 1]) {
                 continue;
             }
+
             subset.add(nums[i]);
             helper(subset, nums, i + 1);
             subset.removeLast();
         }
+    }
+
+    private int[] sort(int[] nums) {
+        int[] frequency = new int[21];
+        int j = 0;
+
+        for (int number: nums) {
+            frequency[number + 10]++;
+        }
+        for (int i = 0; i <= 20; i++) {
+            while (frequency[i]-- > 0) {
+                nums[j++] = i - 10;
+            }
+        }
+
+        return nums;
     }
 }
