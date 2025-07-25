da68ae60031aabafc8d631242986166b2db4f68b
Umar <shumarb@outlook.com>
2025-07-25 10:08:21 +0800

    Combinations: Refactored solution resulted in same performance

--- submissions/Combinations.java
+++ submissions/Combinations.java
@@ -2,50 +2,47 @@
 
 class Combinations {
     private List<List<Integer>> result;
+    private int n;
 
     public List<List<Integer>> combine(int n, int k) {
-        boolean[] isVisited = new boolean[n + 1];
         boolean isTest = false;
-        isTest = false;
         result = new ArrayList<>();
+        this.n = n;
 
         for (int i = 1; i <= n; i++) {
-            if (!isVisited[i]) {
-                helper(i, k, new ArrayList<>(), isVisited);
-            }
+            helper(i, new ArrayList<>(), new boolean[n + 1], k);
         }
         if (isTest) {
             System.out.println("n: " + n + "\nk: " + k);
-            System.out.println("----------------------------\nresult:");
-            for (List<Integer> e: result) {
-                System.out.println(e);
+            System.out.println("-----------------------\nresult:");
+            for (List<Integer> path: result) {
+                System.out.println(" * " + path);
             }
         }
 
         return result;
     }
 
-    private void helper(int i, int k, List<Integer> path, boolean[] isVisited) {
-        isVisited[i] = true;
+    private void helper(int i, List<Integer> path, boolean[] isVisited, int k) {
         path.add(i);
+        isVisited[i] = true;
 
-        // 1. Valid permutation created.
         if (path.size() == k) {
             result.add(new ArrayList<>(path));
-
-        } else {
-            /**
-                2.  Explore current path until valid permutation formed.
-             */
-            for (int j = i + 1; j < isVisited.length; j++) {
-                if (!isVisited[j]) {
-                    helper(j, k, path, isVisited);
-                }
-            }
+            return;
         }
 
-        // 3. Backtrack to form alternative permutations of current path.
-        isVisited[i] = false;
-        path.removeLast();
+        for (int j = i + 1; j <= n; j++) {
+            if (!isVisited[j]) {
+                /**
+                 1.  Explore existing path if an unvisited number encountered
+                     until a valid path is found, then backtrack to explore
+                     remaining unvisited numbers.
+                 */
+                helper(j, path, isVisited, k);
+                path.removeLast();
+                isVisited[j] = false;
+            }
+        }
     }
 }
