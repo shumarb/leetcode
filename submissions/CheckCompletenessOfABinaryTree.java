93757911de5ea1acad2c3622eeca4abe6a064ecb
Umar <shumarb@outlook.com>
2025-08-02 16:31:06 +0800

    Check Completeness of a Binary Tree: Refactored solution for improved performance

--- submissions/CheckCompletenessOfABinaryTree.java
+++ submissions/CheckCompletenessOfABinaryTree.java
@@ -15,78 +15,27 @@
  *     }
  * }
  */
-class Solution {
+class CheckCompletenessOfABinaryTree {
     public boolean isCompleteTree(TreeNode root) {
-        List<String> list = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
-        boolean isTest = false;
-        int deepestLevel = dfs(root) - 1;
-        int level = 0;
-
-        if (isTest) {
-            System.out.println("deepest level: " + deepestLevel);
-            System.out.println("-------------------------------------------------------");
-        }
+        boolean isNullFoundEarlier = false;
 
         queue.offer(root);
-        if (root == null) {
-            list.add("null");
-        } else {
-            list.add(Integer.toString(root.val));
-        }
-
         while (!queue.isEmpty()) {
-            if (isTest) {
-                System.out.println("level: " + level + ": " + list);
+            TreeNode current = queue.poll();
+
+            if (current == null) {
+                isNullFoundEarlier = true;
+                continue;
+
+            } else if (current != null && isNullFoundEarlier) {
+                return false;
             }
 
-            if (level != deepestLevel) {
-                for (int i = list.size() - 1; i >= 0; i--) {
-                    if (list.get(i).equals("null")) {
-                        return false;
-                    }
-                }
-            } else {
-                for (int i = 0; i < list.size() - 1; i++) {
-                    if (list.get(i).equals("null") && !list.get(i + 1).equals("null")) {
-                        return false;
-                    }
-                }
-            }
-
-            list.clear();
-            level++;
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode top = queue.poll();
-                if (top.left == null) {
-                    list.add("null");
-                } else {
-                    list.add(Integer.toString(top.left.val));
-                }
-                if (top.right == null) {
-                    list.add("null");
-                } else {
-                    list.add(Integer.toString(top.right.val));
-                }
-
-                if (top.left != null) {
-                    queue.offer(top.left);
-                }
-                if (top.right != null) {
-                    queue.offer(top.right);
-                }
-            }
+            queue.offer(current.left);
+            queue.offer(current.right);
         }
 
         return true;
-    }
-
-    private int dfs(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return 1 + Math.max(dfs(root.left), dfs(root.right));
     }
 }
