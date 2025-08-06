ee4bef9c30e42a13774564d1800c3b1bac641b05
Umar <shumarb@outlook.com>
2025-08-07 00:14:38 +0800

    Maximum Binary Tree: Refactored solution resulted in improved performnace

--- submissions/MaximumBinaryTree.java
+++ submissions/MaximumBinaryTree.java
@@ -16,41 +16,23 @@
  * }
  */
 class MaximumBinaryTree {
-    private int[] map;
-    private int[] nums;
-
-    private TreeNode constructMaximumBinaryTree(int left, int right) {
+    private TreeNode constructMaximumBinaryTree(int[] nums, int left, int right) {
         if (left > right) {
             return null;
         }
 
-        int maximum = nums[left];
         int maximumIndex = left;
-
         for (int i = left + 1; i <= right; i++) {
-            if (nums[i] > maximum) {
-                maximum = nums[i];
+            if (nums[i] > nums[maximumIndex]) {
                 maximumIndex = i;
             }
         }
 
         TreeNode root = new TreeNode(nums[maximumIndex]);
-        root.left = constructMaximumBinaryTree(left, maximumIndex - 1);
-        root.right = constructMaximumBinaryTree(maximumIndex + 1, right);
+        root.left = constructMaximumBinaryTree(nums, left, maximumIndex - 1);
+        root.right = constructMaximumBinaryTree(nums, maximumIndex + 1, right);
 
         return root;
-    }
-
-    private void populate() {
-        int largest = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            largest = Math.max(largest, nums[i]);
-        }
-        map = new int[largest + 1];
-        for (int i = 0; i < nums.length; i++) {
-            map[nums[i]] = i;
-        }
     }
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
@@ -59,9 +41,6 @@ class MaximumBinaryTree {
             return new TreeNode(nums[0]);
         }
 
-        this.nums = nums;
-        populate();
-
-        return constructMaximumBinaryTree(0, nums.length - 1);
+        return constructMaximumBinaryTree(nums, 0, nums.length - 1);
     }
 }
