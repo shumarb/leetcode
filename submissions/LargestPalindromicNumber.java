8eb9a7b4bf2e56da2b9c9580d1d176165b6c8e74
Umar <shumarb@outlook.com>
2025-08-08 11:17:44 +0800

    Largest Palindromic Number: Refactored solution resulted in same runtime and lower rank

--- submissions/LargestPalindromicNumber.java
+++ submissions/LargestPalindromicNumber.java
@@ -4,8 +4,8 @@ class LargestPalindromicNumber {
     public String largestPalindromic(String num) {
         String center = "";
         String right = "";
-        StringBuilder result = new StringBuilder();
         StringBuilder left = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         boolean isTest = false;
         int[] frequency = new int[10];
 
@@ -14,45 +14,46 @@ class LargestPalindromicNumber {
         }
         if (isTest) {
             System.out.println("num: " + num + "\nbefore, frequency: " + Arrays.toString(frequency));
+            System.out.println("-----------------------------------------------------------------------------");
         }
 
         for (int digit = 9; digit >= 0; digit--) {
             while (frequency[digit] >= 2) {
                 /**
-                 1.  Edge case: num comprises of only zeros.
-                     Left is empty. It can't be appended with zeros
-                     as this means leading zeros are counted, so break.
+                 1.  Edge case: Num comprises of only zeros,
+                 so the answer is 0.
                  */
                 if (digit == 0 && left.isEmpty()) {
                     break;
                 }
+
+                /**
+                 2.  Add digit to left, and since digits in left are in right,
+                 reduce frequency by 2.
+                 */
                 left.append(digit);
                 frequency[digit] -= 2;
             }
         }
 
         /**
-         2.  Search for center digit (if any). It is the largest digit with frequency == 1.
-             At this point, remaining frequency of each digit is either 0 or 1.
+         3.  Center is either empty or 1 digit, which is the largest digit with frequency of 1 after earlier iteration.
          */
-        for (int digit = 9; digit >= 0; digit--) {
+        for (int digit = 9; digit >=0 ; digit--) {
             if (frequency[digit] == 1) {
-                center = Integer.toString(digit);
+                center = String.valueOf(digit);
                 break;
             }
         }
 
         right = left.reverse().toString();
         left.reverse();
-        if (isTest) {
-            System.out.println("---------------------------------------------------------------------------");
-            System.out.println("after, frequency: " + Arrays.toString(frequency));
-            System.out.println("left: " + left + "\ncenter: " + center + "\nright: " + right);
-        }
         result = left.append(center).append(right);
         if (isTest) {
-            System.out.println("---------------------------------------------------------------------------");
-            System.out.println("result: " + result.toString());
+            System.out.println("after, frequency: " + Arrays.toString(frequency));
+            System.out.println("left: " + left + "\ncenter: " + center + "\nright: " + right);
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("result: " + result);
         }
 
         return result.isEmpty() ? "0" : result.toString();
