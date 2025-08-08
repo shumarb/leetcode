87acd6b438f6c424a883623ba5d70f36c21cf85b
Umar <shumarb@outlook.com>
2025-08-09 00:58:47 +0800

    Construct Binary Search Tree from Preorder Traversal: Refactored solution resulted in improved performance

--- submissions/ConstructBinarySearchTreeFromPreorderTraversal.java
+++ submissions/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -16,68 +16,26 @@
  * }
  */
 class ConstructBinarySearchTreeFromPreorderTraversal {
-    private int[] inorderIndexMap;
-    private int[] inorder;
     private int[] preorder;
-    private int i;
-    private int largest;
+    private int rootIndex;
 
-    public TreeNode bstFromPreorder(int[] preorder) {
-        // 1. Edge case: 1 element in tree.
-        if (preorder.length == 1) {
-            return new TreeNode(preorder[0]);
-        }
-
-        boolean isTest = false;
-        i = 0;
-        inorder = sort(preorder.clone());
-        inorderIndexMap = new int[largest + 1];
-        this.preorder = preorder;
-
-        for (int i = 0; i < inorder.length; i++) {
-            inorderIndexMap[inorder[i]] = i;
-        }
-        if (isTest) {
-            System.out.println("preorder: " + Arrays.toString(preorder));
-            System.out.println("inorder:  " + Arrays.toString(inorder) + "\ninorderIndexMap: " + Arrays.toString(inorderIndexMap));
-        }
-
-        return build(0, preorder.length - 1);
-    }
-
-    private int[] sort(int[] arr) {
-        int[] frequency;
-        int j = 0;
-        largest = arr[0];
-
-        for (int number: arr) {
-            largest = Math.max(largest, number);
-        }
-        frequency = new int[largest + 1];
-        for (int number: arr) {
-            frequency[number]++;
-        }
-
-        for (int i = 0; i <= largest; i++) {
-            while (frequency[i]-- > 0) {
-                arr[j++] = i;
-            }
-        }
-
-        return arr;
-    }
-
-    private TreeNode build(int left, int right) {
-        if (left > right) {
+    private TreeNode bstFromPreorder(int lowerBound, int upperBound) {
+        if (rootIndex == preorder.length || preorder[rootIndex] < lowerBound || preorder[rootIndex] > upperBound) {
             return null;
         }
 
-        int rootValue = preorder[i++];
+        int rootValue = preorder[rootIndex++];
         TreeNode root = new TreeNode(rootValue);
-        int rootInorderIndex = inorderIndexMap[rootValue];
-        root.left = build(left, rootInorderIndex - 1);
-        root.right = build(rootInorderIndex + 1, right);
+        root.left = bstFromPreorder(lowerBound, rootValue);
+        root.right = bstFromPreorder(rootValue, upperBound);
 
         return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        rootIndex = 0;
+        this.preorder = preorder;
+
+        return bstFromPreorder(Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
