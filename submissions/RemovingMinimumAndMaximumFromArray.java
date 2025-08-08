ac5d1950f78ed3b126e643a02b92bf9dcee4e557
Umar <shumarb@outlook.com>
2025-08-08 12:23:06 +0800

    Removing Minimum and Maximum From Array: Refactored solution resulted in same performance

--- submissions/RemovingMinimumAndMaximumFromArray.java
+++ submissions/RemovingMinimumAndMaximumFromArray.java
@@ -1,7 +1,6 @@
 // Question: https://leetcode.com/problems/removing-minimum-and-maximum-from-array/description/
 
 class RemovingMinimumAndMaximumFromArray {
-    public int minimumDeletions(int[] nums) {
         boolean isTest = false;
         int leftMost = 0;
         int len = nums.length;
@@ -14,7 +13,7 @@ class RemovingMinimumAndMaximumFromArray {
 
         /**
          1.  Edge case: nums has <= 2 elements,
-         so minimum number of moves is all elements.
+             so minimum number of moves is all elements.
          */
         if (len <= 2) {
             return len;
@@ -23,8 +22,7 @@ class RemovingMinimumAndMaximumFromArray {
         for (int i = 0; i < len; i++) {
             if (nums[i] > nums[maxIndex]) {
                 maxIndex = i;
-            }
-            if (nums[i] < nums[minIndex]) {
+            } else if (nums[i] < nums[minIndex]) {
                 minIndex = i;
             }
         }
@@ -36,13 +34,13 @@ class RemovingMinimumAndMaximumFromArray {
             System.out.println("leftMost: " + leftMost + "\nrightMost: " + rightMost);
         }
 
-        // 1. Remove from left.
+        // 2. Remove from left.
         option1 = rightMost + 1;
 
-        // 2. Remove from right.
+        // 3. Remove from right.
         option2 = len - leftMost;
 
-        // 3. Remove one by one (left followed by right, or right followed by left).
+        // 4. Remove one by one (left followed by right, or right followed by left).
         option3 = (leftMost + 1) + (len - rightMost);
 
         return Math.min(option1, Math.min(option2, option3));
