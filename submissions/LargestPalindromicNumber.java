cc90d947d5734fbee486d4d6ba7a43f25b5703a3
Umar <shumarb@outlook.com>
2025-08-08 11:32:47 +0800

    Largest Palindromic Number: Edited comment indentation

--- submissions/LargestPalindromicNumber.java
+++ submissions/LargestPalindromicNumber.java
@@ -20,16 +20,16 @@ class LargestPalindromicNumber {
         for (int digit = 9; digit >= 0; digit--) {
             while (frequency[digit] >= 2) {
                 /**
-                 1.  Edge case: Num comprises of only zeros,
-                 so the answer is 0.
+                    1.  Edge case: Num comprises of only zeros,
+                        so the answer is 0.
                  */
                 if (digit == 0 && left.isEmpty()) {
                     break;
                 }
 
                 /**
-                 2.  Add digit to left, and since digits in left are in right,
-                 reduce frequency by 2.
+                    2.  Add digit to left, and since digits in left are in right,
+                        reduce frequency by 2.
                  */
                 left.append(digit);
                 frequency[digit] -= 2;
@@ -37,7 +37,7 @@ class LargestPalindromicNumber {
         }
 
         /**
-         3.  Center is either empty or 1 digit, which is the largest digit with frequency of 1 after earlier iteration.
+            3.  Center is either empty or 1 digit, which is the largest digit with frequency of 1 after earlier iteration.
          */
         for (int digit = 9; digit >=0 ; digit--) {
             if (frequency[digit] == 1) {
