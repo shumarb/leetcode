f5cd5ada2859bffa9a5fd447ee0a78737313df58
Umar <shumarb@outlook.com>
2025-08-10 18:25:08 +0800

    Check if Array Is Sorted and Rotated: Refactored solution resulted in same performance

--- submissions/CheckIfArrayIsSortedAndRotated.java
+++ submissions/CheckIfArrayIsSortedAndRotated.java
@@ -1,23 +1,23 @@
 // Question: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
 
 class CheckIfArrayIsSortedAndRotated {
-    public boolean check(int[] nums) {
-        int count = 0;
-        int len = nums.length;
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] frequency = new int[1001];
+        int len = target.length;
 
         /**
-         1.  Compare 2 elements at indices i and (i + 1) % len
-             and count the number of times element decreases
-             from index i to index (i + 1) % len.
-             Sorted and rotated arrays have a count of <= 1.
+            1. Both arrays can be made equal if both have same elements.
          */
         for (int i = 0; i < len; i++) {
-            int next = (i + 1) % len;
-            if (nums[next] < nums[i]) {
-                count++;
+            frequency[target[i]]++;
+            frequency[arr[i]]--;
+        }
+        for (int count: frequency) {
+            if (count != 0) {
+                return false;
             }
         }
 
-        return count <= 1;
+        return true;
     }
 }
