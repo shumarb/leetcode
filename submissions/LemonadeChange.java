37fdc8662ae84d2b609376f030debf0e25381b47
Umar <shumarb@outlook.com>
2025-08-14 03:08:30 +0800

    Lemonade Change: Refactored solution resulted in same performance

--- submissions/LemonadeChange.java
+++ submissions/LemonadeChange.java
@@ -10,6 +10,8 @@ class LemonadeChange {
                 count5++;
 
             } else if (bill == 10) {
+                // 1. For $10 bill, return one $5 bill.
+                if (count5 >= 1) {
                     count10++;
                     count5--;
 
@@ -19,7 +21,12 @@ class LemonadeChange {
 
             } else {
                 /**
+                 2.  For $20 bill, return either
+                     - three $5 bills.
+                     - one $5 & one $10 bills.
+                     Prioritize giving one $5 and one $10 as more notes are available for future exchanges.
                  */
+                if (count5 >= 1 && count10 >= 1) {
                     count5--;
                     count10--;
 
