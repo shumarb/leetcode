c8563463fd7cc0f81cb368f186569ac346f483a0
Umar <shumarb@outlook.com>
2025-08-14 02:29:08 +0800

    Second Largest Digit in a String: Refactored solution resulted in improved performance

--- submissions/SecondLargestDigitInAString.java
+++ submissions/SecondLargestDigitInAString.java
@@ -2,24 +2,13 @@
 
 class SecondLargestDigitInAString {
     public int secondHighest(String s) {
-        boolean[] digitFrequency = new boolean[10];
-        boolean isTest = false;
-
-        for (char c: s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                digitFrequency[Character.getNumericValue(c)] = true;
-            }
-        }
-        if (isTest) {
-            System.out.println("s: " + s + "\ndigitFrequency: " + Arrays.toString(digitFrequency));
-        }
-
         int count = 0;
-        for (int i = digitFrequency.length - 1; i >= 0; i--) {
-            if (digitFrequency[i]) {
+
+        for (char digit = '9'; digit >= '0'; digit--) {
+            if (s.indexOf(digit) >= 0) {
                 count++;
                 if (count == 2) {
-                    return i;
+                    return digit - '0';
                 }
             }
         }
