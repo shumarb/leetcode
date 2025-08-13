ab9f07baa8094da50c27e28ae240c9c2645ee670
Umar <shumarb@outlook.com>
2025-08-14 00:52:46 +0800

    Reverse Bits: Refactored solution resulted in improved performance

--- submissions/ReverseBits.java
+++ submissions/ReverseBits.java
@@ -3,49 +3,36 @@
 class ReverseBits {
     public int reverseBits(int n) {
         List<Integer> list = new ArrayList<>();
-        String nBinary = Integer.toBinaryString(n);
+        StringBuilder nBinary = new StringBuilder(Integer.toBinaryString(n));
         boolean isTest = false;
-        int[] bits = new int[32];
+        char[] bits;
+        int power = 0;
+        int result = 0;
 
-        for (char digit: nBinary.toCharArray()) {
-            if (digit == '1') {
-                list.add(1);
-            } else {
-                list.add(0);
-            }
+        while (nBinary.length() < 32) {
+            nBinary.insert(0, '0');
         }
-        while (list.size() < 32) {
-            list.add(0, 0);
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            bits[i] = (list.get(i) == 1) ? 1 : 0;
-        }
+        bits = nBinary.toString().toCharArray();
         if (isTest) {
-            System.out.println("n: " + n + "\nnBinary: " + nBinary + "\nlist: " + list + "\nbefore swap, bits: " + Arrays.toString(bits));
+            System.out.println("nBinary: " + nBinary + "\nbefore, bits: " + Arrays.toString(bits));
         }
 
         int left = 0;
         int right = bits.length - 1;
         while (left < right) {
-            int temp = bits[left];
+            char temp = bits[left];
             bits[left++] = bits[right];
             bits[right--] = temp;
         }
-        if (isTest) {
-            System.out.println("after swap, bits:  " + Arrays.toString(bits));
-        }
 
-        double power = 0;
-        int result = 0;
         for (int i = bits.length - 1; i >= 0; i--) {
-            if (bits[i] == 1) {
+            if (bits[i] == '1') {
                 result += Math.pow(2.0, power);
             }
             power++;
         }
         if (isTest) {
-            System.out.println("result:  " + result);
+            System.out.println("after, bits:  " + Arrays.toString(bits) + "\nresult: " + result);
         }
 
         return result;
