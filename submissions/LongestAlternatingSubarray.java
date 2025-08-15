71efd83ebeb217d8761cb52e245d2d80cc61b18b
Umar <shumarb@outlook.com>
2025-08-15 12:23:07 +0800

    Longest Alternating Subarray: Refactored solution resulted in improved performance

--- submissions/LongestAlternatingSubarray.java
+++ submissions/LongestAlternatingSubarray.java
@@ -2,25 +2,45 @@
 
 class LongestAlternatingSubarray {
     public int alternatingSubarray(int[] nums) {
-        boolean isTest = false;
         int longestLength = -1;
         int n = nums.length;
 
-        for (int start = 0; start < n - 1; start++) {
-            if (nums[start + 1] - nums[start] != 1) {
+        for (int i = 0; i < n - 1; ) {
+            int current = nums[i];
+            int next = nums[i + 1];
+
+            if (next - current != 1) {
+                i++;
                 continue;
             }
 
+            /**
+             1.  [current, next] is an alternating subarray.
+                 Hence, it's current length is 2.
+                 Find the longest alternating subarray whose
+                 first two numbers are current and next,
+                 starting from the third number
+                 (first number is at index i --> third number is at index (i + 2)).
+             */
             int currentLength = 2;
-            for (int i = start + 2; i < n; i++) {
-                if (nums[i] == nums[i - 2]) {
-                    currentLength++;
-                } else {
+            int j = i + 2;
+            for (; j < n; j++) {
+                if (nums[j] != nums[j - 2]) {
                     break;
+                } else {
+                    currentLength++;
                 }
             }
 
             longestLength = Math.max(currentLength, longestLength);
+
+            /**
+                 2.  Optimization:
+                     Since the pattern broke at index j,
+                     we can start the next index from j - 1 (last valid index),
+                     as earlier indices won't lead to a longer valid subarray.
+             */
+            i = j - 1;
         }
 
         return longestLength;
