8f9dde13195a0f5c028b6e8ad4ccadb73ce02816
Umar <shumarb@outlook.com>
2025-08-16 16:29:13 +0800

    Minimum Operations to Make Binary Array Elements Equal to One I: Refactored solution resulted in improved performance

--- submissions/MinimumOperationsToMakeBinaryArrayElementsEqualToOneOne.java
+++ submissions/MinimumOperationsToMakeBinaryArrayElementsEqualToOneOne.java
@@ -7,15 +7,6 @@ class MinimumOperationsToMakeBinaryArrayElementsEqualToOneOne {
         int len = nums.length;
         int minOperations = 0;
 
-        for (int bit: nums) {
-            if (bit == 1) {
-                countOnes++;
-            }
-        }
-        if (countOnes == len) {
-            return 0;
-        }
-
         if (isTest) {
             System.out.println("before, nums: " + Arrays.toString(nums));
         }
@@ -38,21 +29,22 @@ class MinimumOperationsToMakeBinaryArrayElementsEqualToOneOne {
                 } else {
                     nums[i + 2] = 0;
                 }
+
                 if (isTest) {
                     System.out.println(" * so far, nums:  " + Arrays.toString(nums));
                 }
+
                 minOperations++;
             }
         }
-        if (isTest) {
-            System.out.println("after, nums:  " + Arrays.toString(nums));
-        }
 
-        countOnes = 0;
         for (int bit: nums) {
             if (bit == 1) {
                 countOnes++;
             }
+        }
+        if (isTest) {
+            System.out.println("after, nums:  " + Arrays.toString(nums));
         }
 
         return countOnes == len ? minOperations : -1;
