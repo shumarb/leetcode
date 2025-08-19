38e16fd1627ce72ca567b97eaa26ac9c20b510fd
Umar <shumarb@outlook.com>
2025-08-19 14:16:02 +0800

    Linked List Cycle: Refactored solution resulted in same performance

--- submissions/LinkedListCycle.java
+++ submissions/LinkedListCycle.java
@@ -18,22 +18,18 @@ public class LinkedListCycle {
             return false;
         }
 
-        ListNode slow = head;
+        /**
+         2.  Using slow and fast pointers,
+             If both pointers are pointing to the same node, a cycle exists.
+             If not, set slow to it's next pointer, and fast to its next's next's pointers.
+         */
         ListNode fast = head;
+        ListNode slow = head;
 
-        while (fast != null) {
-            // 2. No cycle.
-            if (fast.next == null) {
-                return false;
-            }
-
-            /**
-             3.  Update slow and fast pointers,
-                 and if both refer to the same node, a cycle exists.
-             */
-            slow = slow.next;
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            if (slow == fast) {
+            slow = slow.next;
+            if (fast == slow) {
                 return true;
             }
         }
