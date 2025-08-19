577ea56b6d6a12f105b77919460b66184b4b8ed1
Umar <shumarb@outlook.com>
2025-08-19 11:46:06 +0800

    Minimum Subsequence in Non-Increasing Order: Refactored solution resulted in improved performance

--- submissions/MinimumSubsequenceInNonIncreasingOrder.java
+++ submissions/MinimumSubsequenceInNonIncreasingOrder.java
@@ -4,56 +4,41 @@ class MinimumSubsequenceInNonIncreasingOrder {
     public List<Integer> minSubsequence(int[] nums) {
         List<Integer> result = new ArrayList<>();
         boolean isTest = false;
-        int i = nums.length - 1;
+        int[] frequency = new int[101];int largest = nums[0];
         int total = 0;
         int totalComplement = 0;
 
-        nums = sort(nums);
         for (int number: nums) {
+            frequency[number]++;
             total += number;
         }
-        if (isTest) {
-            System.out.println("nums: " + Arrays.toString(nums) + "\ntotal: " + total);
-            System.out.println("---------------------------------------------------");
-        }
 
-        while (total >= totalComplement) {
-            int current = nums[i];
-            total -= current;
-            totalComplement += current;
-            result.add(current);
-            if (isTest) {
-                System.out.println("total: " + total + ", totalComplement: " + totalComplement + ", result so far: " + result);
-                System.out.println("---------------------------------------------------");
+        for (int i = 100; i >= 0; i--) {
+            while (frequency[i]-- > 0) {
+                int number = i;
+                if (totalComplement <= total) {
+                    if (isTest) {
+                        System.out.println("number: " + number + "\n * total: " + total + ", totalComplement: " + totalComplement);
+                        System.out.println(" * before, result: " + result);
+                    }
+                    result.add(number);
+                    if (isTest) {
+                        System.out.println(" * after, result: " + result);
+                        System.out.println("--------------------------------------------");
+                    }
+
+                } else {
+                    break;
+                }
+
+                total -= number;
+                totalComplement += number;
             }
-            i--;
         }
         if (isTest) {
             System.out.println("result: " + result);
         }
 
         return result;
-    }
-
-    private int[] sort(int[] nums) {
-        int[] frequency;
-        int j = 0;
-        int largest = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            largest = Math.max(largest, nums[i]);
-        }
-        frequency = new int[largest + 1];
-        for (int number: nums) {
-            frequency[number]++;
-        }
-
-        for (int i = 1; i <= largest; i++) {
-            while (frequency[i]-- > 0) {
-                nums[j++] = i;
-            }
-        }
-
-        return nums;
     }
 }
