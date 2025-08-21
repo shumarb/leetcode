c5f7f502be30edfcb7fa0bca4f41c4c6712bc900
Umar <shumarb@outlook.com>
2025-08-22 00:18:58 +0800

    Check Array Formation Through Concatenation: Refactored solution resulted in improved performance

--- submissions/CheckArrayFormationThroughConcatenation.java
+++ submissions/CheckArrayFormationThroughConcatenation.java
@@ -2,14 +2,12 @@
 
 class CheckArrayFormationThroughConcatenation {
     public boolean canFormArray(int[] arr, int[][] pieces) {
-        Map<Integer, Integer> map = new HashMap<>();
         boolean isTest = false;
+        int[] map = new int[101];
 
+        Arrays.fill(map, -1);
         for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
-        }
-        if (isTest) {
-            System.out.println("map: " + map);
+            map[arr[i]] = i;
         }
 
         for (int[] piece: pieces) {
@@ -17,24 +15,36 @@ class CheckArrayFormationThroughConcatenation {
                 System.out.println("---------------------------------------");
                 System.out.println("piece: " + Arrays.toString(piece));
             }
+
+            /**
+             1.  For pieces of size 1, check if its element is in the map.
+             Return false if element is not in the map because array concatenation can't be formed
+             if element is not present.
+             */
             if (piece.length == 1) {
-                if (!map.containsKey(piece[0])) {
+                if (map[piece[0]] == -1) {
                     return false;
                 }
+
+                /**
+                 2.  For pieces with more than 1 element,
+                 compare two adjacent elements and ensure the indices of each element
+                 differs by exactly 1; otherwise, array concatenation can't be formed.
+                 */
             } else {
                 int previous = piece[0];
-                if (!map.containsKey(previous)) {
+                if (map[previous] == -1) {
                     return false;
                 }
 
                 for (int i = 1; i < piece.length; i++) {
                     int current = piece[i];
-                    if (!map.containsKey(current)) {
+                    if (map[current] == -1) {
                         return false;
                     }
 
-                    int currentIndex = map.get(current);
-                    int previousIndex = map.get(previous);
+                    int currentIndex = map[current];
+                    int previousIndex = map[previous];
 
                     if (isTest) {
                         System.out.println(" * current: " + current + " -> currentIndex: " + currentIndex);
