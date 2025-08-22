4704af08cfb34a8999738ca03c7d53caa309587f
Umar <shumarb@outlook.com>
2025-08-22 12:34:51 +0800

    Find the Width of Columns of a Grid: Refactored solution resulted in improved performance

--- submissions/FindTheWidthOfColumnsOfAGrid.java
+++ submissions/FindTheWidthOfColumnsOfAGrid.java
@@ -8,23 +8,37 @@ class FindTheWidthOfColumnsOfAGrid {
         int[] result = new int[n];
 
         for (int j = 0; j < n; j++) {
-            int longestWidth = Integer.MIN_VALUE;
             if (isTest) {
                 System.out.println("j: " + j);
             }
+
+            int longestWidth = Integer.MIN_VALUE;
             for (int i = 0; i < m; i++) {
-                int currentWidth = getWidth(grid[i][j]);
+                int current = grid[i][j];
+                int currentWidth = 0;
+
+                if (current == 0) {
+                    currentWidth = 1;
+                } else {
+                    while (current != 0) {
+                        currentWidth++;
+                        current /= 10;
+                    }
+                    if (grid[i][j] < 0) {
+                        currentWidth++;
+                    }
+                }
                 if (isTest) {
                     System.out.println(" * number: " + grid[i][j] + " -> width: " + currentWidth);
                 }
-                if (currentWidth > longestWidth) {
-                    longestWidth = currentWidth;
-                }
+
+                longestWidth = Math.max(currentWidth, longestWidth);
             }
             if (isTest) {
                 System.out.println(" ** longestWidth: " + longestWidth);
                 System.out.println("---------------------------------------");
             }
+
             result[j] = longestWidth;
         }
         if (isTest) {
@@ -32,27 +46,5 @@ class FindTheWidthOfColumnsOfAGrid {
         }
 
         return result;
-    }
-
-    private int getWidth(int n) {
-        int nCopy = n;
-        int width = 0;
-
-        if (n == 0) {
-            width = 1;
-
-        } else {
-            if (n < 0) {
-                width++;
-                nCopy *= -1;
-            }
-
-            while (nCopy != 0) {
-                width++;
-                nCopy /= 10;
-            }
-        }
-
-        return width;
     }
 }
