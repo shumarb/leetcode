e50489ae1edca81c7f8c54c9fd83d5ecb5f854f8
Umar <shumarb@outlook.com>
2025-08-26 15:56:12 +0800

    Distribute Candies to People: Refactored solution resulted in same performance

--- submissions/DistributeCandiesToPeople.java
+++ submissions/DistributeCandiesToPeople.java
@@ -1,4 +1,4 @@
-// Question:
+// Question: https://leetcode.com/problems/distribute-candies-to-people/description/
 
 class DistributeCandiesToPeople {
     public int[] distributeCandies(int candies, int num_people) {
@@ -14,21 +14,18 @@ class DistributeCandiesToPeople {
             }
 
             // 1. Edge case: candies < k, so update current person with remaining candies.
-            if (candies - k <= 0) {
+            if (candies < k) {
                 result[i] += candies;
                 candies = 0;
 
-           // 2. candies >= k, so increase current person's candies by k and decrement candies by k.
+                // 2. candies >= k, so increase current person's candies by k and decrement candies by k.
             } else {
                 result[i] += k;
                 candies -= k;
             }
 
             k++;
-            i++;
-            if (i >= n) {
-                i = 0;
-            }
+            i = (i + 1) % num_people;
 
             if (isTest) {
                 System.out.println(" * after: " + Arrays.toString(result) + ", candies: " + candies);
