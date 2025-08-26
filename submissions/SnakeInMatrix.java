e09cba2e1cb6661479b70888cc9c6b89f606fc92
Umar <shumarb@outlook.com>
2025-08-26 11:26:46 +0800

    Snake in Matrix: Refactored solution resulted in same performance and improved space complexity

--- submissions/SnakeInMatrix.java
+++ submissions/SnakeInMatrix.java
@@ -2,37 +2,21 @@
 
 class SnakeInMatrix {
     public int finalPositionOfSnake(int n, List<String> commands) {
-        boolean isTest = false;
-        int[][] grid = new int[n][n];
-        int k = 0;
-        int x = 0;
-        int y = 0;
+        int i = 0;
+        int j = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = k++;
-            }
-        }
         for (String command: commands) {
             if (command.equals("UP")) {
-                x--;
+                i--;
             } else if (command.equals("DOWN")) {
-                x++;
+                i++;
             } else if (command.equals("LEFT")) {
-                y--;
+                j--;
             } else if (command.equals("RIGHT")) {
-                y++;
+                j++;
             }
-        }
-        if (isTest) {
-            System.out.println("grid:");
-            for (int[] e: grid) {
-                System.out.println(Arrays.toString(e));
-            }
-            System.out.println("-------------------------------------------");
-            System.out.println("x: " + x + ", y: " + y + " -> answer: " + grid[x][y]);
         }
 
-        return grid[x][y];
+        return (i * n) + j;
     }
 }
