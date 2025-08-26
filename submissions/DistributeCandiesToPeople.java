68ba647d9a099c6e05720743ca88af41f5d511b0
Umar <shumarb@outlook.com>
2025-08-26 16:00:23 +0800

    Distribute Candies to People: Refactored solution resulted in same performance

--- submissions/DistributeCandiesToPeople.java
+++ submissions/DistributeCandiesToPeople.java
@@ -3,38 +3,27 @@
 class DistributeCandiesToPeople {
     public int[] distributeCandies(int candies, int num_people) {
         boolean isTest = false;
-        int i = 0;
-        int k = 1;
-        int n = num_people;
-        int[] result = new int[n];
+        int[] result = new int[num_people];
 
-        while (candies > 0) {
+        for (int give = 1, i = 0; candies > 0; give++, i++) {
+            if (i >= num_people) {
+                i = 0;
+            }
             if (isTest) {
-                System.out.println("i: " + i + " \n * before: " + Arrays.toString(result) + ", candies: " + candies);
+                System.out.println("i: " + i + "\nbefore: " + Arrays.toString(result) + ", candies: " + candies);
             }
 
-            // 1. Edge case: candies < k, so update current person with remaining candies.
-            if (candies < k) {
-                result[i] += candies;
-                candies = 0;
-
-                // 2. candies >= k, so increase current person's candies by k and decrement candies by k.
-            } else {
-                result[i] += k;
-                candies -= k;
-            }
-
-            k++;
-            i = (i + 1) % num_people;
+            result[i] += Math.min(give, candies);
+            candies -= give;
 
             if (isTest) {
-                System.out.println(" * after: " + Arrays.toString(result) + ", candies: " + candies);
-                System.out.println("----------------------------------------");
+                System.out.println("after: " + Arrays.toString(result) + ", candies: " + candies);
+                System.out.println("------------------------------------------");
             }
         }
         if (isTest) {
-            System.out.println("i: " + i + ", candies: " + candies);
-            System.out.println("final result: " + Arrays.toString(result));
+            System.out.println("final result: " + Arrays.toString(result) + ", candies: " + candies);
+            System.out.println("------------------------------------------");
         }
 
         return result;
