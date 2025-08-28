04e3258a49ddfc980c5e123c70cb8fe3e4b8ff03
Umar <shumarb@outlook.com>
2025-08-28 14:10:09 +0800

    Reconstruct Original Digits from English: Refactored solution resulted in improved performance

--- submissions/ReconstructOriginalDigitsFromEnglish.java
+++ submissions/ReconstructOriginalDigitsFromEnglish.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/reconstruct-original-digits-from-english/description/
 
-class Solution {
+class ReconstructOriginalDigitsFromEnglish {
     public String originalDigits(String s) {
         StringBuilder result = new StringBuilder();
         boolean isTest = false;
@@ -10,14 +10,10 @@ class Solution {
         for (char letter: s.toCharArray()) {
             letterFrequency[letter - 'a']++;
         }
-        if (isTest) {
-            System.out.println("s: " + s + "\nletterFrequency: " + Arrays.toString(letterFrequency));
-            System.out.println("digitFrequency: " + Arrays.toString(digitFrequency));
-        }
 
         /**
          1.  Certain letters are associated with one number,
-         so frequency of these letters = frequency of digit.
+             so frequency of these letters = frequency of digit.
          */
         digitFrequency[0] = letterFrequency['z' - 'a'];
         digitFrequency[2] = letterFrequency['w' - 'a'];
@@ -29,11 +25,15 @@ class Solution {
          2.  Idenfify remaining digits subtracting overlaps
              for each of remaining leftover digits.
          */
+        digitFrequency[1] = letterFrequency['o' - 'a'] - digitFrequency[0] - digitFrequency[2] - digitFrequency[4];
         digitFrequency[3] = letterFrequency['h' - 'a'] - digitFrequency[8];
         digitFrequency[5] = letterFrequency['f' - 'a'] - digitFrequency[4];
         digitFrequency[7] = letterFrequency['s' - 'a'] - digitFrequency[6];
-        digitFrequency[1] = letterFrequency['o' - 'a'] - digitFrequency[0] - digitFrequency[2] - digitFrequency[4];
         digitFrequency[9] = letterFrequency['i' - 'a'] - digitFrequency[5] - digitFrequency[6] - digitFrequency[8];
+        if (isTest) {
+            System.out.println("s: " + s + "\nletterFrequency: " + Arrays.toString(letterFrequency));
+            System.out.println("digitFrequency: " + Arrays.toString(digitFrequency));
+        }
 
         for (int i = 0; i < 10; i++) {
             while (digitFrequency[i]-- > 0) {
