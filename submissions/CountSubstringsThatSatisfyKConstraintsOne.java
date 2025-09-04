0eff1b6b82469edc52c08215b3eaa3e1258b938f
Umar <shumarb@outlook.com>
2025-09-05 00:03:34 +0800

    Count Substrings That Satisfy K-Constraint I: Refactored solution resulted in improved performance

--- submissions/CountSubstringsThatSatisfyKConstraintsOne.java
+++ submissions/CountSubstringsThatSatisfyKConstraintsOne.java
@@ -2,50 +2,27 @@
 
 class CountSubstringsThatSatisfyKConstraintsOne {
     public int countKConstraintSubstrings(String s, int k) {
-        boolean isTest = false;
-        int count = 0;
+        int total = 0;
 
         for (int i = 0; i < s.length(); i++) {
+            int count0 = 0;
+            int count1 = 0;
+
             for (int j = i; j < s.length(); j++) {
-                String substring = s.substring(i, j + 1);
-                if (substring.isEmpty()) {
-                    continue;
+                if (s.charAt(j) == '0') {
+                    count0++;
+                } else {
+                    count1++;
                 }
-                if (isTest) {
-                    System.out.println("substring: " + substring);
-                }
-                if (isValid(substring, k)) {
-                    if (isTest) {
-                        System.out.println(" * valid: " + substring);
-                    }
-                    count++;
+
+                if (count0 <= k || count1 <= k) {
+                    total++;
                 } else {
                     break;
                 }
             }
-            if (isTest) {
-                System.out.println("---------------------------------------------------");
-            }
-        }
-        if (isTest) {
-            System.out.println("count: " + count);
         }
 
-        return count;
-    }
-
-    private boolean isValid(String s, int k) {
-        int count0 = 0;
-        int count1 = 0;
-
-        for (char c: s.toCharArray()) {
-            if (c == '0') {
-                count0++;
-            } else {
-                count1++;
-            }
-        }
-
-        return count0 <= k || count1 <= k;
+        return total;
     }
 }
