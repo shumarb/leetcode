6a83bb8acc519b9dd427739763920db66d75a5b8
Umar <shumarb@outlook.com>
2025-09-05 12:14:54 +0800

    Shortest Subarray With OR at Least K I: Refactored solution resulted in improved performance

--- submissions/ShortestSubarrayWithOrAtLeastKOne.java
+++ submissions/ShortestSubarrayWithOrAtLeastKOne.java
@@ -2,25 +2,16 @@
 
 class ShortestSubarrayWithOrAtLeastKOne {
     public int minimumSubarrayLength(int[] nums, int k) {
-        boolean isTest = false;
         int shortest = Integer.MAX_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            if (isTest) {
-                System.out.println("-------------------------------------------------");
-            }
-            for (int j = i; j < nums.length; j++) {
-                int[] subarray = Arrays.copyOfRange(nums, i, j + 1);
-                int bitwiseOr = 0;
+            int bitwiseOr = 0;
 
-                for (int number: subarray) {
-                    bitwiseOr |= number;
-                }
+            for (int j = i; j < nums.length; j++) {
+                bitwiseOr |= nums[j];
                 if (bitwiseOr >= k) {
                     shortest = Math.min(j - i + 1, shortest);
-                }
-                if (isTest) {
-                    System.out.println("subarray: " + Arrays.toString(subarray) + " -> bitwiseOr: " + bitwiseOr + " | shortest: " + shortest);
+                    break;
                 }
             }
         }
