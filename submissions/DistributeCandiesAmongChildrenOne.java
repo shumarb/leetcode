6fff3ef3e31895ded6ac996a9797f83e26725360
Umar <shumarb@outlook.com>
2025-09-08 00:01:29 +0800

    Distribute Candies Among Children I: Refactored solution resulted in improved performance

--- submissions/DistributeCandiesAmongChildrenOne.java
+++ submissions/DistributeCandiesAmongChildrenOne.java
@@ -4,19 +4,11 @@ class DistributeCandiesAmongChildrenOne {
     public int distributeCandies(int n, int limit) {
         boolean isTest = false;
         int total = 0;
+        int upperBound = Math.min(n, limit);
 
-        for (int i = 0; i <= n; i++) {
-            if (i > limit) {
-                continue;
-            }
-            for (int j = 0; j <= n; j++) {
-                if (j > limit) {
-                    continue;
-                }
-                for (int k = 0; k <= n; k++) {
-                    if (k > limit) {
-                        continue;
-                    }
+        for (int i = 0; i <= upperBound; i++) {
+            for (int j = 0; j <= upperBound; j++) {
+                for (int k = 0; k <= upperBound; k++) {
                     if (i + j + k == n) {
                         total++;
                         if (isTest) {
