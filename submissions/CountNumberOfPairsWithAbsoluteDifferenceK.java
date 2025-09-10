7380531633959b97402335df909e579ca3f36c0c
Umar <shumarb@outlook.com>
2025-09-10 16:03:21 +0800

    Count Number of Pairs With Absolute Difference K: Refactored solution resulted in improved performance

--- submissions/CountNumberOfPairsWithAbsoluteDifferenceK.java
+++ submissions/CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -2,19 +2,36 @@
 
 class CountNumberOfPairsWithAbsoluteDifferenceK {
     public int countKDifference(int[] nums, int k) {
+        boolean isTest = false;
+        int[] map = formsMap(nums);
         int count = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int number: nums) {
-            map.put(number, 1 + map.getOrDefault(number, 0));
-        }
 
+        if (isTest) {
+            System.out.println("map: " + Arrays.toString(map));
+        }
         for (int number: nums) {
-            int complement = k + number;
-            if (map.containsKey(complement)) {
-                count += map.get(complement);
+            int complement = number + k;
+            if (complement >= 0 && complement < map.length && map[complement] > 0) {
+                count += map[complement];
             }
         }
 
         return count;
+    }
+
+    private int[] formsMap(int[] nums) {
+        int[] map;
+        int largest = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            largest = Math.max(largest, nums[i]);
+        }
+
+        map = new int[largest + 1];
+        for (int number: nums) {
+            map[number]++;
+        }
+
+        return map;
     }
 }
