c0f38c57c0ea21c3d9838b89adc522319bafe6bb
Umar <shumarb@outlook.com>
2025-09-16 00:17:06 +0800

    Maximum Area of Longest Diagonal Rectangle: Refactored solution resulted in improved performance

--- submissions/MaximumAreaOfLongestDiagonalRectangle.java
+++ submissions/MaximumAreaOfLongestDiagonalRectangle.java
@@ -1,42 +1,19 @@
 // Question: https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/description/
 
-class Solution {
+class MaximumAreaOfLongestDiagonalRectangle {
     public int areaOfMaxDiagonal(int[][] dimensions) {
-        if (dimensions.length == 1) {
-            return dimensions[0][0] * dimensions[0][1];
-        }
-
-        List<Dimension> list = new ArrayList<>();
-        boolean isTest = false;
+        double maximumDiagonal = 0;
+        int maximumArea = 0;
 
         for (int[] dimension: dimensions) {
-            list.add(new Dimension(dimension[0], dimension[1]));
-        }
-        list.sort((a, b) -> Double.compare(b.diagonal, a.diagonal) == 0
-                ? Integer.compare(b.area, a.area)
-                : Double.compare(b.diagonal, a.diagonal)
-        );
-        if (isTest) {
-            System.out.println("list:");
-            for (Dimension e: list) {
-                System.out.println("l: " + e.length + ", w: " + e.width + " -> d: " + e.diagonal + ", a: " + e.area);
+            double diagonal = Math.sqrt(dimension[0] * dimension[0] + dimension[1] * dimension[1]);
+            int area = dimension[0] * dimension[1];
+            if (diagonal > maximumDiagonal || diagonal == maximumDiagonal && area > maximumArea) {
+                maximumArea = area;
+                maximumDiagonal = diagonal;
             }
         }
 
-        return list.get(0).area;
-    }
-}
-
-class Dimension {
-    int area;
-    int length;
-    int width;
-    double diagonal;
-
-    public Dimension(int length, int width) {
-        this.area = length * width;
-        this.diagonal = Math.sqrt(length * length + width * width);
-        this.length = length;
-        this.width = width;
+        return maximumArea;
     }
 }
