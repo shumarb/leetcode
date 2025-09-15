16317bba3ee62824e177a75f8e870d912f753285
Umar <shumarb@outlook.com>
2025-09-16 00:03:10 +0800

    Largest Local Values in a Matrix: Refactored solution resulted in improved performance

--- submissions/LargestLocalValuesInAMatrix.java
+++ submissions/LargestLocalValuesInAMatrix.java
@@ -3,26 +3,26 @@
 class LargestLocalValuesInAMatrix {
     public int[][] largestLocal(int[][] grid) {
         int n = grid.length;
-        int startRow = 0;
         int[][] result = new int[n - 2][n - 2];
 
-        while (startRow <= n - 3) {
-            int startColumn = 0;
-            while (startColumn <= n - 3) {
-                int maximum = 0;
-
-                for (int i = startRow; i < (startRow + 3); i++) {
-                    for (int j = startColumn; j < (startColumn + 3); j++) {
-                        maximum = Math.max(maximum, grid[i][j]);
-                    }
-                }
-
-                result[startRow][startColumn] = maximum;
-                startColumn++;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                result[i][j] = findMaximum(grid, i, j);
             }
-            startRow++;
         }
 
         return result;
+    }
+
+    private int findMaximum(int[][] grid, int startRow, int startColumn) {
+        int maximum = 0;
+
+        for (int i = startRow; i < grid.length && i < startRow + 3; i++) {
+            for (int j = startColumn; j < grid[0].length && j < startColumn + 3; j++) {
+                maximum = Math.max(grid[i][j], maximum);
+            }
+        }
+
+        return maximum;
     }
 }
