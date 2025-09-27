7efb1b6e9ad9238d7c0fc204858dc2f946b514ae
Umar <shumarb@outlook.com>
2025-09-28 00:24:51 +0800

    Restore Finishing Order: Refactored solution resulted in improved performance

--- submissions/RestoreFinishingOrder.java
+++ submissions/RestoreFinishingOrder.java
@@ -2,30 +2,15 @@
 
 class RestoreFinishingOrder {
     public int[] recoverOrder(int[] order, int[] friends) {
-        PriorityQueue<Integer> heap;
-        boolean isTest = false;
-        int[] map = new int[order.length + 1];
         int[] result = new int[friends.length];
-        int i = 0;
+        int k = 0;
 
-        heap = new PriorityQueue<>(
-                (a, b) -> Integer.compare(map[a], map[b])
-        );
-        Arrays.fill(map, -1);
-
-        for (; i < order.length; i++) {
-            map[order[i]] = i;
-        }
-        for (int friend: friends) {
-            heap.add(friend);
-        }
-        i = 0;
-        while (!heap.isEmpty()) {
-            result[i++] = heap.poll();
-        }
-        if (isTest) {
-            System.out.println("map: " + Arrays.toString(map));
-            System.out.println("heap: " + heap + "\nresult: " + Arrays.toString(result));
+        for (int i = 0; i < order.length; i++) {
+            for (int j = 0; j < friends.length; j++) {
+                if (order[i] == friends[j]) {
+                    result[k++] = friends[j];
+                }
+            }
         }
 
         return result;
