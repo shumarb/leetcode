46e841d9d07b1ec3d112e0d55ccc570a4de0a287
Umar <shumarb@outlook.com>
2025-09-29 22:43:40 +0800

    Decompress Run-Length Encoded List: Refactored solution resulted in improved performance

--- submissions/DecompressRunLengthEncodedList.java
+++ submissions/DecompressRunLengthEncodedList.java
@@ -15,9 +15,8 @@ class DecompressRunLengthEncodedList {
         for (int i = 0; i < n; i += 2) {
             int freq = nums[i];
             int val = nums[i + 1];
-            for (int j = 0; j < freq; j++) {
-                result[k++] = val;
-            }
+            Arrays.fill(result, k, k + freq, val);
+            k += freq;
         }
         if (isTest) {
             System.out.println("totalFrequency: " + totalFrequency + "\nresult: " + Arrays.toString(result));
