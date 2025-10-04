6bdcb82ff9348eeb218b967c3c19b81d75058832
Umar <shumarb@outlook.com>
2025-10-05 00:58:16 +0800

    Design a Food Rating System: Refactored solution resulted in improved performance

--- submissions/FoodRatings.java
+++ submissions/FoodRatings.java
@@ -4,48 +4,37 @@ class FoodRatings {
     private Map<String, TreeSet<String>> cuisineFood;
     private Map<String, String> foodCuisine;
     private Map<String, Integer> foodRating;
-    private boolean isTest;
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
         cuisineFood = new HashMap<>();
         foodCuisine = new HashMap<>();
         foodRating = new HashMap<>();
-        isTest = false;
         int n = foods.length;
 
         for (int i = 0; i < n; i++) {
-            foodCuisine.put(foods[i], cuisines[i]);
-            foodRating.put(foods[i], ratings[i]);
-            if (!cuisineFood.containsKey(cuisines[i])) {
+            String cuisine = cuisines[i];
+            String food = foods[i];
+            int rating = ratings[i];
+
+            foodCuisine.put(food, cuisine);
+            foodRating.put(food, rating);
+            if (!cuisineFood.containsKey(cuisine)) {
                 TreeSet<String> foodTree = new TreeSet<>(
                         (a, b) -> Integer.compare(foodRating.get(b), foodRating.get(a)) == 0
                                 ? a.compareTo(b)
                                 : Integer.compare(foodRating.get(b), foodRating.get(a))
                 );
-                foodTree.add(foods[i]);
-                cuisineFood.put(cuisines[i], foodTree);
+                foodTree.add(food);
+                cuisineFood.put(cuisine, foodTree);
             } else {
-                TreeSet<String> foodTree = cuisineFood.get(cuisines[i]);
-                foodTree.add(foods[i]);
-                cuisineFood.put(cuisines[i], foodTree);
+                TreeSet<String> foodTree = cuisineFood.get(cuisine);
+                foodTree.add(food);
+                cuisineFood.put(cuisine, foodTree);
             }
-        }
-
-        if (isTest) {
-            System.out.println("foodCuisine: " + foodCuisine + "\nfoodRating: " + foodRating);
-            System.out.println("cuisineFood:");
-            for (Map.Entry<String, TreeSet<String>> e: cuisineFood.entrySet()) {
-                System.out.println(" * " + e.getKey() + " -> " + e.getValue());
-            }
-            System.out.println("-------------------------------------------------");
         }
     }
 
     public void changeRating(String food, int newRating) {
-        if (isTest) {
-            System.out.println(" * changeRating | food: " + food + ", newRating:" + newRating);
-            System.out.println("\n before, foodRating: " + foodRating);
-        }
         if (foodCuisine.containsKey(food)) {
             String cuisine = foodCuisine.get(food);
             Set<String> foodTree = cuisineFood.get(cuisine);
@@ -53,22 +42,9 @@ class FoodRatings {
             foodRating.put(food, newRating);
             foodTree.add(food);
         }
-        if (isTest) {
-            System.out.println("\n after, foodRating: " + foodRating);
-            System.out.println("-------------------------------------------------");
-        }
     }
 
     public String highestRated(String cuisine) {
-        if (isTest) {
-            System.out.println(" * highest rated | cuisine: " + cuisine);
-            System.out.println("cuisineFood:");
-            for (Map.Entry<String, TreeSet<String>> e: cuisineFood.entrySet()) {
-                System.out.println(" * " + e.getKey() + " -> " + e.getValue());
-            }
-            System.out.println("-------------------------------------------------");
-        }
-
         return cuisineFood.containsKey(cuisine) ? cuisineFood.get(cuisine).first() : null;
     }
 }
