1ebc8080071e298a870b2bad44cde37018a4a6bf
Umar <shumarb@outlook.com>
2025-10-07 11:30:32 +0800

    Number of Lines to Write String: Refactored solution resulted in improved performance

--- submissions/NumberOfLinesToWriteString.java
+++ submissions/NumberOfLinesToWriteString.java
@@ -1,33 +1,20 @@
 // Question: https://leetcode.com/problems/number-of-lines-to-write-string/description/
 
-class Solution {
+class NumberOfLinesToWriteString {
     public int[] numberOfLines(int[] widths, String s) {
-        boolean isTest = false;
-        int currentWidth = 0;
-        int totalLines = 0;
+        int totalLines = 1;
         int width = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char letter = s.charAt(i);
-            if (isTest) {
-                System.out.println("i: " + i + " -> letter: " + letter);
-                System.out.println(" * before, currentWidth: " + currentWidth + "\n * totalLines: " + totalLines);
-            }
-            if (currentWidth + widths[letter - 'a'] <= 100) {
-                currentWidth += widths[letter - 'a'];
-            } else {
+        for (char letter: s.toCharArray()) {
+            int letterWidth = widths[letter - 'a'];
+            if (width + letterWidth > 100) {
                 totalLines++;
-                currentWidth = widths[letter - 'a'];
+                width = letterWidth;
+            } else {
+                width += letterWidth;
             }
-            if (isTest) {
-                System.out.println(" * after, currentWidth: " + currentWidth + ", totalLines: " + totalLines);
-                System.out.println("-------------------------------------------------------");
-            }
-        }
-        if (currentWidth > 0) {
-            totalLines++;
         }
 
-        return new int[] {totalLines, currentWidth};
+        return new int[] {totalLines, width};
     }
 }
