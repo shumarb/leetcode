ee5a0d05cf6a1e10060a8fd14a8c8e1625b3b9d2
Umar <shumarb@outlook.com>
2025-10-07 10:57:06 +0800

    Find the Prefix Common Array of Two Arrays: Refactored solution resulted in improved performance

--- submissions/FindThePrefixCommonArrayOfTwoArrays.java
+++ submissions/FindThePrefixCommonArrayOfTwoArrays.java
@@ -1,14 +1,12 @@
 // Question: https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/
 
 class FindThePrefixCommonArrayOfTwoArrays {
-    private int[] frequency;
-    private int largest;
-
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         boolean isTest = false;
+        int largest = Integer.MIN_VALUE;
         int n = A.length;
         int[] C = new int[n];
-        largest = A[0];
+        int[] frequency;
 
         for (int number: A) {
             largest = Math.max(largest, number);
@@ -19,28 +17,22 @@ class FindThePrefixCommonArrayOfTwoArrays {
         frequency = new int[largest + 1];
 
         for (int i = 0; i < n; i++) {
-            C[i] = update(A, B, i);
+            int count = 0;
+
+            frequency[A[i]]++;
+            frequency[B[i]]++;
+            for (int j = 1; j <= largest; j++) {
+                if (frequency[j] > 1) {
+                    count++;
+                }
+            }
+
+            C[i] = count;
         }
         if (isTest) {
             System.out.println("C: " + Arrays.toString(C));
         }
 
         return C;
-    }
-
-    private int update(int[] A, int[] B, int index) {
-        int count = 0;
-        int n = A.length;
-
-        frequency[A[index]]++;
-        frequency[B[index]]++;
-
-        for (int i = 0; i <= largest; i++) {
-            if (frequency[i] > 1) {
-                count++;
-            }
-        }
-
-        return count;
     }
 }
