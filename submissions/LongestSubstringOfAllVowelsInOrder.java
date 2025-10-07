07ddf34eed96c5f506cdd5723657f9321adee5f4
Umar <shumarb@outlook.com>
2025-10-07 11:12:55 +0800

    Longest Substring Of All Vowels in Order: Refactored solution resulted in improved performance

--- submissions/LongestSubstringOfAllVowelsInOrder.java
+++ submissions/LongestSubstringOfAllVowelsInOrder.java
@@ -7,58 +7,27 @@ class LongestSubstringOfAllVowelsInOrder {
             return 0;
         }
 
+        int count = 1;
+        int current = 1;
         int longest = 0;
         int n = word.length();
 
-        for (int i = 0; i < n - 1; ) {
-            /**
-             2.  Move to next character if it does not start with 'a'
-                 or for every adjacent pair of letters, the first letter > second letter
-             */
-            char first = word.charAt(i);
-            char second = word.charAt(i + 1);
-            if (first != 'a' || first > second) {
-                i++;
-                continue;
+        for (int i = 1; i < n; i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                current++;
+
+            } else if (word.charAt(i) > word.charAt(i - 1)) {
+                count++;
+                current++;
+
+            } else {
+                count = 1;
+                current = 1;
             }
 
-            /**
-             3.  Mark the first and second letters as seen
-                 and increment countUnique for each letter being seen for first time.
-
-                 Initialise j as i + 1. While j is a valid index and the j-th letter
-                 is >= (j - 1)-th letter, mark the j-th letter as seen and increment countUnique
-                 if it is being checked for first time, and increment j.
-
-                 If all 5 vowels are present between indices i & j,
-                 compare longest and length between indices j - 1 and assign the larger value to longest,
-                 and start the next check at index j.
-             */
-            boolean[] isChecked = new boolean[26];
-            int countUnique = 0;
-            int j = i + 1;
-
-            if (!isChecked[first - 'a']) {
-                isChecked[first - 'a'] = true;
-                countUnique++;
+            if (count == 5) {
+                longest = Math.max(current, longest);
             }
-            if (!isChecked[second - 'a']) {
-                isChecked[second - 'a'] = true;
-                countUnique++;
-            }
-
-            while (j < n && word.charAt(j) >= word.charAt(j - 1)) {
-                if (!isChecked[word.charAt(j) - 'a']) {
-                    isChecked[word.charAt(j) - 'a'] = true;
-                    countUnique++;
-                }
-                j++;
-            }
-            if (countUnique == 5) {
-                longest = Math.max(longest, j - i);
-            }
-
-            i = j;
         }
 
         return longest;
