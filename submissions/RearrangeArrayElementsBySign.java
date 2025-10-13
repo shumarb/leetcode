e92cd92f464a3f238efe553281a6316e311c9885
Umar <shumarb@outlook.com>
2025-10-14 00:23:40 +0800

    Rearrange Array Elements by Sign: Refactored solution resulted in improved performance

--- submissions/RearrangeArrayElementsBySign.java
+++ submissions/RearrangeArrayElementsBySign.java
@@ -2,39 +2,19 @@
 
 class RearrangeArrayElementsBySign {
     public int[] rearrangeArray(int[] nums) {
-        boolean isPositive = true;
-        int i = 0;
         int j = 0;
-        int k = 0;
+        int k = 1;
         int n = nums.length;
-        int[] negative = new int[n / 2];
         int[] result = new int[n];
-        int[] positive = new int[n / 2];
 
         for (int number: nums) {
             if (number > 0) {
-                positive[j++] = number;
+                result[j] = number;
+                j += 2;
             } else {
-                negative[k++] = number;
+                result[k] = number;
+                k += 2;
             }
-        }
-
-        j = 0;
-        k = 0;
-        while (j < n / 2 && k < n / 2) {
-            if (isPositive) {
-                result[i++] = positive[j++];
-                isPositive = false;
-            } else {
-                result[i++] = negative[k++];
-                isPositive = true;
-            }
-        }
-        if (j < n / 2) {
-            result[i++] = positive[j++];
-        }
-        if (k < n / 2) {
-            result[i++] = negative[k++];
         }
 
         return result;
