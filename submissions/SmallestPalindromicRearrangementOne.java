364a5a45f730709aaffe4cfdd3e5f91b30dcdd9f
Umar <shumarb@outlook.com>
2025-10-28 00:40:07 +0800

    Smallest Palindromic Rearrangement I: Refactored solution resulted in same performance

--- submissions/SmallestPalindromicRearrangementOne.java
+++ submissions/SmallestPalindromicRearrangementOne.java
@@ -4,22 +4,20 @@ class SmallestPalindromicRearrangementOne {
     public String smallestPalindrome(String s) {
         int[] frequency = new int[26];
         int n = s.length();
-        char oddLetter = 'X';
         char[] left = s.substring(0, n / 2).toCharArray();
 
+        Arrays.sort(left);
+        String first = new String(left);
+        String second = new StringBuilder(first).reverse().toString();
         for (char c: s.toCharArray()) {
             frequency[c - 'a']++;
         }
         for (int i = 0; i < 26; i++) {
             if (frequency[i] % 2 == 1) {
-                oddLetter = (char)('a' + i);
-                break;
+                return first + (char) ('a' + i) + second;
             }
         }
-        Arrays.sort(left);
-        String first = new String(left);
-        String second = new StringBuilder(first).reverse().toString();
 
-        return oddLetter == 'X' ? first + second : first + oddLetter + second;
+        return first + second;
     }
 }
