4379d1cd057ad1ceeeef65d568d7ab8be8af8473
Umar <shumarb@outlook.com>
2025-10-29 01:19:22 +0800

    Repeated DNA Sequences: Refactored solution resulted in improved performance

--- submissions/RepeatedDnaSequences.java
+++ submissions/RepeatedDnaSequences.java
@@ -2,26 +2,20 @@
 
 class RepeatedDnaSequences {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> repeated = new HashSet<>();
+        Set<String> seen = new HashSet<>();
         boolean isTest = false;
 
+        for (int i = 0; i <= s.length() - 10; i++) {
             String substring = s.substring(i, i + 10);
-            map.put(substring, 1 + map.getOrDefault(substring, 0));
-        }
-        for (Map.Entry<String, Integer> e: map.entrySet()) {
-            if (e.getValue() > 1) {
-                result.add(e.getKey());
+            if (!seen.add(substring)) {
+                repeated.add(substring);
             }
         }
         if (isTest) {
-            System.out.println("map:");
-            for (Map.Entry<String, Integer> e: map.entrySet()) {
-                System.out.println(e.getKey() + " -> " + e.getValue());
-            }
-            System.out.println("-------------------------------\nresult: " + result);
+            System.out.println("seen: " + seen + "\nrepeated: " + repeated);
         }
 
-        return result;
+        return new ArrayList<>(repeated);
     }
 }
