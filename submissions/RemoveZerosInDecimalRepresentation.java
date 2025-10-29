906d6fecd6a63a57f249e8a7266485b831eab3fb
Umar <shumarb@outlook.com>
2025-10-29 21:57:19 +0800

    Remove Zeros in Decimal Representation: Refactored solution resulted in improved performance

--- submissions/RemoveZerosInDecimalRepresentation.java
+++ submissions/RemoveZerosInDecimalRepresentation.java
@@ -2,28 +2,16 @@
 
 class RemoveZerosInDecimalRepresentation {
     public long removeZeros(long n) {
-        Stack<Long> stack = new Stack<>();
-        boolean isTest = false;
+        long multiplier = 1;
         long result = 0;
 
         while (n > 0) {
             long digit = n % 10;
-            if (digit > 0) {
-                stack.push(digit);
+            if (digit != 0) {
+                result = result + digit * multiplier;
+                multiplier *= 10;
             }
             n /= 10;
-        }
-        if (isTest) {
-            System.out.println("stack: " + stack);
-        }
-
-        while (!stack.isEmpty()) {
-            long digit = stack.pop();
-            result *= 10;
-            result += digit;
-        }
-        if (isTest) {
-            System.out.println("result: " + result);
         }
 
         return result;
