fb5573159b29f5048db570a61583f18160baeb09
Umar <shumarb@outlook.com>
2025-11-03 15:52:33 +0800

    Count Alternating Subarrays: Refactored solution resulted in improved  performance

--- submissions/CountAlternatingSubarrays.java
+++ submissions/CountAlternatingSubarrays.java
@@ -2,22 +2,16 @@
 
 class CountAlternatingSubarrays {
     public long countAlternatingSubarrays(int[] nums) {
-        boolean isTest = false;
-        int n = nums.length;
-        long[] dp = new long[n];
-        long result = 0;
+        long count = 1;
+        long result = 1;
 
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[i - 1]) {
-                dp[i] = 1 + dp[i - 1];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                result++;
+                count = 1;
+            } else {
+                result += ++count;
             }
-        }
-        for (long count: dp) {
-            result += count;
-        }
-        if (isTest) {
-            System.out.println("dp: " + Arrays.toString(dp) + "\nresult: " + result);
         }
 
         return result;
