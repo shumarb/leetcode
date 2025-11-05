368f1231ebbe583ddb88959eb0f5f06f3a4e381b
Umar <shumarb@outlook.com>
2025-11-05 17:20:10 +0800

    Difference Between Ones and Zeros in Row and Column: Refactored solution resulted in improved performance

--- submissions/DifferenceBetweenOnesAndZerosInRowAndColumn.java
+++ submissions/DifferenceBetweenOnesAndZerosInRowAndColumn.java
@@ -5,7 +5,7 @@ class DifferenceBetweenOnesAndZerosInRowAndColumn {
         boolean isTest = false;
         int m = grid.length;
         int n = grid[0].length;
-        int[][] result = new int[m][n];
+        int[][] diff = new int[m][n];
         int[] onesCol = new int[n];
         int[] onesRow = new int[m];
 
@@ -19,42 +19,18 @@ class DifferenceBetweenOnesAndZerosInRowAndColumn {
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                result[i][j] = onesCol[j] + onesRow[i] - (m - onesCol[j]) - (n - onesRow[i]);
+                diff[i][j] = onesCol[j] + onesRow[i] - (m - onesCol[j]) - (n - onesRow[i]);
             }
         }
         if (isTest) {
             System.out.println("onesCol: " + Arrays.toString(onesCol) + "\nonesRow: " + Arrays.toString(onesRow));
             System.out.println("----------------------------------------------");
-            System.out.println("result:");
-            for (int[] row: result) {
+            System.out.println("diff:");
+            for (int[] row: diff) {
                 System.out.println(Arrays.toString(row));
             }
         }
 
-        return result;
-    }
-
-    private int countColumnOnes(int[][] grid, int j) {
-        int count = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            if (grid[i][j] == 1) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private int countRowOnes(int[] row) {
-        int count = 0;
-
-        for (int e: row) {
-            if (e == 1) {
-                count++;
-            }
-        }
-
-        return count;
+        return diff;
     }
 }
