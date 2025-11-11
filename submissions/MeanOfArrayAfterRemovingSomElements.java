abe636992264e32962ffbe908d6428f04e9f8975
Umar <shumarb@outlook.com>
2025-11-11 10:14:19 +0800

    Mean of Array After Removing Some Elements: Refactored solution resulted in improved performance

--- submissions/MeanOfArrayAfterRemovingSomElements.java
+++ submissions/MeanOfArrayAfterRemovingSomElements.java
@@ -2,37 +2,15 @@
 
 class MeanOfArrayAfterRemovingSomElements {
     public double trimMean(int[] arr) {
-        double average = 0;
+        double mean = 0;
         int n = arr.length;
         int countRemovals = (int) (0.05 * n);
 
-        arr = sort(arr);
+        Arrays.sort(arr);
         for (int i = countRemovals; i < n - countRemovals; i++) {
-            average += arr[i];
+            mean += arr[i];
         }
 
-        return average / (double) (n - countRemovals * 2.0);
-    }
-
-    private int[] sort(int[] arr) {
-        int[] frequency;
-        int j = 0;
-        int largest = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            largest = Math.max(arr[i], largest);
-        }
-        frequency = new int[largest + 1];
-
-        for (int number: arr) {
-            frequency[number]++;
-        }
-        for (int i = 0; i <= largest; i++) {
-            while (frequency[i]-- > 0) {
-                arr[j++] = i;
-            }
-        }
-
-        return arr;
+        return mean / (double) (n - countRemovals * 2.0);
     }
 }
