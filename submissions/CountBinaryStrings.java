1de5682e1cbca39d3f41dd14f1f6948ea9b7cdc9
Umar <shumarb@outlook.com>
2025-11-14 12:13:17 +0800

    Count Binary Strings: Refactored solution resulted in improved performance

--- submissions/CountBinaryStrings.java
+++ submissions/CountBinaryStrings.java
@@ -2,12 +2,13 @@
 
 class CountBinaryStrings {
     public int countBinarySubstrings(String s) {
+        char[] bits = s.toCharArray();
         int currentConsecutive = 1;
         int previousConsecutive = 0;
         int result = 0;
 
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
+        for (int i = 1; i < bits.length; i++) {
+            if (bits[i] == bits[i - 1]) {
                 currentConsecutive++;
             } else {
                 result += Math.min(currentConsecutive, previousConsecutive);
