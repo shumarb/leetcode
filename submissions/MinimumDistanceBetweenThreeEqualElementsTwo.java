e137e1f4ffe76126da496102a942b0c65b29b079
Umar <shumarb@outlook.com>
2025-11-14 20:58:13 +0800

    Minimum Distance Between Three Equal Elements I: Refactored solution resulted in improved performance

--- submissions/MinimumDistanceBetweenThreeEqualElementsTwo.java
+++ submissions/MinimumDistanceBetweenThreeEqualElementsTwo.java
@@ -2,26 +2,30 @@
 
 class MinimumDistanceBetweenThreeEqualElementsTwo {
     public int minimumDistance(int[] nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer>[] map;
+        int largest = Integer.MIN_VALUE;
         int result = Integer.MAX_VALUE;
+
+        for (int e: nums) {
+            largest = Math.max(e, largest);
+        }
+        map = new List[largest + 1];
+        for (int i = 1; i <= largest; i++) {
+            map[i] = new ArrayList<>();
+        }
 
         for (int c = 0; c < nums.length; c++) {
             int key = nums[c];
-            if (!map.containsKey(key)) {
-                List<Integer> value = new ArrayList<>();
-                value.add(c);
-                map.put(key, value);
-            } else {
-                map.get(key).add(c);
-            }
+            map[key].add(c);
 
-            if (map.get(key).size() >= 3) {
-                List<Integer> value = map.get(key);
+            if (map[key].size() >= 3) {
+                List<Integer> value = map[key];
                 int n = value.size();
                 int i = value.get(n - 3);
                 int j = value.get(n - 2);
                 int k = value.get(n - 1);
                 int absoluteDifferenceSum = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
+
                 result = Math.min(absoluteDifferenceSum, result);
             }
         }
