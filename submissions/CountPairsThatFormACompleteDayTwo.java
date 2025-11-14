2f70c015ea5f0ab57e1683a5b9567bbe90ca722c
Umar <shumarb@outlook.com>
2025-11-14 10:24:11 +0800

    Count Pairs That Form a Complete Day II: Refactored solution resulted in improved performance

--- submissions/CountPairsThatFormACompleteDayTwo.java
+++ submissions/CountPairsThatFormACompleteDayTwo.java
@@ -2,14 +2,26 @@
 
 class CountPairsThatFormACompleteDayTwo {
     public long countCompleteDayPairs(int[] hours) {
+        boolean isTest = false;
+        long[] remainders = new long[24];
         long result = 0;
-        int[] frequency = new int[24];
 
         for (int hour: hours) {
-            int remainder = hour % 24;
-            int complement = (24 - remainder) % 24;
-            result += frequency[complement];
-            frequency[remainder]++;
+            remainders[hour % 24]++;
+        }
+        // 1. Special case: Count valid pairs of hours divisible by 24, which are 0 & 12.
+        result += remainders[0] * (remainders[0] - 1) / 2;
+        result += remainders[12] * (remainders[12] - 1) / 2;
+
+        /**
+         2. Form pairs from remainders i & (24 - i) for i ranging from [1, 11].
+            Iterate from 1 to 11 to ensure no double counting.
+         */
+        for (int i = 1; i < 12; i++) {
+            result += remainders[i] * remainders[24 - i];
+        }
+        if (isTest) {
+            System.out.println("remainders: " + Arrays.toString(remainders) + "\nresult: " + result);
         }
 
         return result;
