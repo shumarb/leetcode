4a7d26125ca4a469311c4ea0084e68e83e19c517
Umar <shumarb@outlook.com>
2025-11-14 12:12:47 +0800

    Count Pairs That Form a Complete Day I: Refactored solution resulted in same performance

--- submissions/CountPairsThatFormACompleteDayOne.java
+++ submissions/CountPairsThatFormACompleteDayOne.java
@@ -2,16 +2,27 @@
 
 class CountPairsThatFormACompleteDayOne {
     public int countCompleteDayPairs(int[] hours) {
-        int countCompleteDayPairs = 0;
-        int len = hours.length;
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                long sum = hours[i] + hours[j];
-                if (sum % 24 == 0) {
-                    countCompleteDayPairs++;
-                }
-            }
+        boolean isTest = false;
+        long[] remainders = new long[24];
+        long result = 0;
+
+        for (int hour: hours) {
+            remainders[hour % 24]++;
         }
-        return countCompleteDayPairs;
+
+        // 1. Form pairs amongst groups that are multiples of 24 (0, 12).
+        result += remainders[0] * (remainders[0] - 1) / 2;
+        result += remainders[12] * (remainders[12] - 1) / 2;
+
+        // 2. Form pairs amongst remainders not multiple of 24.
+        for (int i = 1; i < 12; i++) {
+            result += remainders[i] * remainders[24 - i];
+        }
+
+        if (isTest) {
+            System.out.println("remainders: " + Arrays.toString(remainders) + "\nresult: " + result);
+        }
+
+        return (int) result;
     }
 }
