208eaee3ece18b118553c572d9518a2a3c0109e2
Umar <shumarb@outlook.com>
2025-11-20 00:07:25 +0800

    Maximum Number of Occurrences of a Substring: Refactored solution resulted in improved performance

--- submissions/MaximumNumberOfOccurrencesOfASubstring.java
+++ submissions/MaximumNumberOfOccurrencesOfASubstring.java
@@ -6,51 +6,38 @@ class MaximumNumberOfOccurrencesOfASubstring {
         boolean isTest = false;
         int n = s.length();
         int result = 0;
-        int windowSize = minSize;
 
-        while (windowSize <= maxSize) {
-            if (isTest) {
-                System.out.println("-------------------------------------------");
-                System.out.println("windowSize: " + windowSize);
+        for (int i = 0; i + minSize <= n; i++) {
+            String substring = s.substring(i, i + minSize);
+            if (isValid(substring, maxLetters)) {
+                map.put(substring, 1 + map.getOrDefault(substring, 0));
+                result = Math.max(map.get(substring), result);
             }
-            for (int i = 0; i <= n - windowSize; i++) {
-                String substring = s.substring(i, i + windowSize);
-                boolean[] isPresent = new boolean[26];
-                boolean isValid = true;
-                int countDistinctLetters = 0;
-
-                for (char c: substring.toCharArray()) {
-                    if (!isPresent[c - 'a']) {
-                        isPresent[c - 'a'] = true;
-                        countDistinctLetters++;
-                        if (countDistinctLetters > maxLetters) {
-                            isValid = false;
-                            break;
-                        }
-                    }
-                }
-                if (isValid) {
-                    if (isTest) {
-                        System.out.println("i: " + i + " -> valid substring: " + substring);
-                    }
-                    map.put(substring, 1 + map.getOrDefault(substring, 0));
-                }
-            }
-            windowSize++;
-        }
-
-        for (Map.Entry<String, Integer> e: map.entrySet()) {
-            result = Math.max(e.getValue(), result);
         }
         if (isTest) {
-            System.out.println("-------------------------------------------");
             System.out.println("map:");
             for (Map.Entry<String, Integer> e: map.entrySet()) {
                 System.out.println(" * " + e.getKey() + " -> " + e.getValue());
             }
-            System.out.println("-------------------------------------------\nresult: " + result);
+            System.out.println("-------------------------------------\nresult: " + result);
         }
 
         return result;
+    }
+
+    private boolean isValid(String check, int maxLetters) {
+        boolean[] isPresent = new boolean[26];
+        int countDistinct = 0;
+
+        for (char c: check.toCharArray()) {
+            if (!isPresent[c - 'a']) {
+                if (++countDistinct > maxLetters) {
+                    return false;
+                }
+                isPresent[c - 'a'] = true;
+            }
+        }
+
+        return true;
     }
 }
