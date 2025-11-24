179383881df060e0322dbee0362640a3ed325d38
Umar <shumarb@outlook.com>
2025-11-24 19:54:50 +0800

    Cells in a Range on an Excel Sheet: Refactored solution resulted in same performance

--- submissions/CellsInARangeOnAnExcelSheet.java
+++ submissions/CellsInARangeOnAnExcelSheet.java
@@ -4,10 +4,10 @@ class CellsInARangeOnAnExcelSheet {
     public List<String> cellsInRange(String s) {
         List<String> result = new ArrayList<>();
         boolean isTest = false;
-        char firstLetter = s.charAt(0);
-        char lastLetter = s.charAt(3);
         char firstDigit = s.charAt(1);
+        char firstLetter = s.charAt(0);
         char lastDigit = s.charAt(4);
+        char lastLetter = s.charAt(3);
 
         for (char i = firstLetter; i <= lastLetter; i++) {
             StringBuilder current = new StringBuilder();
