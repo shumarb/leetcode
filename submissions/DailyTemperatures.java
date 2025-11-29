1fedd4f349008c09649b238d43f2ce16926038ca
Umar <shumarb@outlook.com>
2025-11-29 09:27:29 +0800

    Daily Temperatures: Refactored solution resulted in same runtime and lower rank

--- submissions/DailyTemperatures.java
+++ submissions/DailyTemperatures.java
@@ -1,32 +1,28 @@
 // Question: https://leetcode.com/problems/daily-temperatures/description/
 
-import java.util.Stack;
-
 class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        int len = temperatures.length;
-        int[] answer = new int[len];
-        Stack<Integer> nextGreaterElementIndexStack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        boolean isTest = false;
+        int n = temperatures.length;
+        int[] result = new int[n];
 
-        for (int i = len - 1; i >= 0; i--) {
-            // 1. Remove elements from stack until element greater than current element is found
-            while (!nextGreaterElementIndexStack.isEmpty() && temperatures[nextGreaterElementIndexStack.peek()] <= temperatures[i]) {
-                nextGreaterElementIndexStack.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
             }
-
-            // 2. If the stack is not empty, the top element of stack is index of next greater element of current element
-            // so take index of next greater element (top element of stack) - current index
-            // If stack is empty, there is no element greater than current element from the right of current element,
-            // hence answer[i] = 0, but since answer array has been initialised, we don't have to manually set answer[i] to 0.
-            if (!nextGreaterElementIndexStack.isEmpty()) {
-                answer[i] = nextGreaterElementIndexStack.peek() - i;
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
             }
-
-            // 3. Lastly, add current index into stack as it could be the index of next greater element
-            // of elements in temperatures array from indices [0, i - 1]
-            nextGreaterElementIndexStack.push(i);
+            if (isTest) {
+                System.out.println("---------------------------------\ni: " + i + "\nstack: " + stack + "\nresult: " + Arrays.toString(result));
+            }
+            stack.push(i);
+        }
+        if (isTest) {
+            System.out.println("---------------------------------\nfinal result: " + Arrays.toString(result));
         }
 
-        return answer;
+        return result;
     }
 }
