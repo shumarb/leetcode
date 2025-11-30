f7b7a032cfa949b8983141c7d68fc7de4b074812
Umar <shumarb@outlook.com>
2025-11-30 12:53:06 +0800

    Min Cost Climbing Stairs: Refactored solution resulted in same performance

--- submissions/MinCostClimbingStairs.java
+++ submissions/MinCostClimbingStairs.java
@@ -1,45 +1,19 @@
 // Question: https://leetcode.com/problems/min-cost-climbing-stairs/description/
 
 class MinCostClimbingStairs {
-    private int[] cost;
-    private int[] dp;
-
-    private int helper(int i) {
-        if (i >= cost.length) {
-            return 0;
-        }
-
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int take1Step = cost[i] + helper(i + 1);
-        int take2Steps = cost[i] + helper(i + 2);
-        dp[i] = Math.min(take1Step, take2Steps);
-
-        return dp[i];
-    }
-
     public int minCostClimbingStairs(int[] cost) {
         boolean isTest = false;
-        int n = cost.length;
-        int startFrom0 = 0;
-        int startFrom1 = 0;
-        this.cost = cost;
-        this.dp = new int[n];
 
-        Arrays.fill(dp, -1);
-        startFrom0 = helper(0);
         if (isTest) {
-            System.out.println("startFrom0: " + startFrom0 + "\n * dp: " + Arrays.toString(dp));
+            System.out.println("before, cost: " + Arrays.toString(cost));
+        }
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        }
+        if (isTest) {
+            System.out.println("after, cost:  " + Arrays.toString(cost) + "\nresult: " + Math.min(cost[0], cost[1]));
         }
 
-        Arrays.fill(dp, -1);
-        startFrom1 = helper(1);
-        if (isTest) {
-            System.out.println("\nstartFrom1: " + startFrom1 + "\n * dp: " + Arrays.toString(dp));
-        }
-
-        return Math.min(startFrom0, startFrom1);
+        return Math.min(cost[0], cost[1]);
     }
 }
