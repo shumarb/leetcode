e057556c4794c14f7c74cf1adda2142bd1386fb6
Umar <shumarb@outlook.com>
2025-12-03 00:04:58 +0800

    Concatenate Non-Zero Digits and Multiply By Sum I: Refactored solution resulted in same performance

--- submissions/ConcatenateNonZeroDigitsAndMultiplyBySumOne.java
+++ submissions/ConcatenateNonZeroDigitsAndMultiplyBySumOne.java
@@ -1,25 +1,20 @@
 // Question: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/description/
 
-class Solution {
+class ConcatenateNonZeroDigitsAndMultiplyBySumOne {
     public long sumAndMultiply(int n) {
         boolean isTest = false;
-        long numberReverse = 0;
         long number = 0;
         long sum = 0;
+        long tens = 1;
 
         while (n > 0) {
             int digit = n % 10;
             if (digit > 0) {
-                numberReverse *= 10;
-                numberReverse += digit;
+                number += (tens * digit);
                 sum += digit;
+                tens *= 10;
             }
             n /= 10;
-        }
-        while (numberReverse > 0) {
-            number *= 10;
-            number += numberReverse % 10;
-            numberReverse /= 10;
         }
         if (isTest) {
             System.out.println("number: " + number + "\nsum: " + sum);
