b750f27a4628dcf460a341f190dec768c65a3303
Umar <shumarb@outlook.com>
2025-12-09 12:33:07 +0800

    Prime Pairs With Target Sum: Refactored solution resulted in improved performance

--- submissions/PairPrimesWithTargetSum.java
+++ submissions/PairPrimesWithTargetSum.java
@@ -3,51 +3,36 @@
 class PairPrimesWithTargetSum {
     public List<List<Integer>> findPrimePairs(int n) {
         List<List<Integer>> result = new ArrayList<>();
-        Set<Integer> counted = new HashSet<>();
-        Set<Integer> primes = new HashSet<>();
+        boolean[] isCounted = new boolean[n + 1];
         boolean[] isPrime = new boolean[n + 1];
         boolean isTest = false;
 
-        Arrays.fill(isPrime, true);
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
+                for (int j = i * i; j < n; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
-        for (int i = 2; i < isPrime.length; i++) {
-            if (isPrime[i]) {
-                primes.add(i);
-            }
-        }
-        for (int e: primes) {
-            int complement = n - e;
-            if (counted.contains(e) && counted.contains(complement)) {
-                continue;
-            }
-            if (primes.contains(e) && primes.contains(complement)) {
+        for (int i = 2; i < n; i++) {
+            int complement = n - i;
+            if (isPrime[i] && isPrime[complement] && !isCounted[i] && !isCounted[complement]) {
                 List<Integer> pair = new ArrayList<>();
-                counted.add(e);
-                counted.add(complement);
-                pair.add(e);
+                pair.add(i);
                 pair.add(complement);
-                if (pair.get(0) > pair.get(1)) {
-                    int temp = pair.get(0);
-                    pair.set(0, pair.get(1));
-                    pair.set(1, temp);
-                }
+                isCounted[i] = true;
+                isCounted[complement] = true;
+                Collections.sort(pair);
                 result.add(pair);
             }
         }
-        Collections.sort(result,
-                (a, b) -> Integer.compare(a.get(0), b.get(0)) == 0
-                        ? Integer.compare(a.get(1), b.get(1))
-                        : Integer.compare(a.get(0), b.get(0))
-        );
+        Collections.sort(result, (a, b) -> Integer.compare(a.get(0), b.get(0)));
         if (isTest) {
-            System.out.println("primes: " + primes);
-            System.out.println("------------------------------\nresult: ");
+            System.out.println("isPrime:    " + Arrays.toString(isPrime));
+            System.out.println("--------------------------------------------------------\nresult: ");
             for (List<Integer> e: result) {
                 System.out.println(" * " + e);
             }
