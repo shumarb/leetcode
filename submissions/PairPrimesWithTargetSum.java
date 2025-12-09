bac6952e98ce1d7245734a44aa056e219434f7b2
Umar <shumarb@outlook.com>
2025-12-09 12:36:32 +0800

    Prime Pairs With Target Sum: Refactored solution resulted in improved performance

--- submissions/PairPrimesWithTargetSum.java
+++ submissions/PairPrimesWithTargetSum.java
@@ -7,17 +7,15 @@ class PairPrimesWithTargetSum {
         boolean[] isPrime = new boolean[n + 1];
         boolean isTest = false;
 
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-        for (int i = 2; i * i < n; i++) {
+        Arrays.fill(isPrime, 2, isPrime.length, true);
+        for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                for (int j = i * i; j <= n; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             int complement = n - i;
             if (isPrime[i] && isPrime[complement] && !isCounted[i] && !isCounted[complement]) {
                 List<Integer> pair = new ArrayList<>();
