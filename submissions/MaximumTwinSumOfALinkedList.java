da58c3b2977dba63033b86be017cf847653bd2fa
Umar <shumarb@outlook.com>
2025-12-15 00:18:46 +0800

    Maximum Twin Sum of a Linked List: Refactored solution resulted in improved performance

--- submissions/MaximumTwinSumOfALinkedList.java
+++ submissions/MaximumTwinSumOfALinkedList.java
@@ -12,43 +12,78 @@
  */
 class MaximumTwinSumOfALinkedList {
     public int pairSum(ListNode head) {
-        List<ListNode> list = new ArrayList<>();
-        ListNode current = head;
+        ListNode first = head;
+        ListNode second = head.next;
         boolean isTest = false;
+        int index = 0;
         int result = 0;
-        int i = 0;
-        int j;
 
-        while (current != null) {
-            list.add(current);
-            current = current.next;
+        while (second != null && second.next != null) {
+            second = second.next.next;
+            first = first.next;
+            index++;
         }
         if (isTest) {
-            System.out.print("list: ");
-            for (ListNode e: list) {
-                System.out.print(e.val + " ");
-            }
-            System.out.println("\n---------------------------");
+            System.out.println("first: " + first.val + ", index: " + index);
+            display("before: ", head);
         }
 
-        j = list.size() - 1;
-        while (i < j) {
-            int left = list.get(i).val;
-            int right = list.get(j).val;
+        first.next = reverse(first.next);
+
+        first = head;
+        second = head;
+        while (first != null && first.next != null) {
+            first = first.next.next;
+            second = second.next;
+        }
+        if (isTest) {
+            System.out.print("second: " + second.val);
+            if (second.next == null) {
+                System.out.println(", next: " + null);
+            } else {
+                System.out.println(", next: " + second.next.val);
+            }
+            System.out.println("--------------------------------");
+        }
+        first = head;
+        while (first != second && second != null) {
+            int sum = first.val + second.val;
             if (isTest) {
-                System.out.println(" * i: " + i + " -> left: " + left);
-                System.out.println(" * j: " + j + " -> right: " + right);
-                System.out.println(" compare | sum: " + (left + right) + ", result: " + result);
-                System.out.println("---------------------------");
+                System.out.println(" * first: " + first.val + ", second: " + second.val + " -> sum: " + sum);
             }
-            result = Math.max(result, left + right);
-            i++;
-            j--;
+            result = Math.max(result, sum);
+            first = first.next;
+            second = second.next;
         }
         if (isTest) {
-            System.out.println("result: " + result);
+            display("after: ", head);
         }
 
         return result;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+
+    private void display(String s, ListNode head) {
+        System.out.print(s);
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println("\n--------------------------------");
     }
 }
