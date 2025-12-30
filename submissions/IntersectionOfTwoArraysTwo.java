298dc442bb4232eb48599e59992b28a286329be0
Umar <shumarb@outlook.com>
2025-12-31 00:56:09 +0800

    Intersection of Two Arrays II: Refactored solution resulted in improved performance

--- submissions/IntersectionOfTwoArraysTwo.java
+++ submissions/IntersectionOfTwoArraysTwo.java
@@ -1,67 +1,25 @@
 // Question: https://leetcode.com/problems/intersection-of-two-arrays/description/
 
 class IntersectionOfTwoArraysTwo {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        int[] small;
-        int[] big;
-        boolean t = false;
+    public int[] intersect(int[] nums1, int[] nums2) {
+        boolean isTest = false;
+        int[] count = new int[1001];
+        int[] result = new int[1001];
+        int total = 0;
 
-        if (nums1.length > nums2.length) {
-            big = nums1;
-            small = nums2;
-        } else {
-            small = nums1;
-            big = nums2;
+        for (int e: nums1) {
+            count[e]++;
         }
-        if (t) {
-            System.out.println("nums1: " + Arrays.toString(nums1) + ", nums2: " + Arrays.toString(nums2));
-            System.out.println("big: " + Arrays.toString(big) + ", small: " + Arrays.toString(small));
-        }
-
-        int max1 = getMax(nums1);
-        int max2 = getMax(nums2);
-        if (t) {
-            System.out.println("max1: " + max1 + ", max2: " + max2);
-        }
-
-        boolean[] bigFreq = new boolean[1 + Math.max(max1, max2)];
-        for (int x: big) {
-            bigFreq[x] = true;
-        }
-        if (t) {
-            System.out.println("bigFreq: " + Arrays.toString(bigFreq));
-        }
-
-        List<Integer> list = new ArrayList<> ();
-        for (int x: small) {
-            if (bigFreq[x]) {
-                if (t) {
-                    System.out.println("intersect: " + x);
-                }
-                list.add(x);
-                bigFreq[x] = false;
+        for (int e: nums2) {
+            if (count[e]-- > 0) {
+                result[total++] = e;
             }
         }
-        if (t) {
-            System.out.println("list: " + list);
+        result = Arrays.copyOfRange(result, 0, total);
+        if (isTest) {
+            System.out.println("result: " + Arrays.toString(result));
         }
 
-        int[] ans = new int[list.size()];
-        int j = 0;
-        for (int x: list) {
-            ans[j++] = x;
-        }
-        if (t) {
-            System.out.println("ans: " + Arrays.toString(ans));
-        }
-        return ans;
-    }
-
-    private int getMax(int[] a) {
-        int max = a[0];
-        for (int x: a) {
-            max = Math.max(max, x);
-        }
-        return max;
+        return result;
     }
 }
