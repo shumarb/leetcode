45f5eddea9f0544a2d2a248af554d1caff97b080
Umar <shumarb@outlook.com>
2026-01-15 12:00:12 +0800

    Count Residue Prefixes: Refactored solution resulted in same performance

--- submissions/CountResiduePrefixes.java
+++ submissions/CountResiduePrefixes.java
@@ -4,17 +4,15 @@ class CountResiduePrefixes {
     public int residuePrefixes(String s) {
         boolean[] isCounted = new boolean[26];
         int countDistinct = 0;
-        int len = s.length();
         int result = 0;
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (!isCounted[c - 'a']) {
                 countDistinct++;
                 isCounted[c - 'a'] = true;
             }
-            int prefixLen = i + 1;
-            if (countDistinct == (prefixLen % 3)) {
+            if (countDistinct == ((i + 1) % 3)) {
                 result++;
             }
         }
