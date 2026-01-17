594cd1bb5a9879cf5cbd87ca9514047f02d642ad
Umar <shumarb@outlook.com>
2026-01-17 09:55:57 +0800

    Minimum Number of Operations to Have Distinct Elements: Refactored solution resulted in improved performance

--- submissions/MinimumNumberOfOperationsToHaveDistinctElements.java
+++ submissions/MinimumNumberOfOperationsToHaveDistinctElements.java
@@ -2,18 +2,17 @@
 
 class MinimumNumberOfOperationsToHaveDistinctElements {
     public int minOperations(int[] nums) {
-        Queue<Integer> queue = new LinkedList<>();
         boolean[] isPresent;
         boolean isTest = false;
         int[] count;
         int countDistinct = 0;
+        int i = 0;
         int largest = 0;
         int n = nums.length;
         int result = 0;
 
         for (int e: nums) {
             largest = Math.max(e, largest);
-            queue.offer(e);
         }
         isPresent = new boolean[largest + 1];
         count = new int[largest + 1];
@@ -31,34 +30,17 @@ class MinimumNumberOfOperationsToHaveDistinctElements {
         if (countDistinct == n) {
             return result;
         }
-
-        while (queue.size() >= 3) {
-            if (isTest) {
-                System.out.println("----------------------------------");
-                System.out.println("before, queue: " + queue + "\n * countDistinct: " + countDistinct + "\n * result: " + result);
-            }
-            for (int i = 0; i < 3; i++) {
-                int top = queue.poll();
-                count[top]--;
-                if (count[top] == 0) {
+        while (n - i >= 3) {
+            for (int j = 0; j < 3; j++) {
+                int top = nums[i++];
+                if (--count[top] == 0) {
                     countDistinct--;
                 }
             }
             result++;
-            if (isTest) {
-                System.out.println("\nafter, queue: " + queue + "\n * countDistinct: " + countDistinct + "\n * result: " + result);
-            }
-
-            if (countDistinct == queue.size()) {
-                if (isTest) {
-                    System.out.println(" ** queue has distinct remaining elements, return result: " + result);
-                }
+            if (countDistinct == n - i) {
                 return result;
             }
-        }
-        if (isTest) {
-            System.out.println("----------------------------------");
-            System.out.println("remaining queue after while: " + queue + "\n * countDistinct: " + countDistinct + "\n * result: " + result);
         }
 
         return countDistinct == n ? result : result + 1;
