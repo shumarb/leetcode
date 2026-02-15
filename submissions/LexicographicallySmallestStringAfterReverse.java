398788437a4e6416f3a02b7be21fa922b2ba5e83
Umar <shumarb@outlook.com>
2026-02-15 23:05:17 +0800

    Lexicographically Smallest String After Reverse: Refactored solution led to strong performance

--- submissions/LexicographicallySmallestStringAfterReverse.java
+++ submissions/LexicographicallySmallestStringAfterReverse.java
@@ -4,15 +4,16 @@ class LexicographicallySmallestStringAfterReverse {
     public String lexSmallest(String s) {
         String result = s;
         boolean isTest = false;
-        char[] letters = s.toCharArray();
         int n = s.length();
 
-        for (int i = 2; i <= n; i++) {
-            String reverseFirst = reverse(letters.clone(), "reverseFirst", i);
-            String reverseLast = reverse(letters.clone(), "reverseLast", i);
+        for (int k = 1; k <= n; ++k) {
+            String reverseFirst = new StringBuilder(s.substring(0, k)).reverse().toString() + s.substring(k);
+            String reverseLast = s.substring(0, n - k) + new StringBuilder(s.substring(n - k)).reverse().toString();
+
             if (isTest) {
-                System.out.println("i: " + i + " -> reverseFirst: " + reverseFirst + ", reverseLast: " + reverseLast);
+                System.out.println("k: " + k + " -> reverseFirst: " + reverseFirst + ", reverseLast: " + reverseLast);
             }
+
             if (reverseFirst.compareTo(result) < 0) {
                 result = reverseFirst;
             }
@@ -22,27 +23,5 @@ class LexicographicallySmallestStringAfterReverse {
         }
 
         return result;
-    }
-
-    private String reverse(char[] letters, String type, int size) {
-        int i;
-        int j;
-
-        if (type.equals("reverseFirst")) {
-            i = 0;
-            j = size - 1;
-
-        } else {
-            i = letters.length - size;
-            j = letters.length - 1;
-        }
-
-        while (i < j) {
-            char temp = letters[i];
-            letters[i++] = letters[j];
-            letters[j--] = temp;
-        }
-
-        return new String(letters);
     }
 }
