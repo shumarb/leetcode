8a5b895cd431592b9f86e0406941fa124a97bec4
Umar <shumarb@outlook.com>
2026-02-16 22:19:44 +0800

    New submission: Optimal Partition of String

---
+++ submissions/OptimalPartitionOfString.java
@@ -0,0 +1,40 @@
+// Question: https://leetcode.com/problems/optimal-partition-of-string/description/
+
+class OptimalPartitionOfString {
+    public int partitionString(String s) {
+        // 1. Edge case: s has only 1 character.
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        StringBuilder partition = new StringBuilder();
+        boolean[] isUsed = new boolean[26];
+        boolean isTest = false;
+        int result = 0;
+
+        for (char c: s.toCharArray()) {
+            if (!isUsed[c - 'a']) {
+                partition.append(c);
+                isUsed[c - 'a'] = true;
+
+            } else {
+                if (isTest) {
+                    System.out.println(" * partition: " + partition);
+                }
+                result++;
+                partition = new StringBuilder();
+                partition.append(c);
+                Arrays.fill(isUsed, false);
+                isUsed[c - 'a'] = true;
+            }
+        }
+        if (partition.length() > 0) {
+            result++;
+        }
+        if (isTest) {
+            System.out.println(" * after loop, partition: " + partition + "\nresult: " + result);
+        }
+
+        return result;
+    }
+}
