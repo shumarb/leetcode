109c9849b20628f873d59bf933edc0df94c7be73
Umar <shumarb@outlook.com>
2026-02-19 12:48:03 +0800

    Number of Good Ways to Split a String: Refactored solution led to improved performance

--- submissions/NumberOfGoodWaysToSplitAString.java
+++ submissions/NumberOfGoodWaysToSplitAString.java
@@ -3,48 +3,40 @@
 class NumberOfGoodWaysToSplitAString {
     public int numSplits(String s) {
         boolean isTest = false;
-        int n = s.length();
+        int countDistinctLeft = 0;
+        int countDistinctRight = 0;
         int result = 0;
         int[] countLeft = new int[26];
         int[] countRight = new int[26];
 
         for (char c: s.toCharArray()) {
-            countRight[c - 'a']++;
+            if (++countRight[c - 'a'] == 1) {
+                countDistinctRight++;
+            }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            countLeft[c - 'a']++;
-            countRight[c - 'a']--;
+            if (++countLeft[c - 'a'] == 1) {
+                countDistinctLeft++;
+            }
+            if (--countRight[c - 'a'] == 0) {
+                countDistinctRight--;
+            }
 
-            if (isValid(countLeft, countRight)) {
+            if (countDistinctLeft == countDistinctRight) {
                 if (isTest) {
                     System.out.println("valid");
                 }
                 result++;
             }
             if (isTest) {
-                System.out.println("i: " + i + " -> c: " + c + "\n * countLeft: " + Arrays.toString(countLeft));
-                System.out.println(" * countRight: " + Arrays.toString(countRight));
+                System.out.println("i: " + i + " -> c: " + c + "\n * left: " + s.substring(0, i + 1) + " -> countLeft: " + Arrays.toString(countLeft));
+                System.out.println(" * right: " + s.substring(i + 1) + " -> countRight: " + Arrays.toString(countRight));
                 System.out.println("------------------------------------------------------------------------------------------------------");
             }
         }
 
         return result;
-    }
-
-    private boolean isValid(int[] first, int[] second) {
-        int count1 = 0;
-        int count2 = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (first[i] > 0) {
-                count1++;
-            }
-            if (second[i] > 0) {
-                count2++;
-            }
-        }
-        return count1 == count2;
     }
 }
