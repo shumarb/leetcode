f5cb5c3b04e987639b9ecc742093c212dccb4810
Umar <shumarb@outlook.com>
2026-02-25 18:31:55 +0800

    Search in a Binary Search Tree Refactored solution led to same performance

--- submissions/SearchInABinarySearchTree.java
+++ submissions/SearchInABinarySearchTree.java
@@ -17,16 +17,14 @@
  */
 class SearchInABinarySearchTree {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root.val == val) {
-            return root;
-
-        } else if (val < root.val && root.left != null) {
-            return searchBST(root.left, val);
-
-        } else if (val > root.val && root.right != null) {
-            return searchBST(root.right, val);
+        if (root == null) {
+            return null;
         }
 
-        return null;
+        if (root.val == val) {
+            return root;
+        }
+
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
     }
 }
