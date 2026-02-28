d1d667f1cde5518fc84f5a52a4dae776319c8a17
Umar <shumarb@outlook.com>
2026-02-28 15:06:09 +0800

    Minimum Time to Type Words Using Special Typewriter: Refactored solution led to same performance

--- submissions/MinimumTimeToTypeWordUsingSpecialTypewriter.java
+++ submissions/MinimumTimeToTypeWordUsingSpecialTypewriter.java
@@ -3,24 +3,29 @@
 class MinimumTimeToTypeWordUsingSpecialTypewriter {
     public int minTimeToType(String word) {
         boolean isTest = false;
-        char last = 'a';
-        final int click = 1;
+        char previous = 'a';
         int result = 0;
 
-        for (char c: word.toCharArray()) {
+        for (char current: word.toCharArray()) {
+            int clockwise = Math.abs(previous - current);
+            int anticlockwise = Math.abs(26 - clockwise);
+
             if (isTest) {
-                System.out.println("last: " + last + ", c: " + c);
-            }
-            int diff1 = Math.abs(last - c);
-            int diff2 = Math.abs(26 - diff1);
-            if (isTest) {
-                System.out.println(" * " + last + " ==> " + c + ": " + diff1);
-                System.out.println(" * " + c + " ==> " + last + ": " + diff2);
+                System.out.println("previous: " + previous + ", current: " + current);
+                System.out.println(" * clockwise | " + previous + " ==> " + current + ": " + clockwise);
+                System.out.println(" * anticlockwise |  " + current + " ==> " + previous + ": " + anticlockwise);
                 System.out.println("----------------------------------------------");
             }
-            result += Math.min(diff1, diff2);
-            result += click;
-            last = c;
+
+            // 1. Increase result by minimum steps from .
+            result += Math.min(clockwise, anticlockwise);
+
+            // 2. Increase result by time taken to type letter.
+            ++result;
+            previous = current;
+        }
+        if (isTest) {
+            System.out.println("result: " + result);
         }
 
         return result;
