4d1e2b95f992c6f0d4a91f1c121a3bece607ef60
Umar <shumarb@outlook.com>
2026-03-03 13:01:37 +0800

    Complete Prime Number: Refactored solution led to improved rank

--- submissions/CompletePrimeNumber.java
+++ submissions/CompletePrimeNumber.java
@@ -2,64 +2,37 @@
 
 class CompletePrimeNumber {
     public boolean completePrime(int num) {
-        String value = Integer.toString(num);
+        String number = Integer.toString(num);
         boolean isTest = false;
-        int n = value.length();
-        int prefix = 0;
-        int suffix = 0;
 
-        if (isTest) {
-            System.out.println("value: " + value + "\n---------------------------------------\nprefix:");
-        }
-        for (int i = 0; i < n; i++) {
-            prefix = Integer.parseInt(value.substring(0, i + 1));
+        for (int i = 0; i < number.length(); i++) {
+            int prefix = Integer.parseInt(number.substring(0, i + 1));
+            int suffix = Integer.parseInt(number.substring(i));
             if (isTest) {
-                System.out.print(" * prefix: " + prefix);
+                System.out.println("i: " + i);
+                System.out.println(" * prefix: " + prefix + "\n * suffix: " + suffix);
+                System.out.println("-----------------------------------------------");
             }
-            if (!isPrime(prefix)) {
-                if (isTest) {
-                    System.out.println(" -> not prime");
-                }
-                return false;
-            }
-            if (isTest) {
-                System.out.println();
-            }
-        }
 
-        if (isTest) {
-            System.out.println("---------------------------------------\nsuffix:");
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            suffix = Integer.parseInt(value.substring(i));
-            if (isTest) {
-                System.out.print(" * suffix: " + suffix);
-            }
-            if (!isPrime(suffix)) {
-                if (isTest) {
-                    System.out.println(" -> not prime");
-                }
+            if (!isPrime(prefix) || !isPrime(suffix)) {
                 return false;
-            }
-            if (isTest) {
-                System.out.println();
             }
         }
 
         return true;
     }
 
-    private boolean isPrime(int n) {
-        if (n <= 1) {
+    private boolean isPrime(int num) {
+        if (num <= 1) {
             return false;
 
-        } else if (n == 2) {
+        } else if (num == 2) {
             return true;
         }
 
-        int limit = (int) Math.sqrt(n);
+        int limit = (int) Math.sqrt(num);
         for (int i = 2; i <= limit; i++) {
-            if (n % i == 0) {
+            if (num % i == 0) {
                 return false;
             }
         }
