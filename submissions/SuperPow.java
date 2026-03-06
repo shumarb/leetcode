30ce33399d3ea5599c54ce04f5e465ddfcab8b00
Umar <shumarb@outlook.com>
2026-03-06 12:02:10 +0800

    Super Pow: Refactored solution led to improved performance

--- submissions/SuperPow.java
+++ submissions/SuperPow.java
@@ -6,7 +6,6 @@ class SuperPow {
     public int superPow(int a, int[] b) {
         BigInteger aValue = new BigInteger(Integer.toString(a));
         BigInteger exponent;
-        BigInteger mod = new BigInteger(Integer.toString(1337));
         BigInteger result;
         StringBuilder bSb = new StringBuilder();
         boolean isTest = false;
@@ -15,7 +14,7 @@ class SuperPow {
             bSb.append(digit);
         }
         exponent = new BigInteger(bSb.toString());
-        aValue = aValue.modPow(exponent, mod);
+        aValue = aValue.modPow(exponent, new BigInteger("1337"));
         if (isTest) {
             System.out.println("aValue: " + aValue + "\nexponent: " + exponent);
         }
