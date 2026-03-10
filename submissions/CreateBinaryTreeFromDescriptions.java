683d904502f30781a909cb19d58cb576aadbffca
Umar <shumarb@outlook.com>
2026-03-10 15:41:31 +0800

    Create Binary Tree From Descriptions: Refactored solution led to improved performance

--- submissions/CreateBinaryTreeFromDescriptions.java
+++ submissions/CreateBinaryTreeFromDescriptions.java
@@ -23,40 +23,36 @@ class CreateBinaryTreeFromDescriptions {
         boolean isTest = false;
 
         for (int[] row: descriptions) {
-            int parent = row[0];
             int child = row[1];
             int isLeft = row[2];
+            int parent = row[0];
             if (isTest) {
                 System.out.println(" * parent: " + parent + " | child: " + child + " | isLeft: " + isLeft);
             }
 
-            if (!map.containsKey(parent)) {
-                map.put(parent, new TreeNode(parent));
-            }
-            if (!map.containsKey(child)) {
-                map.put(child, new TreeNode(child));
-            }
-
-            TreeNode childNode = map.get(child);
-            TreeNode parentNode = map.get(parent);
+            TreeNode childNode = map.getOrDefault(child, new TreeNode(child));
+            TreeNode parentNode = map.getOrDefault(parent, new TreeNode(parent));
             if (isLeft == 1) {
                 parentNode.left = childNode;
             } else {
                 parentNode.right = childNode;
             }
+
+            map.put(child, childNode);
+            map.put(parent, parentNode);
             nonRoots.add(child);
         }
         if (isTest) {
             System.out.println("---------------------------------------\nmap:");
             for (Map.Entry<Integer, TreeNode> e: map.entrySet()) {
-                TreeNode node = e.getValue();
                 System.out.print(" * " + e.getKey());
+
+                TreeNode node = e.getValue();
                 if (node.left != null) {
                     System.out.print(" | left: " + node.left.val);
                 } else {
                     System.out.print(" | left: null ");
                 }
-
                 if (node.right != null) {
                     System.out.println(" | right: " + node.right.val);
                 } else {
