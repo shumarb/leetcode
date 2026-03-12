bb1290cd40d64065382aa61afdac0e987a82650a
Umar <shumarb@outlook.com>
2026-03-13 00:46:54 +0800

    Trim Trailing Words: Refactored solution led to same performance
    # Conflicts:
    #	submissions/TrimTrailingWords.java

--- submissions/TrimTrailingWords.java
+++ submissions/TrimTrailingWords.java
@@ -1,21 +1,14 @@
-// Question:
-
 class TrimTrailingWords {
     public String trimTrailingVowels(String s) {
         boolean isTest = false;
         int index = s.length() - 1;
 
-        while (index >= 0 && isVowel(s.charAt(index))) {
-            index--;
+                index--;
         }
         if (isTest) {
             System.out.println("index: " + index);
         }
 
         return s.substring(0, index + 1);
-    }
-
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
