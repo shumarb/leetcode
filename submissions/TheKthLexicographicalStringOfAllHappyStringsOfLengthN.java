63185ca7189806f30a5e231822d08e08384e10da
Umar <shumarb@outlook.com>
2026-03-14 18:24:45 +0800

    New submission: The k-th Lexicographical String of All Happy Strings of Length n

--- submissions/TheKthLexicographicalStringOfAllHappyStringsOfLengthN.java
+++ submissions/TheKthLexicographicalStringOfAllHappyStringsOfLengthN.java
@@ -1,5 +1,6 @@
 // Question: https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/
 
+public class TheKthLexicographicalStringOfAllHappyStringsOfLengthN {
     private boolean isTest;
     private char[] letters;
     private int k;
