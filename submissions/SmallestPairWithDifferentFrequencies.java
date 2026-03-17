b22eb7733c6a9281d16cda841feba51d6c206602
Umar <shumarb@outlook.com>
2026-03-17 15:10:03 +0800

    Palindromic Substrings: Updated rank after latest check

--- submissions/SmallestPairWithDifferentFrequencies.java
+++ submissions/SmallestPairWithDifferentFrequencies.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/smallest-pair-with-different-frequencies/description/
 
-class SmallestPairWithDifferentFrequencies {
+class SmallestPairWithDifferentFrequencie {
     public int[] minDistinctFreqPair(int[] nums) {
         // 1. Edge case: nums has 1 element.
         if (nums.length == 1) {
@@ -9,26 +9,24 @@ class SmallestPairWithDifferentFrequencies {
 
         boolean isTest = false;
         int[] countElements = populate(nums);
-        int first = -1;
-        int second = -1;
+        int firstUniqueFrequency = -1;
+        int secondUniqueFrequency = -1;
         int x = -1;
         int y = -1;
+
         for (int i = 1; i < countElements.length; i++) {
-            if (countElements[i] > 0) {
-                first = countElements[i];
+            if (countElements[i] > 0 && firstUniqueFrequency == -1) {
+                firstUniqueFrequency = countElements[i];
                 x = i;
-                break;
-            }
-        }
-        for (int i = 1; i < countElements.length; i++) {
-            if (countElements[i] > 0 && countElements[i] != first) {
-                second = countElements[i];
+            } else if (countElements[i] > 0 && countElements[i] != firstUniqueFrequency) {
+                secondUniqueFrequency = countElements[i];
                 y = i;
                 break;
             }
         }
         if (isTest) {
-            System.out.println("countElements: " + Arrays.toString(countElements) + "\nx: " + x + "\ny: " + y);
+            System.out.println("countElements: " + Arrays.toString(countElements) + "\nx: " + x + " | firstUniqueFrequency: " + firstUniqueFrequency);
+            System.out.println("y: " + y + " | secondUniqueFrequency: " + secondUniqueFrequency);
         }
 
         return x == -1 || y == -1 ? new int[] {-1, -1} : new int[] {x, y};
