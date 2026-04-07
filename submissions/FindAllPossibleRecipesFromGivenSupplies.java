3359d54231a8b467a377bedfcda586d25f3e7b78
Umar <shumarb@outlook.com>
2026-04-08 01:13:52 +0800

    Find All Possible Recipes from Given Supplies: Refactored solution led to improved performance

--- submissions/FindAllPossibleRecipesFromGivenSupplies.java
+++ submissions/FindAllPossibleRecipesFromGivenSupplies.java
@@ -3,23 +3,19 @@
 class FindAllPossibleRecipesFromGivenSupplies {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         List<String> result = new ArrayList<>();
-        Map<String, Set<String>> graph = new HashMap<>();
+        Map<String, List<String>> graph = new HashMap<>();
         Map<String, Integer> inDegree = new HashMap<>();
         Queue<String> queue = new LinkedList<>();
-        Set<String> recipesSet = new HashSet<>();
+        Set<String> recipesSet = new HashSet<>(Arrays.asList(recipes));
         boolean isTest = false;
         int n = recipes.length;
 
         for (int i = 0; i < n; i++) {
             String recipe = recipes[i];
-            inDegree.put(recipe, 0);
-            recipesSet.add(recipe);
 
             for (String part: ingredients.get(i)) {
-                inDegree.putIfAbsent(part, 0);
+                graph.computeIfAbsent(part, k -> new ArrayList<>()).add(recipe);
                 inDegree.put(recipe, 1 + inDegree.getOrDefault(recipe, 0));
-                graph.putIfAbsent(part, new HashSet<>());
-                graph.get(part).add(recipe);
             }
         }
         if (isTest) {
@@ -31,34 +27,21 @@ class FindAllPossibleRecipesFromGivenSupplies {
             for (String key: graph.keySet()) {
                 System.out.println(" * " + key + ": " + graph.get(key));
             }
-            System.out.println("-----------------------------------------------");
         }
 
         for (String supply: supplies) {
-            if (inDegree.containsKey(supply) && inDegree.get(supply) == 0) {
-                queue.offer(supply);
-            }
+            queue.offer(supply);
         }
         while (!queue.isEmpty()) {
-            if (isTest) {
-                System.out.println(" * queue: " + queue);
+            String source = queue.poll();
+
+            if (recipesSet.contains(source)) {
+                result.add(source);
             }
-
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                String source = queue.poll();
-
-                if (recipesSet.contains(source)) {
-                    result.add(source);
-                }
-                if (graph.containsKey(source)) {
-                    for (String destination: graph.get(source)) {
-                        inDegree.put(destination, inDegree.get(destination) - 1);
-                        if (inDegree.get(destination) == 0) {
-                            queue.offer(destination);
-                        }
-                    }
+            for (String destination: graph.getOrDefault(source, new ArrayList<>())) {
+                inDegree.put(destination, inDegree.get(destination) - 1);
+                if (inDegree.get(destination) == 0) {
+                    queue.offer(destination);
                 }
             }
         }
