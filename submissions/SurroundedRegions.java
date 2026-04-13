9a4b26fc42de6d3d96b862c26d72d94cd04dd666
Umar <shumarb@outlook.com>
2026-04-14 00:23:59 +0800

    Surrounded Regions: Refactored solution led to same performance

--- submissions/SurroundedRegions.java
+++ submissions/SurroundedRegions.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/surrounded-regions/description/
 
-class SurroundedRegions {
+class SurroundedRegions class Solution {
     private List<int[]> component;
     private boolean[][] isVisited;
     private char[][] board;
@@ -25,33 +25,35 @@ class SurroundedRegions {
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O' && !isVisited[i][j]) {
-                    dfs(i, j);
-                    if (isTest) {
-                        System.out.print("component: ");
-                        for (int[] point: component) {
-                            System.out.print(Arrays.toString(point) + " ");
-                        }
-                        System.out.println();
-                    }
-
-                    boolean isValid = true;
-                    for (int[] point: component) {
-                        int column = point[1];
-                        int row = point[0];
-
-                        if (column == 0 || column == n - 1 || row == 0 || row == m - 1) {
-                            isValid = false;
-                            break;
-                        }
-                    }
-                    if (isValid) {
-                        for (int[] point: component) {
-                            board[point[0]][point[1]] = 'X';
-                        }
-                    }
-                    component.clear();
+                if (board[i][j] == 'X' || isVisited[i][j]) {
+                    continue;
                 }
+
+                dfs(i, j);
+                if (isTest) {
+                    System.out.print("component: ");
+                    for (int[] point: component) {
+                        System.out.print(Arrays.toString(point) + " ");
+                    }
+                    System.out.println();
+                }
+
+                boolean isValid = true;
+                for (int[] point: component) {
+                    int column = point[1];
+                    int row = point[0];
+
+                    if (column == 0 || column == n - 1 || row == 0 || row == m - 1) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid) {
+                    for (int[] point: component) {
+                        board[point[0]][point[1]] = 'X';
+                    }
+                }
+                component.clear();
             }
         }
         if (isTest) {
