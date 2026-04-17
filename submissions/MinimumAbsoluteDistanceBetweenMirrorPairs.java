2086cbfe8ca5b0754ce7040fe5a2aa053ac56524
Umar <shumarb@outlook.com>
2026-04-17 10:21:04 +0800

    Minimum Absolute Difference Between Mirror Pairs: Refactored solution led to improved performance

--- submissions/MinimumAbsoluteDistanceBetweenMirrorPairs.java
+++ submissions/MinimumAbsoluteDistanceBetweenMirrorPairs.java
@@ -2,27 +2,41 @@
 
 class MinimumAbsoluteDistanceBetweenMirrorPairs {
     public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap();
+        boolean isTest = false;
+        int n = nums.length;
         int result = Integer.MAX_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
+        if (isTest) {
+            System.out.println("nums: " + Arrays.toString(nums));
+        }
+        for (int i = 0; i < n; i++) {
             int current = nums[i];
-            int reverse = reverse(current);
+            int reverse = getReverse(current);
+
             if (map.containsKey(current)) {
+                if (isTest) {
+                    System.out.println("-------------------------\nfound @ i: " + i);
+                    System.out.println(" * current: " + current + "\n * reverse: " + reverse + "\n * map: " + map);
+                }
+
                 result = Math.min(Math.abs(i - map.get(current)), result);
             }
+
             map.put(reverse, i);
+        }
+        if (isTest) {
+            System.out.println("-------------------------\nmap: " + map + "\nresult: " + result);
         }
 
         return result == Integer.MAX_VALUE ? -1 : result;
     }
 
-    private int reverse(int n) {
+    private int getReverse(int n) {
         int result = 0;
 
         while (n > 0) {
-            result *= 10;
-            result += n % 10;
+            result = result * 10 + n % 10;
             n /= 10;
         }
 
