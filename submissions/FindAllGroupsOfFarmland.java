cfe00aeb75652f59c61ea94900ad244ace11df01
Umar <shumarb@outlook.com>
2026-04-19 17:46:43 +0800

    Find All Groups of Farmland: Refactored solution led to improved performance

--- submissions/FindAllGroupsOfFarmland.java
+++ submissions/FindAllGroupsOfFarmland.java
@@ -1,7 +1,6 @@
 // Question: https://leetcode.com/problems/find-all-groups-of-farmland/description/
 
 class FindAllGroupsOfFarmland {
-    private boolean[][] isVisited;
     private int[][] land;
     private int m;
     private int n;
@@ -12,15 +11,18 @@ class FindAllGroupsOfFarmland {
         int[][] result = new int[0][0];
         m = land.length;
         n = land[0].length;
-        isVisited = new boolean[m][n];
         this.land = land;
 
         if (isTest) {
-            print("land:");
+            System.out.println("land:");
+            for (int[] row: land) {
+                System.out.println(Arrays.toString(row));
+            }
+            System.out.println("------------------------------------");
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (land[i][j] == 0 || isVisited[i][j]) {
+                if (land[i][j] == 0) {
                     continue;
                 }
 
@@ -63,7 +65,7 @@ class FindAllGroupsOfFarmland {
                 int column = top[1];
                 int row = top[0];
 
-                isVisited[row][column] = true;
+                land[row][column] = 0;
                 result[2] = row;
                 result[3] = column;
 
@@ -73,27 +75,15 @@ class FindAllGroupsOfFarmland {
                     int adjacentRow = row + direction[0];
 
                     if (adjacentColumn < 0 || adjacentColumn >= n || adjacentRow < 0 || adjacentRow >= m
-                            || land[adjacentRow][adjacentColumn] == 0 || isVisited[adjacentRow][adjacentColumn]) {
+                            || land[adjacentRow][adjacentColumn] == 0) {
                         continue;
                     }
-                    isVisited[adjacentRow][adjacentColumn] = true;
+                    land[adjacentRow][adjacentColumn] = 0;
                     queue.offer(new int[] {adjacentRow, adjacentColumn});
                 }
             }
         }
 
         return result;
-    }
-
-    private void print(String s) {
-        System.out.println(s);
-        for (int[] row: land) {
-            System.out.println(Arrays.toString(row));
-        }
-        System.out.println("\nisVisited:");
-        for (boolean[] row: isVisited) {
-            System.out.println(Arrays.toString(row));
-        }
-        System.out.println("------------------------------------");
     }
 }
