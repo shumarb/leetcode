0812ca7b4e8d5b6b5a1c817159cd688953677e78
Umar <shumarb@outlook.com>
2026-04-24 00:56:37 +0800

    Smallest Stable Index II: Refactored solution led to improved performance

--- submissions/SmallestStableIndexTwo.java
+++ submissions/SmallestStableIndexTwo.java
@@ -3,27 +3,22 @@
 class SmallestStableIndexTwo {
     public int firstStableIndex(int[] nums, int k) {
         boolean isTest = false;
+        int maximum = nums[0];
         int n = nums.length;
-        int[] prefix = new int[n];
         int[] suffix = new int[n];
 
-        prefix[0] = nums[0];
         suffix[n - 1] = nums[n - 1];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(prefix[i - 1], nums[i]);
-        }
         for (int i = n - 2; i >= 0; i--) {
             suffix[i] = Math.min(suffix[i + 1], nums[i]);
         }
         if (isTest) {
-            System.out.println("prefix: " + Arrays.toString(prefix));
             System.out.println("nums:   " + Arrays.toString(nums));
             System.out.println("suffix: " + Arrays.toString(suffix));
             System.out.println("----------------------------------");
         }
 
         for (int i = 0; i < n; i++) {
-            int maximum = prefix[i];
+            maximum = Math.max(maximum, nums[i]);
             int minimum = suffix[i];
             int instability = maximum - minimum;
             if (isTest) {
