71b8609a999f11c23f4e60d133c365c52be50885
Umar <shumarb@outlook.com>
2026-04-27 15:30:44 +0800

    Max Consecutives Ones: Refactored solution led to improved rank

--- submissions/MaxConsecutiveOnes.java
+++ submissions/MaxConsecutiveOnes.java
@@ -2,18 +2,18 @@
 
 class MaxConsecutiveOnes {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxLength = 0;
-        int currentStreak = 0;
+        int current = 0;
+        int result = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                currentStreak++;
+        for (int e: nums) {
+            if (e == 1) {
+                current++;
             } else {
-                maxLength = Math.max(currentStreak, maxLength);
-                currentStreak = 0;
+                result = Math.max(current, result);
+                current = 0;
             }
         }
 
-        return Math.max(maxLength, currentStreak);
+        return Math.max(current, result);
     }
 }
