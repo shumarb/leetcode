66159daea8adcbe2a3382471bddfc3607cbfaa25
Umar <shumarb@outlook.com>
2026-04-30 00:04:34 +0800

    Pacific Atlantic Water Flow: Refactored solution led to same performance

--- submissions/PacificAtlanticWaterFlow.java
+++ submissions/PacificAtlanticWaterFlow.java
@@ -1,9 +1,6 @@
 // Question: https://leetcode.com/problems/pacific-atlantic-water-flow/description/
 
 class PacificAtlanticWaterFlow {
-    private boolean[][] canReachAtlantic;
-    private boolean[][] canReachPacific;
-    private boolean[][] isVisited;
     private int[][] heights;
     private int m;
     private int n;
@@ -12,29 +9,20 @@ class PacificAtlanticWaterFlow {
         List<List<Integer>> result = new ArrayList<>();
         Queue<int[]> atlanticQueue = new LinkedList<>();
         Queue<int[]> pacificQueue = new LinkedList<>();
-        boolean isTest = false;
         m = heights.length;
         n = heights[0].length;
-        canReachAtlantic = new boolean[m][n];
-        canReachPacific = new boolean[m][n];
-        isVisited = new boolean[m][n];
+        boolean[][] canReachAtlantic = new boolean[m][n];
+        boolean[][] canReachPacific = new boolean[m][n];
+        boolean isTest = false;
         this.heights = heights;
 
         for (int i = 0; i < m; i++) {
-            canReachAtlantic[i][n - 1] = true;
-            canReachPacific[i][0] = true;
             atlanticQueue.offer(new int[] {i, n - 1});
             pacificQueue.offer(new int[] {i, 0});
-            isVisited[i][n - 1] = true;
-            isVisited[i][0] = true;
         }
         for (int j = 0; j < n; j++) {
-            canReachAtlantic[m - 1][j] = true;
-            canReachPacific[0][j] = true;
             atlanticQueue.offer(new int[] {m - 1, j});
             pacificQueue.offer(new int[] {0, j});
-            isVisited[m - 1][j] = true;
-            isVisited[0][j] = true;
         }
 
         if (isTest) {
@@ -42,11 +30,11 @@ class PacificAtlanticWaterFlow {
             for (int[] row: heights) {
                 System.out.println(Arrays.toString(row));
             }
-            print("-----------------------------------\nbefore:");
+            print("-----------------------------------\nbefore:", canReachAtlantic, canReachPacific);
         }
 
-        bfs(atlanticQueue, 0);
-        bfs(pacificQueue, 1);
+        bfs(atlanticQueue, canReachAtlantic);
+        bfs(pacificQueue, canReachPacific);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (canReachAtlantic[i][j] && canReachPacific[i][j]) {
@@ -55,7 +43,7 @@ class PacificAtlanticWaterFlow {
             }
         }
         if (isTest) {
-            print("-----------------------------------\nafter:");
+            print("-----------------------------------\nafter:", canReachAtlantic, canReachPacific);
             System.out.println("\n-----------------------------------\nresult:");
             for (List<Integer> e: result) {
                 System.out.println(e);
@@ -65,8 +53,7 @@ class PacificAtlanticWaterFlow {
         return result;
     }
 
-    private void bfs(Queue<int[]> queue, int oceanCode) {
-        boolean[][] isVisited = new boolean[m][n];
+    private void bfs(Queue<int[]> queue, boolean[][] isVisited) {
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         while (!queue.isEmpty()) {
@@ -74,11 +61,7 @@ class PacificAtlanticWaterFlow {
             int column = source[1];
             int row = source[0];
 
-            if (oceanCode == 0) {
-                canReachAtlantic[row][column] = true;
-            } else {
-                canReachPacific[row][column] = true;
-            }
+            isVisited[row][column] = true;
 
             for (int[] direction: directions) {
                 int adjacentColumn = column + direction[1];
@@ -86,17 +69,17 @@ class PacificAtlanticWaterFlow {
 
                 if (adjacentColumn < 0 || adjacentColumn >= n
                         || adjacentRow < 0 || adjacentRow >= m
-                        || isVisited[adjacentRow][adjacentColumn] || heights[adjacentRow][adjacentColumn] < heights[row][column]) {
+                        || isVisited[adjacentRow][adjacentColumn]
+                        || heights[adjacentRow][adjacentColumn] < heights[row][column]) {
                     continue;
                 }
 
                 queue.offer(new int[] {adjacentRow, adjacentColumn});
-                isVisited[adjacentRow][adjacentColumn] = true;
             }
         }
     }
 
-    private void print(String s) {
+    private void print(String s, boolean[][] canReachAtlantic, boolean[][] canReachPacific) {
         System.out.println(s);
         System.out.println("\ncanReachAtlantic:");
         for (boolean[] row: canReachAtlantic) {
