d510727c82abbcb0054fe62e8873d8bfed0213f3
Umar <shumarb@outlook.com>
2026-04-29 16:04:06 +0800

    Pacific Atlantic Water Flow: Refactored solution led to same performance

--- submissions/PacificAtlanticWaterFlow.java
+++ submissions/PacificAtlanticWaterFlow.java
@@ -1,35 +1,59 @@
 // Question: https://leetcode.com/problems/pacific-atlantic-water-flow/description/
 
-class Solution {
-    private boolean[][] isAtlantic;
-    private boolean[][] isPacific;
-    private boolean isTest;
+class PacificAtlanticWaterFlow {
+    private boolean[][] canReachAtlantic;
+    private boolean[][] canReachPacific;
     private int[][] heights;
     private int m;
     private int n;
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> result = new ArrayList<>();
-        isTest = false;
+        boolean isTest = false;
         m = heights.length;
         n = heights[0].length;
-        isAtlantic = new boolean[m][n];
-        isPacific = new boolean[m][n];
+        canReachAtlantic = new boolean[m][n];
+        canReachPacific = new boolean[m][n];
         this.heights = heights;
 
-        populate();
+        for (int i = 0; i < m; i++) {
+            canReachAtlantic[i][n - 1] = true;
+            canReachPacific[i][0] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            canReachAtlantic[m - 1][j] = true;
+            canReachPacific[0][j] = true;
+        }
+
         if (isTest) {
-            print();
+            System.out.println("heights: ");
+            for (int[] row: heights) {
+                System.out.println(Arrays.toString(row));
+            }
+            print("-----------------------------------\nbefore:");
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    bfs(i, j, "pacific");
+                }
+
+                if (i == m - 1 || j == n - 1) {
+                    bfs(i, j, "atlantic");
+                }
+            }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (bfs(i, j)) {
+                if (canReachAtlantic[i][j] && canReachPacific[i][j]) {
                     result.add(Arrays.asList(i, j));
                 }
             }
         }
         if (isTest) {
-            System.out.println("----------------------------\nresult:");
+            print("-----------------------------------\nafter:");
+            System.out.println("\n-----------------------------------\nresult:");
             for (List<Integer> e: result) {
                 System.out.println(e);
             }
@@ -38,87 +62,50 @@ class Solution {
         return result;
     }
 
-    private boolean bfs(int i, int j) {
+    private void bfs(int i, int j, String ocean) {
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] isVisited = new boolean[m][n];
-        boolean canReachAtlantic = false;
-        boolean canReachPacific = false;
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         queue.offer(new int[] {i, j});
         isVisited[i][j] = true;
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
+            int[] source = queue.poll();
+            int column = source[1];
+            int row = source[0];
 
-            while (size-- > 0) {
-                int[] source = queue.poll();
-                int column = source[1];
-                int row = source[0];
+            if (ocean.equals("atlantic")) {
+                canReachAtlantic[row][column] = true;
+            } else {
+                canReachPacific[row][column] = true;
+            }
 
-                if (isPacific[row][column]) {
-                    canReachPacific = true;
+            for (int[] direction: directions) {
+                int adjacentColumn = column + direction[1];
+                int adjacentRow = row + direction[0];
+
+                if (adjacentColumn < 0 || adjacentColumn >= n
+                        || adjacentRow < 0 || adjacentRow >= m
+                        || isVisited[adjacentRow][adjacentColumn] || heights[adjacentRow][adjacentColumn] < heights[row][column]) {
+                    continue;
                 }
 
-                if (isAtlantic[row][column]) {
-                    canReachAtlantic = true;
-                }
-
-                if (canReachAtlantic && canReachPacific) {
-                    return true;
-                }
-
-                for (int[] direction: directions) {
-                    int adjacentColumn = column + direction[1];
-                    int adjacentRow = row + direction[0];
-
-                    if (adjacentColumn < 0 || adjacentColumn >= n
-                            || adjacentRow < 0 || adjacentRow >= m
-                            || isVisited[adjacentRow][adjacentColumn]
-                            || heights[adjacentRow][adjacentColumn] > heights[row][column]) {
-                        continue;
-                    }
-
-                    queue.offer(new int[] {adjacentRow, adjacentColumn});
-                    isVisited[adjacentRow][adjacentColumn] = true;
-                }
+                queue.offer(new int[] {adjacentRow, adjacentColumn});
+                isVisited[adjacentRow][adjacentColumn] = true;
             }
         }
-
-        return canReachAtlantic && canReachPacific;
     }
 
-    private void populate() {
-        for (int i = 0; i < n; i++) {
-            isPacific[0][i] = true;
-        }
-
-        for (int i = 0; i < m; i++) {
-            isPacific[i][0] = true;
-        }
-
-        for (int i = 0; i < m; i++) {
-            isAtlantic[i][n - 1] = true;
-        }
-
-        for (int i = 0; i < n; i++) {
-            isAtlantic[m - 1][i] = true;
-        }
-    }
-
-    private void print() {
-        System.out.println("heights");
-        for (int[] row: heights) {
+    private void print(String s) {
+        System.out.println(s);
+        System.out.println("\ncanReachAtlantic:");
+        for (boolean[] row: canReachAtlantic) {
             System.out.println(Arrays.toString(row));
         }
 
-        System.out.println("\nisAtlantic:");
-        for (boolean[] row: isAtlantic) {
-            System.out.println(Arrays.toString(row));
-        }
-
-        System.out.println("\nisPacific:");
-        for (boolean[] row: isPacific) {
+        System.out.println("\ncanReachPacific:");
+        for (boolean[] row: canReachPacific) {
             System.out.println(Arrays.toString(row));
         }
     }
