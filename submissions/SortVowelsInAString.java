1fa513865480b99be11877235b9e28d53bc13df4
Umar <shumarb@outlook.com>
2026-05-03 22:22:07 +0800

    Sort Vowels in a String: Refactored solution led to improved performance

--- submissions/SortVowelsInAString.java
+++ submissions/SortVowelsInAString.java
@@ -2,23 +2,28 @@
 
 class SortVowelsInAString {
     public String sortVowels(String s) {
-        PriorityQueue<Character> minHeap = new PriorityQueue<>();
+        List<Character> list = new ArrayList<>();
         boolean isTest = false;
         char[] letters = s.toCharArray();
 
         for (char letter: letters) {
             if (isVowel(letter)) {
-                minHeap.offer(letter);
+                list.add(letter);
+            }
+        }
+        Collections.sort(list);
+        if (isTest) {
+            System.out.println("s: " + s + "\nbefore, letters: " + Arrays.toString(letters) + "\nlist: " + list);
+        }
+
+        int j = 0;
+        for (int i = 0; i < letters.length; i++) {
+            if (isVowel(letters[i])) {
+                letters[i] = list.get(j++);
             }
         }
         if (isTest) {
-            System.out.println("s: " + s + "\nletters: " + Arrays.toString(letters) + "\nminHeap: " + minHeap);
-        }
-
-        for (int i = 0; i < letters.length; i++) {
-            if (isVowel(letters[i])) {
-                letters[i] = minHeap.poll();
-            }
+            System.out.println("after, letters: " + Arrays.toString(letters));
         }
 
         return new String(letters);
