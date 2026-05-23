ca1e780e552fb848431a28c882aad485ad024bd2
Umar <shumarb@outlook.com>
2026-05-24 00:52:36 +0800

    Compare Sum of Bitonic Parts: Refactored solution led to improved performance

--- submissions/CompareSumsOfBitonicParts.java
+++ submissions/CompareSumsOfBitonicParts.java
@@ -3,17 +3,25 @@
 class CompareSumsOfBitonicParts {
     public int compareBitonicSums(int[] nums) {
         boolean isTest = false;
+        int left = 0;
         int n = nums.length;
-        int peak = nums[0];
-        int peakIndex = 0;
+        int peakIndex = -1;
+        int right = n - 1;
         long sumAscending = 0;
         long sumDescending = 0;
 
-        for (int i = 1; i < n; i++) {
-            int element = nums[i];
-            if (element > peak) {
-                peak = element;
-                peakIndex = i;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // 1. Peak is after mid-th element [mid + 1, right]
+            if (nums[mid] < nums[mid + 1]) {
+                peakIndex = mid + 1;
+                left = mid + 1;
+
+                // 2. Peak is before mid-th element [mid, right];
+            } else if (nums[mid] > nums[mid + 1]) {
+                peakIndex = mid;
+                right = mid;
             }
         }
         for (int i = 0; i <= peakIndex; i++) {
@@ -23,8 +31,7 @@ class CompareSumsOfBitonicParts {
             sumDescending += nums[i];
         }
         if (isTest) {
-            System.out.println("nums: " + Arrays.toString(nums) + "\npeakIndex: " + peakIndex + ": -> e: " + peak);
-            System.out.println("sumAscending: " + sumAscending + "\nsumDescending: " + sumDescending);
+            System.out.println("peakIndex: " + peakIndex + "\nsumAscending: " + sumAscending + "\nsumDescending: " + sumDescending);
         }
 
         if (sumAscending == sumDescending) {
