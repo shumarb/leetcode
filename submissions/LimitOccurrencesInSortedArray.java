559dd9136d555e6ed3a5108b5b368d4348345376
Umar <shumarb@outlook.com>
2026-05-24 12:59:04 +0800

    Refactored solutions led to improved performance

--- submissions/LimitOccurrencesInSortedArray.java
+++ submissions/LimitOccurrencesInSortedArray.java
@@ -2,32 +2,33 @@
 
 class LimitOccurrencesInSortedArray {
     public int[] limitOccurrences(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
         boolean isTest = false;
         int[] count = new int[101];
         int[] result = null;
+        int j = 0;
         int largest = 0;
-        int limit = k;
+        int size = 0;
 
         for (int e: nums) {
             largest = Math.max(e, largest);
             count[e]++;
         }
         for (int i = 1; i <= largest; i++) {
-            while (count[i] > 0 && limit > 0) {
-                list.add(i);
-                count[i]--;
-                limit--;
-            }
-            limit = k;
+            size += Math.min(k, count[i]);
         }
-        result = new int[list.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = list.get(i);
+
+        result = new int[size];
+        for (int i = 1; i <= largest; i++) {
+            if (count[i] > 0) {
+                int limit = Math.min(k, count[i]);
+                while (limit-- > 0) {
+                    result[j++] = i;
+                }
+            }
         }
         if (isTest) {
             System.out.println("nums: " + Arrays.toString(nums));
-            System.out.println("count: " + Arrays.toString(Arrays.copyOfRange(count, 0, largest + 1)) + "\nlist: " + list);
+            System.out.println("count: " + Arrays.toString(Arrays.copyOfRange(count, 0, largest + 1)) + "\nsize: " + size);
             System.out.println("result: " + Arrays.toString(result));
         }
 
--- submissions/PasswordStrength.java
+++ submissions/PasswordStrength.java
@@ -1,37 +1,40 @@
 // Question: https://leetcode.com/problems/password-strength/description/
 
-class PasswordStrength {
+class PasswordStrength class Solution {
     public int passwordStrength(String password) {
-        Set<Character> digit = new HashSet<>();
-        Set<Character> lower = new HashSet<>();
         Set<Character> special = new HashSet<>();
-        Set<Character> upper = new HashSet<>();
+        boolean[] digits = new boolean[10];
+        boolean[] letters = new boolean[52];
         boolean isTest = false;
         int result = 0;
 
         for (char e: password.toCharArray()) {
-            if (Character.isDigit(e) && digit.add(e)) {
+            if (Character.isDigit(e) && !digits[e - '0']) {
                 if (isTest) {
                     System.out.println(" * counted: " + e + ": " + 3);
                 }
+                digits[e - '0'] = true;
                 result += 3;
                 continue;
             }
 
-            if (Character.isLowerCase(e) && lower.add(e)) {
-                if (isTest) {
-                    System.out.println(" * counted: " + e + ": " + 1);
-                }
-                result += 1;
-                continue;
-            }
+            if (Character.isLetter(e)) {
+                if (Character.isUpperCase(e) && !letters[e - 'A']) {
+                    if (isTest) {
+                        System.out.println(" * counted: " + e + ": " + 2);
+                    }
+                    letters[e - 'A'] = true;
+                    result += 2;
+                    continue;
 
-            if (Character.isUpperCase(e) && upper.add(e)) {
-                if (isTest) {
-                    System.out.println(" * counted: " + e + ": " + 2);
+                } else if (Character.isLowerCase(e) && !letters[e - 'a' + 26]) {
+                    if (isTest) {
+                        System.out.println(" * counted: " + e + ": " + 1);
+                    }
+                    letters[e - 'a' + 26] = true;
+                    result += 1;
+                    continue;
                 }
-                result += 2;
-                continue;
             }
 
             if ("!@#$".indexOf(e) >= 0 && special.add(e)) {
