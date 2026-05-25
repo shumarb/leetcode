9ce7f10c7f295a03065e7b57f1592dfc13067eb3
Umar <shumarb@outlook.com>
2026-05-25 22:18:25 +0800

    Password Strength: Refactored solution led to improved performance

--- submissions/PasswordStrength.java
+++ submissions/PasswordStrength.java
@@ -2,51 +2,28 @@
 
 class PasswordStrength {
     public int passwordStrength(String password) {
-        Set<Character> special = new HashSet<>();
-        boolean[] digits = new boolean[10];
-        boolean[] letters = new boolean[52];
-        boolean isTest = false;
+        boolean[] isUsed = new boolean[128];
         int result = 0;
 
-        for (char e: password.toCharArray()) {
-            if (Character.isDigit(e) && !digits[e - '0']) {
-                if (isTest) {
-                    System.out.println(" * counted: " + e + ": " + 3);
-                }
-                digits[e - '0'] = true;
+        for (char c: password.toCharArray()) {
+            if (isUsed[c]) {
+                continue;
+            }
+
+            isUsed[c] = true;
+
+            if (c >= 'a' && c <= 'z') {
+                result++;
+
+            } else if (c >= 'A' && c <= 'Z') {
+                result += 2;
+
+            } else if (c >= '0' && c <= '9') {
                 result += 3;
-                continue;
-            }
 
-            if (Character.isLetter(e)) {
-                if (Character.isUpperCase(e) && !letters[e - 'A']) {
-                    if (isTest) {
-                        System.out.println(" * counted: " + e + ": " + 2);
-                    }
-                    letters[e - 'A'] = true;
-                    result += 2;
-
-                } else if (Character.isLowerCase(e) && !letters[e - 'a' + 26]) {
-                    if (isTest) {
-                        System.out.println(" * counted: " + e + ": " + 1);
-                    }
-                    letters[e - 'a' + 26] = true;
-                    result += 1;
-                }
-
-                continue;
-            }
-
-            if ("!@#$".indexOf(e) >= 0 && special.add(e)) {
-                if (isTest) {
-                    System.out.println(" * counted: " + e + ": " + 5);
-                }
+            } else {
                 result += 5;
-                continue;
             }
-        }
-        if (isTest) {
-            System.out.println("\nresult: " + result);
         }
 
         return result;
