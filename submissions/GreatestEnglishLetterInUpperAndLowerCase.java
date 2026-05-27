eaa1b8444e8016a1bce5f9f689adbf6618eb4602
Umar <shumarb@outlook.com>
2026-05-27 08:34:13 +0800

    Greatest English Letter in Upper and Lower Case: Refactored solution led to improved performance

--- submissions/GreatestEnglishLetterInUpperAndLowerCase.java
+++ submissions/GreatestEnglishLetterInUpperAndLowerCase.java
@@ -3,25 +3,23 @@
 class GreatestEnglishLetterInUpperAndLowerCase {
     public String greatestLetter(String s) {
         boolean isTest = false;
-        boolean[] isLowerCaseLetterPresent = new boolean[26];
-        boolean[] isUpperCaseLetterPresent = new boolean[26];
+        boolean[] isLower = new boolean[26];
+        boolean[] isUpper = new boolean[26];
 
-        for (char letter: s.toCharArray()) {
-            if (Character.isUpperCase(letter)) {
-                isUpperCaseLetterPresent[letter - 'A'] = true;
+        for (char c: s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                isLower[c - 'a'] = true;
             } else {
-                isLowerCaseLetterPresent[letter - 'a'] = true;
+                isUpper[c - 'A'] = true;
             }
         }
         if (isTest) {
-            System.out.println("s: " + s);
-            System.out.println("isUpperCaseLetterPresent: " + Arrays.toString(isUpperCaseLetterPresent));
-            System.out.println("isLowerCaseLetterPresent: " + Arrays.toString(isLowerCaseLetterPresent));
+            System.out.println("s: " + s + "\nisLower: " + Arrays.toString(isLower) + "\nisUpper: " + Arrays.toString(isUpper));
         }
 
         for (int i = 25; i >= 0; i--) {
-            if (isLowerCaseLetterPresent[i] && isUpperCaseLetterPresent[i]) {
-                return Character.toString('A' + i);
+            if (isLower[i] && isUpper[i]) {
+                return Character.toString((char) ('A' + i));
             }
         }
 
