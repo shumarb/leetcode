3618b5edb2ea3b96e1a8a2f6aec2171c33a66823
Umar <shumarb@outlook.com>
2026-06-01 09:58:17 +0800

    Jump Game II: Refactored solution led to improved performance

--- submissions/JumpGameII.java
+++ submissions/JumpGameII.java
@@ -1,6 +1,6 @@
-// Question:
+// Question: https://leetcode.com/problems/jump-game-ii/description/
 
-class JumpGameII {
+class JumpGameII class Solution {
     public int jump(int[] nums) {
         Queue<Integer> queue = new LinkedList<>();
         int n = nums.length;
@@ -28,12 +28,22 @@ class JumpGameII {
                     if (isTest) {
                         System.out.println(" * result @ level " + result);
                     }
+
                     return result;
                 }
 
                 int maximumJumps = nums[index];
                 for (int i = 1; i <= maximumJumps; i++) {
                     int nextIndex = index + i;
+                    if (nextIndex == n - 1) {
+                        result++;
+                        if (isTest) {
+                            System.out.println(" * result @ level " + result);
+                        }
+
+                        return result;
+                    }
+
                     if (nextIndex < n && !isVisited[nextIndex]) {
                         queue.offer(nextIndex);
                         isVisited[nextIndex] = true;
