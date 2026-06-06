7aeb1c0488b6d20aa485663f863c475a2bc3a683
Umar <shumarb@outlook.com>
2026-06-06 18:12:17 +0800

    Smallest Stable Index II: Updated rank after latest check

--- submissions/FloodFill.java
+++ submissions/FloodFill.java
@@ -3,49 +3,49 @@
 class FloodFill {
     private boolean[][] isVisited;
     private int[][] image;
-    private int color;
+    private int key;
     private int m;
     private int n;
-    private int target;
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         boolean isTest = false;
         m = image.length;
         n = image[0].length;
         isVisited = new boolean[m][n];
-        target = image[sr][sc];
-        this.color = color;
+        key = image[sr][sc];
         this.image = image;
 
         if (isTest) {
-            display("before:");
+            System.out.println("start @ [" + sr + ", " + sc + "]" + "\nconvert " + image[sr][sc] + " -> " + color);
+            print("before:");
         }
-        dfs(sr, sc);
+        dfs(sr, sc, color);
         if (isTest) {
-            display("\nafter:");
+            print("after:");
         }
 
         return image;
     }
 
-    private void dfs(int row, int column) {
-        if (row < 0 || column < 0 || row >= m || column >= n || isVisited[row][column] || image[row][column] != target) {
+    private void dfs(int row, int column, int color) {
+        if (row < 0 || row >= m || column < 0 || column >= n || isVisited[row][column] || image[row][column] != key) {
             return;
         }
 
         image[row][column] = color;
         isVisited[row][column] = true;
 
-        dfs(row - 1, column);
-        dfs(row + 1, column);
-        dfs(row, column - 1);
-        dfs(row, column + 1);
+        dfs(row - 1, column, color);
+        dfs(row + 1, column, color);
+        dfs(row, column - 1, color);
+        dfs(row, column + 1, color);
     }
 
-    private void display(String s) {
+    private void print(String s) {
         System.out.println(s);
         for (int[] row: image) {
             System.out.println(Arrays.toString(row));
         }
+        System.out.println("----------------------");
     }
 }
