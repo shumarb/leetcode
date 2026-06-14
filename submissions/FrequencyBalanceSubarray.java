e7d1a6bb13b741aafcd6f6edf5af5252c48a0d64
Umar <shumarb@outlook.com>
2026-06-14 21:44:21 +0800

    Frequency Balance Subarray: Refactored solutions led to improved performance

--- submissions/FrequencyBalanceSubarray.java
+++ submissions/FrequencyBalanceSubarray.java
@@ -6,35 +6,36 @@ class FrequencyBalanceSubarray {
         int n = nums.length;
         int result = 1;
 
-        for (int i = 0; i < n; i++) {
+        if (n == 1) {
+            return n;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
             Map<Integer, Integer> map = new HashMap<>();
-            int maximumFrequency = 1;
-            int totalElements = 1;
+            int maximumFrequency = 0;
+            int totalElements = 0;
 
-            map.put(nums[i], 1);
-            if (isTest) {
-                System.out.println("-----------------------------------------------------------------------------------------\ni: " + i);
-                System.out.println(" * incoming: " + nums[i] + " | map: " + map + " | maximumFrequency: " + maximumFrequency + " | totalElements: " + totalElements);
-            }
-
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i; j < n; j++) {
                 int incoming = nums[j];
+                int value = 1 + map.getOrDefault(incoming, 0);
+
+                map.put(incoming, value);
+                maximumFrequency = Math.max(maximumFrequency, value);
                 totalElements++;
-                map.put(incoming, 1 + map.getOrDefault(incoming, 0));
-                maximumFrequency = Math.max(maximumFrequency, map.get(incoming));
 
                 // 1. Maximum frequency must be even for formula to be applied.
                 if (maximumFrequency % 2 == 1) {
                     continue;
                 }
-                if (isTest) {
-                    System.out.println("\n * incoming: " + nums[j] + " | map: " + map + " | maximumFrequency: " + maximumFrequency + " | totalElements: " + totalElements);
-                }
 
+                if (isTest) {
+                    System.out.println("\n * incoming: " + incoming + " | map: " + map + " | maximumFrequency: " + maximumFrequency + " | totalElements: " + totalElements);
+                }
                 if (isBalanced(map, maximumFrequency)) {
                     if (isTest) {
                         System.out.println(" * balanced");
                     }
+
                     result = Math.max(result, totalElements);
                 }
             }
@@ -47,19 +48,22 @@ class FrequencyBalanceSubarray {
     }
 
     private boolean isBalanced(Map<Integer, Integer> map, int maximumFrequency) {
+        // 2. Array contains only 1 distinct value.
         if (map.size() == 1) {
             return true;
         }
 
         int halfCount = 0;
+        int halfFrequency = maximumFrequency / 2;
         int maximumCount = 0;
 
-        for (int key: map.keySet()) {
-            int value = map.get(key);
+        for (int value: map.values()) {
             if (value == maximumFrequency) {
                 maximumCount++;
-            } else if (value == maximumFrequency / 2) {
+
+            } else if (value == halfFrequency) {
                 halfCount++;
+
             } else {
                 return false;
             }
