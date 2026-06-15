e744c9d972f1afa29c6c2247001d9c26f95faa7a
Umar <shumarb@outlook.com>
2026-06-15 09:23:37 +0800

    Refactored solution and updated ranks

--- submissions/DeleteTheMiddleNodeOfALinkedList.java
+++ submissions/DeleteTheMiddleNodeOfALinkedList.java
@@ -12,40 +12,50 @@
  */
 class DeleteTheMiddleNodeOfALinkedList {
     public ListNode deleteMiddle(ListNode head) {
+        // 1. Edge case: List with 1 element.
         if (head.next == null) {
             return null;
         }
 
-        boolean isTest = false;
+        ListNode fast = head.next.next;
         ListNode slow = head;
-        ListNode fast = head;
+        boolean isTest = false;
 
         while (fast != null && fast.next != null) {
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
         }
         if (isTest) {
-            display(head);
-            System.out.println("middle: " + slow.val);
+            printList("before: ", head);
+            print("slow: ", slow);
+            print("fast: ", fast);
         }
 
-        previous.next = slow.next;
-        slow.next = null;
+        slow.next = slow.next.next;
         if (isTest) {
-            display(head);
+            printList("------------------------------\nafter: ", head);
         }
 
         return head;
     }
 
-    private void display(ListNode head) {
+    private void print(String s, ListNode node) {
+        System.out.print(s);
+        if (node == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(node.val);
+        }
+    }
+
+    private void printList(String s, ListNode head) {
         ListNode current = head;
+
+        System.out.print(s);
         while (current != null) {
-            System.out.print(current.val + " --> ");
-            if (current.next == null) {
-                System.out.println(current.val);
-            }
+            System.out.print(current.val + " ");
             current = current.next;
         }
+        System.out.println("\n------------------------------");
     }
 }
