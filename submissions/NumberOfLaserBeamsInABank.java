d4c18bbd2a7030b04ce846f9b67e7f0c37aec3c5
Umar <shumarb@outlook.com>
2026-06-15 15:32:05 +0800

    Number of Laser Beams in a Bank: Refactored solutions led to improved performance

--- submissions/NumberOfLaserBeamsInABank.java
+++ submissions/NumberOfLaserBeamsInABank.java
@@ -2,59 +2,21 @@
 
 class NumberOfLaserBeamsInABank {
     public int numberOfBeams(String[] bank) {
-        boolean isTest = false;
-        int index = 0;
-        int m = bank.length;
-        int n = bank[0].length();
+        int previous = 0;
         int result = 0;
-        int[] rowsWithLasers = new int[m];
 
-        for (int i = 0; i < m; i++) {
-            if (bank[i].indexOf('1') >= 0) {
-                rowsWithLasers[index++] = i;
+        for (String e: bank) {
+            int current = 0;
+
+            for (char c: e.toCharArray()) {
+                current += (c - '0');
             }
-        }
-        if (isTest) {
-            System.out.println("bank: " + Arrays.toString(bank));
-            System.out.println("rows with lasers: " + Arrays.toString(rowsWithLasers));
-            System.out.println("------------------------------------------");
-        }
-
-        for (int i = 0; i < index - 1; i++) {
-            int current = rowsWithLasers[i];
-            int next = rowsWithLasers[i + 1];
-            int totalLaserBeams = count(bank[current], bank[next]);
-
-            if (isTest) {
-                System.out.println(" * " + current + " -> " + next + ": " + totalLaserBeams);
+            if (current > 0) {
+                result += current * previous;
+                previous = current;
             }
-
-            result += totalLaserBeams;
-        }
-        if (isTest) {
-            System.out.println("------------------------------------------\nresult: " + result);
         }
 
         return result;
-    }
-
-    private int count(String first, String second) {
-        char[] firstTokens = first.toCharArray();
-        char[] secondTokens = second.toCharArray();
-        int lasersInFirst = 0;
-        int lasersInSecond = 0;
-
-        for (char e: firstTokens) {
-            if (e == '1') {
-                lasersInFirst++;
-            }
-        }
-        for (char e: secondTokens) {
-            if (e == '1') {
-                lasersInSecond++;
-            }
-        }
-
-        return lasersInFirst * lasersInSecond;
     }
 }
