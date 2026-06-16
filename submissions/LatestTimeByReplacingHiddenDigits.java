d0e0a5a83c5fb0b392497b512f20729d531efcf3
Umar <shumarb@outlook.com>
2026-06-16 10:23:28 +0800

    Latest Time by Replacing Hidden Digits: Refactored solution led to same performance

--- submissions/LatestTimeByReplacingHiddenDigits.java
+++ submissions/LatestTimeByReplacingHiddenDigits.java
@@ -5,16 +5,11 @@ class LatestTimeByReplacingHiddenDigits {
         char[] digits = time.toCharArray();
 
         if (digits[0] == '?') {
-            if (digits[1] == '?' || digits[1] <= '3') {
-                digits[0] = '2';
-            }
-            else {
-                digits[0] = '1';
-            }
+            digits[0] = digits[1] == '?' || digits[1] <= '3' ? '2' : '1';
         }
 
         if (digits[1] == '?') {
-            digits[1] = digits[0] == '0' || digits[0] == '1' ? '9' : '3';
+            digits[1] = digits[0] <= '1' ? '9' : '3';
         }
 
         if (digits[3] == '?') {
