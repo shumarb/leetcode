964556e4e0f4f93831d6bd88d0febd9b944e91f5
Umar <shumarb@outlook.com>
2026-06-28 22:52:36 +0800

    Maximum Element after Decreasing and Rearranging: Refactored solution led to same performance

--- submissions/MaximumElementAfterDecreasingAndRearranging.java
+++ submissions/MaximumElementAfterDecreasingAndRearranging.java
@@ -13,34 +13,15 @@ class MaximumElementAfterDecreasingAndRearranging {
         if (isTest) {
             System.out.println("sorted arr: " + Arrays.toString(arr));
         }
-        if (isValid(arr)) {
-            return arr[n - 1];
-        }
 
         arr[0] = 1;
         for (int i = 1; i < n; i++) {
-            if (Math.abs(arr[i] - arr[i - 1]) > 1) {
-                arr[i] = 1 + arr[i - 1];
-            }
+            arr[i] = Math.min(arr[i], 1 + arr[i - 1]);
         }
         if (isTest) {
             System.out.println("after update, arr: " + Arrays.toString(arr));
         }
 
         return arr[n - 1];
-    }
-
-    private boolean isValid(int[] arr) {
-        if (arr[0] != 1) {
-            return false;
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            if (Math.abs(arr[i] - arr[i - 1]) > 1) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
