6555cbf303848bf3115163a8b12d638be92428eb
Umar <shumarb@outlook.com>
2026-06-29 09:19:48 +0800

    Number of Provinces: Refactored solution led to improved performance

--- submissions/NumberOfProvinces.java
+++ submissions/NumberOfProvinces.java
@@ -1,48 +1,27 @@
 // Question: https://leetcode.com/problems/number-of-provinces/description/
 
 class NumberOfProvinces {
-    private int n;
-    private boolean[] isVisited;
-
     public int findCircleNum(int[][] isConnected) {
-        int countProvinces = 0;
-        n = isConnected.length;
-        isVisited = new boolean[n];
+        int n = isConnected.length;
+        int result = 0;
+        boolean[] isVisited = new boolean[n];
 
         for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
-                // 1. Execute depth-first search on visited vertex.
-                dfs(i, isConnected);
-
-                /**
-                 2.  A province is found by doing a depth-first search on current unvisited vertex
-                     and marking all vertices encountered during this search as visited.
-                 */
-                countProvinces++;
+                dfs(i, isConnected, isVisited);
+                result++;
             }
         }
 
-        return countProvinces;
+        return result;
     }
 
-    private void dfs(int vertex, int[][] isConnected) {
-        // 3. Mark vertex as visited and add it to stack.
-        Stack<Integer> stack = new Stack<>();
-        stack.push(vertex);
+    private void dfs(int vertex, int[][] isConnected, boolean[] isVisited) {
         isVisited[vertex] = true;
 
-        while (!stack.isEmpty()) {
-            // 4. Retrieve last-visited element.
-            int current = stack.pop();
-
-            /**
-             5.  Perform depth-first search
-                 on an unvisited adjacent neighbour of last-visited element.
-             */
-            for (int i = 0; i < n; i++) {
-                if (isConnected[vertex][i] == 1 && !isVisited[i]) {
-                    dfs(i, isConnected);
-                }
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[vertex][j] == 1 && !isVisited[j]) {
+                dfs(j, isConnected, isVisited);
             }
         }
     }
