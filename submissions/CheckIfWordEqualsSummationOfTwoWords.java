ee878406b7101592fb2a5e474d342a5b2f5bd00e
Umar <shumarb@outlook.com>
2026-07-06 12:18:16 +0800

    Check if Word Equals Summation of Two Words: Refactored solution led to same performance

--- submissions/CheckIfWordEqualsSummationOfTwoWords.java
+++ submissions/CheckIfWordEqualsSummationOfTwoWords.java
@@ -1,49 +1,30 @@
 // Question: https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/description/
 
 class CheckIfWordEqualsSummationOfTwoWords {
-    private boolean isTest = false;
-
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        int firstWordNumericalValue = computeValue(firstWord);
-        int secondWordNumericalValue = computeValue(secondWord);
-        int targetWordNumericalValue = computeValue(targetWord);
+        boolean isTest = false;
+        int firstValue = getValue(firstWord);
+        int secondValue = getValue(secondWord);
+        int targetValue = getValue(targetWord);
+
         if (isTest) {
-            System.out.println("firstWord: " + firstWord + " --> " + firstWordNumericalValue);
-            System.out.println("secondWord: " + secondWord + " --> " + secondWordNumericalValue);
-            System.out.println("targetWord: " + targetWord + " --> " + targetWordNumericalValue);
+            System.out.println("firstWord:  " + firstWord + " -> value: " + firstValue);
+            System.out.println("secondWord: " + secondWord + " -> value: " + secondValue);
+            System.out.println("targetWord: " + targetWord + " -> value: " + targetValue);
         }
 
-        return (firstWordNumericalValue + secondWordNumericalValue) == targetWordNumericalValue;
+        return firstValue + secondValue == targetValue;
     }
 
-    private int computeValue(String word) {
-        StringBuilder number = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            int digit = word.charAt(i) - 'a';
-            number.append(digit);
+    private int getValue(String word) {
+        char[] letters = word.toCharArray();
+        int result = 0;
+
+        for (int i = 0; i < letters.length; i++) {
+            result *= 10;
+            result += (int) (letters[i] - 'a');
         }
 
-        int firstNoZeroIndex = 0;
-        while (firstNoZeroIndex < number.length() && number.charAt(firstNoZeroIndex) == '0') {
-            firstNoZeroIndex++;
-        }
-        if (isTest) {
-            System.out.println("word: " + word + " --> " + number);
-            System.out.println(" * firstNoZeroIndex: " + firstNoZeroIndex);
-        }
-        if (firstNoZeroIndex == number.length()) {
-            return 0;
-        }
-
-        int value = 0;
-        for (int i = firstNoZeroIndex; i < number.length(); i++) {
-            value *= 10;
-            value += number.charAt(i) - '0';
-        }
-        if (isTest) {
-            System.out.println(" * value: " + value);
-        }
-
-        return value;
+        return result;
     }
 }
