b91c26590358b6a77fd2fd572ccce58b0a6775fc
Umar <shumarb@outlook.com>
2026-07-19 00:21:25 +0800

    Rearrange String to Avoid Character Pair: Refactored solution led to improved performance

--- submissions/RearrangeStringToAvoidCharacterPair.java
+++ submissions/RearrangeStringToAvoidCharacterPair.java
@@ -2,44 +2,26 @@
 
 class RearrangeStringToAvoidCharacterPair {
     public String rearrangeString(String s, char x, char y) {
-        StringBuilder result = new StringBuilder();
-        boolean isTest = false;
-        int[] count = new int[26];
+        char[] letters = s.toCharArray();
+        char[] result = new char[letters.length];
+        int index = 0;
 
-        for (char c: s.toCharArray()) {
-            count[c - 'a']++;
-        }
-        for (int i = 0; i < count[y - 'a']; i++) {
-            result.append(y);
-        }
-        for (int i = 0; i < count[x - 'a']; i++) {
-            result.append(x);
-        }
-        if (isTest) {
-            System.out.println("s: " + s + "\ncount: " + Arrays.toString(count) + "\nresult so far: " + result);
-        }
-
-        for (int i = 0; i < 26; i++) {
-            char letter = (char) ('a' + i);
-            if (letter == x || letter == y) {
-                continue;
-            }
-
-            if (count[letter - 'a'] > 0) {
-                if (isTest) {
-                    System.out.println(" * add: " + letter);
-                }
-
-                int total = count[letter - 'a'];
-                while (total-- > 0) {
-                    result.append(letter);
-                }
+        for (char c: letters) {
+            if (c == y) {
+                result[index++] = y;
             }
         }
-        if (isTest) {
-            System.out.println("result: " + result);
+        for (char c: letters) {
+            if (c == x) {
+                result[index++] = x;
+            }
+        }
+        for (char c: letters) {
+            if (c != x && c != y) {
+                result[index++] = c;
+            }
         }
 
-        return result.toString();
+        return new String(result);
     }
 }
