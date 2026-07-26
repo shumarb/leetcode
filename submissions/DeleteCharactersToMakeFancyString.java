a64161a93568c850baf5d811f244630898f3236a
Umar <shumarb@outlook.com>
2026-07-26 19:11:11 +0800

    Delete Characters to Make Fancy String: Refactored solution led to improved performance

--- submissions/DeleteCharactersToMakeFancyString.java
+++ submissions/DeleteCharactersToMakeFancyString.java
@@ -2,28 +2,31 @@
 
 class DeleteCharactersToMakeFancyString {
     public String makeFancyString(String s) {
+        StringBuilder result = new StringBuilder();
+        char[] letters = s.toCharArray();
+        char last = letters[0];
+        int count = 1;
+        int n = letters.length;
+
         // 1. Edge case: s has < 3 characters, so answer is itself.
-        if (s.length() < 3) {
+        if (n < 3) {
             return s;
         }
 
-        StringBuilder result = new StringBuilder();
-        boolean isTest = false;
-        char last = s.charAt(0);
-        int count = 1;
-
         result.append(last);
-        for (int i = 1; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (int i = 1; i < n; i++) {
+            char c = letters[i];
             if (c != last) {
                 result.append(c);
                 count = 1;
+
             } else {
                 if (count + 1 != 3) {
                     result.append(c);
                     count++;
                 }
             }
+
             last = c;
         }
 
