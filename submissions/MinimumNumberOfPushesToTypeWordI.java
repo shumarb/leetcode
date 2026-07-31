f95dcb943d6f343dfb20a67b1639f3958fe6412c
Umar <shumarb@outlook.com>
2026-08-01 00:55:47 +0800

    Refactored solutions

--- submissions/MinimumNumberOfPushesToTypeWordI.java
+++ submissions/MinimumNumberOfPushesToTypeWordI.java
@@ -2,45 +2,24 @@
 
 class MininumNumberOfPushesToTypeWordI {
     public int minimumPushes(String word) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-                (a, b) -> Integer.compare(b[1], a[1])
-        );
         boolean isTest = false;
-        int[] count = new int[26];
+        int countLettersPerGroup = 0;
         int countPresses = 1;
-        int countUniqueButtons = 0;
         int result = 0;
 
-        for (char c: word.toCharArray()) {
-            count[c - 'a']++;
-        }
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > 0) {
-                maxHeap.offer(new int[] {i, count[i]});
+        for (char letter: word.toCharArray()) {
+            if (isTest) {
+                System.out.println(" * letter: " + letter + " -> " + countPresses + " presses");
+            }
+
+            result += countPresses;
+            if (++countLettersPerGroup == 8) {
+                countLettersPerGroup = 0;
+                countPresses++;
             }
         }
         if (isTest) {
-            System.out.println("maxHeap:");
-            for (int[] e: maxHeap) {
-                char letter = (char) ('a' + e[0]);
-                if (isTest) {
-                    System.out.println(" * " + letter + ": " + e[1]);
-                }
-            }
-            System.out.println("----------------------------------");
-        }
-        while (!maxHeap.isEmpty()) {
-            int[] top = maxHeap.poll();
-            char letter = (char) ('a' + top[0]);
-            result += (top[1] * countPresses);
-
-            if (isTest) {
-                System.out.println(" * " + letter + ", press: " + countPresses + " times");
-            }
-            if (++countUniqueButtons == 8) {
-                countPresses++;
-                countUniqueButtons = 0;
-            }
+            System.out.println("-------------------------\nresult: " + result);
         }
 
         return result;
--- submissions/MinimumNumberOfPushesToTypeWordII.java
+++ submissions/MinimumNumberOfPushesToTypeWordII.java
@@ -2,48 +2,27 @@
 
 class MininumNumberOfPushesToTypeWordII {
     public int minimumPushes(String word) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-                (a, b) -> Integer.compare(b[1], a[1])
-        );
-        boolean isTest = false;
         int[] count = new int[26];
         int countPresses = 1;
-        int countUniqueLetters = 0;
-        long result = 0;
+        int countUniqueLettersInGroup = 0;
+        int result = 0;
 
         for (char c: word.toCharArray()) {
             count[c - 'a']++;
         }
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > 0) {
-                maxHeap.offer(new int[] {i, count[i]});
+        Arrays.sort(count);
+        for (int i = 25; i >= 0; i--) {
+            if (count[i] == 0) {
+                break;
             }
-        }
-        if (isTest) {
-            System.out.println("maxHeap:");
-            for (int[] e: maxHeap) {
-                System.out.println(" * " + (char) ('a' + e[0]) + ": " + e[1]);
-            }
-            System.out.println("----------------------------------------------------------------------");
-        }
-        while (!maxHeap.isEmpty()) {
-            int[] top = maxHeap.poll();
-            char letter = (char) ('a' + top[0]);
-            int totalPresses = top[1] * countPresses;
 
-            if (isTest) {
-                System.out.println(" * " + letter + ", frequency: " + top[1] + ", presses per letter: " + countPresses + ", total presses: " + totalPresses);
-            }
-            result += totalPresses;
-            if (++countUniqueLetters == 8) {
+            result += count[i] * countPresses;
+            if (++countUniqueLettersInGroup == 8) {
                 countPresses++;
-                countUniqueLetters = 0;
+                countUniqueLettersInGroup = 0;
             }
         }
-        if (isTest) {
-            System.out.println("----------------------------------------------------------------------\nresult: " + result);
-        }
 
-        return (int) result;
+        return result;
     }
 }
