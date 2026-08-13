8b20c8f5097fd880eb131d9f61f6c9e11bf9bdfb
Umar <shumarb@outlook.com>
2026-08-14 00:01:57 +0800

    Minimum Hours of Training to Win a Competition: Refactored solution led to improved performance

--- submissions/MinimumHoursOfTrainingToWinACompetition.java
+++ submissions/MinimumHoursOfTrainingToWinACompetition.java
@@ -1,6 +1,6 @@
 // Question: https://leetcode.com/problems/minimum-hours-of-training-to-win-a-competition/description/
 
-class MinimumHoursOfTrainingToWinACompetition {
+class MinimumHoursOfTrainingToWinACompetition class Solution {
     public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
         boolean isTest = false;
         int n = energy.length;
@@ -13,25 +13,22 @@ class MinimumHoursOfTrainingToWinACompetition {
 
             int currentEnergy = energy[i];
             int currentExperience = experience[i];
+            int difference = 0;
 
-            if (initialEnergy > currentEnergy && initialExperience > currentExperience) {
-                initialEnergy -= currentEnergy;
-                initialExperience += currentExperience;
-
-            } else {
-                while (initialEnergy <= currentEnergy) {
-                    initialEnergy++;
-                    result++;
-                }
-
-                while (initialExperience <= currentExperience) {
-                    initialExperience++;
-                    result++;
-                }
-
-                initialEnergy -= currentEnergy;
-                initialExperience += currentExperience;
+            if (initialEnergy <= currentEnergy) {
+                difference = Math.abs(initialEnergy - currentEnergy) + 1;
+                initialEnergy += difference;
+                result += difference;
             }
+
+            if (initialExperience <= currentExperience) {
+                difference = Math.abs(initialExperience - currentExperience) + 1;
+                initialExperience += difference;
+                result += difference;
+            }
+
+            initialEnergy -= currentEnergy;
+            initialExperience += currentExperience;
 
             if (isTest) {
                 System.out.println("\n * after, initialEnergy: " + initialEnergy + ", initialExperience: " + initialExperience + ", result: " + result);
