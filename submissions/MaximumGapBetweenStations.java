b6c4ba61e3153dd898a8d3c8d1707d9a00922325
Umar <shumarb@outlook.com>
2026-08-16 18:06:03 +0800

    Maximum Gap Between Stations: Refactored solution led to improved performance

--- submissions/MaximumGapBetweenStations.java
+++ submissions/MaximumGapBetweenStations.java
@@ -12,30 +12,44 @@ class MaximumGapBetweenStations {
         int[] earliest = new int[n];
         int[] latest = new int[n];
 
-        Arrays.fill(earliest, -1);
-        Arrays.fill(latest, -1);
+        // 1. Find earliest and latest indices of mapping each character in skills to a an equal character in stations.
         for (int i = 0; i < n; i++) {
-            while (stations[j] != skills[i]) {
+            while (skills[i] != stations[j]) {
                 j++;
             }
 
             earliest[i] = j++;
         }
+
         j = m - 1;
         for (int i = n - 1; i >= 0; i--) {
-            while (stations[j] != skills[i]) {
+            while (skills[i] != stations[j]) {
                 j--;
             }
+
             latest[i] = j--;
         }
+        if (isTest) {
+            System.out.println("station:  " + Arrays.toString(stations) + "\nskills:   " + Arrays.toString(skills) + "\nearliest: " + Arrays.toString(earliest) + "\nlatest:   " + Arrays.toString(latest));
+            System.out.println("----------------------------------------------------------------------------");
+        }
 
+        // 2. For every consecutive worker, compare the difference in distance between the first worker's earliest index and the second
+        //    worker's latest index to determine the maximum possible difference between the station indices.
         for (int i = 0; i < n - 1; i++) {
-            result = Math.max(latest[i + 1] - earliest[i], result);
+            char first = skills[i];
+            char second = skills[i + 1];
+            int gap = latest[i + 1] - earliest[i];
+
+            if (isTest) {
+                System.out.println(" * indices: [" + i + ", " + (i + 1) + "] | letters: [" + first + ", " + second + "] | earliest-latest: [" + earliest[i] + ", " + latest[i + 1] + "] | gap: " + gap);
+            }
+
+            result = Math.max(gap, result);
         }
         if (isTest) {
-            System.out.println("earliest: " + Arrays.toString(earliest) + "\nlatest: " + Arrays.toString(latest) + "\nresult: " + result);
+            System.out.println("-----------------------------------------------------------------------------\nresult: " + result);
         }
-
 
         return result;
     }
