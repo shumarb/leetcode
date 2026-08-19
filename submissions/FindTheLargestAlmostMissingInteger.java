6ab1bacafc8abb0978fadbafb64c87f6df847ccb
Umar <shumarb@outlook.com>
2026-08-19 13:04:10 +0800

    Find the Largest Almost Missing Integer: Refactored solution led to same runtime and lower rank

--- submissions/FindTheLargestAlmostMissingInteger.java
+++ submissions/FindTheLargestAlmostMissingInteger.java
@@ -3,46 +3,42 @@
 class FindTheLargestAlmostMissingInteger {
     public int largestInteger(int[] nums, int k) {
         boolean isTest = false;
-        int largest = nums[0];
-        int len = nums.length;
-        int[] frequency;
-        int[] window = new int[k];
+        int[] count;
+        int largest = 0;
+        int result = -1;
 
-        for (int number: nums) {
-            largest = Math.max(largest, number);
+        for (int e: nums) {
+            largest = Math.max(e, largest);
         }
-        frequency = new int[largest + 1];
+        count = new int[largest + 1];
 
-        for (int i = 0; i <= len - k; i++) {
-            int j = i;
-            for (int a = 0; a < k; a++) {
-                window[a] = nums[j++];
+        for (int i = 0; i <= nums.length - k; i++) {
+            boolean[] isChecked = new boolean[largest + 1];
+            int[] subarray = Arrays.copyOfRange(nums, i, i + k);
+
+            for (int e: subarray) {
+                if (isChecked[e]) {
+                    continue;
+                }
+
+                count[e]++;
+                isChecked[e] = true;
             }
             if (isTest) {
-                System.out.println("window: " + Arrays.toString(window));
+                System.out.println(" * indices: [" + i + ", " + (i + k - 1) + "] | subarray: " + Arrays.toString(subarray) + " | count: " + Arrays.toString(count));
             }
-            update(window, frequency);
+        }
+
+        for (int i = largest; i >= 0; i--) {
+            if (count[i] == 1) {
+                result = i;
+                break;
+            }
         }
         if (isTest) {
-            System.out.println("----------------------------------------------------------");
-            System.out.println("k: " + k + "\nnums: " + Arrays.toString(nums) + "\nfrequency: " + Arrays.toString(frequency));
-        }
-        for (int i = frequency.length - 1; i >= 0; i--) {
-            if (frequency[i] == 1) {
-                return i;
-            }
+            System.out.println("-----------------------------------------\ncount: " + Arrays.toString(count) + "\nresult: " + result);
         }
 
-        return -1;
-    }
-
-    private void update(int[] window, int[] frequency) {
-        boolean[] isCounted = new boolean[frequency.length];
-        for (int element: window) {
-            if (!isCounted[element]) {
-                isCounted[element] = true;
-                frequency[element]++;
-            }
-        }
+        return result;
     }
 }
