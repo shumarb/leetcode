4f50997c8a6efbc17f9772e12c0fc605521bde1c
Umar <shumarb@outlook.com>
2026-08-22 13:08:58 +0800

    Updates after latest checks and resubmission

--- submissions/CheckDivisibilityByDigitSumAndProduct.java
+++ submissions/CheckDivisibilityByDigitSumAndProduct.java
@@ -2,20 +2,17 @@
 
 class CheckDivisibilityByDigitSumAndProduct {
     public boolean checkDivisibility(int n) {
-        boolean isTest = false;
-        int product = 1;
-        int sum = 0;
+        int digitProduct = 1;
+        int digitSum = 0;
+        int nCopy = n;
 
-        while (nCopy != 0) {
+        while (nCopy > 0) {
             int digit = nCopy % 10;
-            product *= digit;
-            sum += digit;
+            digitProduct *= digit;
+            digitSum += digit;
             nCopy /= 10;
         }
-        if (isTest) {
-            System.out.println("n: " + n + "\nsum: " + sum + "\nproduct: " + product);
-        }
 
-        return n % (sum + product) == 0;
+        return n % (digitSum + digitProduct) == 0;
     }
 }
