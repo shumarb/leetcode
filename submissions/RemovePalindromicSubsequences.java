ef7594160065e8adfe2977017baa9dd7f93055a4
Umar <shumarb@outlook.com>
2026-09-22 09:49:13 +0800

    Refactored solution led to same performance; Updated rank after latest check

--- submissions/RemovePalindromicSubsequences.java
+++ submissions/RemovePalindromicSubsequences.java
@@ -2,19 +2,24 @@
 
 class RemovePalindromicSubsequences {
     public int removePalindromeSub(String s) {
-        return isPalindrome(s.toCharArray()) ? 1 : 2;
-    }
-
-    private boolean isPalindrome(char[] letters) {
+        char[] letters = s.toCharArray();
         int left = 0;
         int right = letters.length - 1;
 
         while (left < right) {
             if (letters[left++] != letters[right--]) {
-                return false;
+                /**
+                 1.  Given string is not a palindrome, and there are only 2 characters,
+                     remove all letters of the first letter (1 step), then all letters of the second letter (1 step),
+                     hence 2 steps are neded to convert s to an empty string.
+                 */
+                return 2;
             }
         }
 
-        return true;
+        /**
+            2. String is a palindrome, so remove all its characters to get an empty string.
+         */
+        return 1;
     }
 }
